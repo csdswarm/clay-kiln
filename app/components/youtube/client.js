2fa7bc89e347768c0b753ca6c8ed21d8fa8f00b3
Jon Winton <winton.jon@gmail.com>
2018-07-18 17:01:54 -0400

    removing Dollar Slice from youtube component

--- app/components/youtube/client.js
+++ app/components/youtube/client.js
@@ -2,7 +2,7 @@
 
 const youtubeVideoPlayer = require('../../services/universal/youtube-video-player');
 
-DS.controller('youtube', ['$visibility', '$gtm', function ($visibility, $gtm) {
+
   function Constructor(el) {
     var videoConfig = {
         videoContainerId: el.getAttribute('data-element-id').trim(),
@@ -24,8 +24,7 @@ DS.controller('youtube', ['$visibility', '$gtm', function ($visibility, $gtm) {
           autoPlayNextVideo: el.getAttribute('data-autoplay-next-video').trim(),
           trackVideoType: el.getAttribute('data-track-video-type').trim()
         }
-      },
-      analytics = getAnalyticsCustomDimensions(el);
+      }
 
     if (videoConfig.customParams.trackVideoType === 'Sponsored') {
       videoConfig.playerParams.list = '';
@@ -33,34 +32,16 @@ DS.controller('youtube', ['$visibility', '$gtm', function ($visibility, $gtm) {
     this.el = el;
     this.visible = new $visibility.Visible(this.el, { preloadThreshold: 800 });
 
-    // when the video player element enters the viewport, load the video(s)
-    if (this.visible.preload && $visibility.isElementNotHidden(el)) {
-      // if the YouTube api is ready the videos(s) can be loaded
-      if (window.nymYTApiReady === true) {
-        youtubeVideoPlayer.init(videoConfig);
-      } else {
-        // wait and listen for the YouTube api to be ready before loading the video(s)
-        document.addEventListener('clay-youtube-event:youtube-api-ready', function () {
-          youtubeVideoPlayer.init(videoConfig);
-        });
-      }
+
+    // if the YouTube api is ready the videos(s) can be loaded
+    if (window.nymYTApiReady === true) {
+      youtubeVideoPlayer.init(videoConfig);
     } else {
-      this.visible.on('preload', function () {
+      // wait and listen for the YouTube api to be ready before loading the video(s)
+      document.addEventListener('clay-youtube-event:youtube-api-ready', function () {
         youtubeVideoPlayer.init(videoConfig);
       });
     }
-
-    /**
-     * Player ready event
-     * this fires when the player is initially loaded and pushes variables specific to the
-     * component into the data layer. Information about the video itself is captured from the
-     * native gtm.video trigger on play and finish
-     */
-    document.addEventListener('player-ready-' + videoConfig.videoContainerId, function () {
-      $gtm.reportNow(Object.assign({
-        youtubeAction: 'player ready'
-      }, analytics));
-    });
 
     /**
      * Player start event
@@ -73,14 +54,6 @@ DS.controller('youtube', ['$visibility', '$gtm', function ($visibility, $gtm) {
 
       if (hasChanged) {
         updateElementAttributes(el, evt.player);
-        // this will tell the gtm.video trigger to stop ignoring gtm.video events
-        // in the case that an external video was played initially then switched to
-        // an internal playlist
-        $gtm.reportNow(Object.assign({
-          event: 'youtubeVideoReset',
-          youtubeVideoId: evt.player.videoId,
-          youtubeChannelName: 'New York Magazine'
-        }));
       }
     });
   }
@@ -94,22 +67,4 @@ DS.controller('youtube', ['$visibility', '$gtm', function ($visibility, $gtm) {
     el.setAttribute('data-video-id', config.videoId);
   }
 
-  /**
-   * Gets analytics custom dimensions for video player
-   * @param {Object} el
-   * @returns {Object} analytics
-   */
-  function getAnalyticsCustomDimensions(el) {
-    return {
-      event: 'youtubeVideo',
-      youtubeVideoId: el.getAttribute('data-video-id'),
-      youtubeVideoLocation: el.getAttribute('data-track-video-location'),
-      youtubeVideoType: el.getAttribute('data-track-video-type'),
-      youtubeVideoTitle: el.getAttribute('data-track-video-title'),
-      youtubeChannelName: el.getAttribute('data-track-channel-name'),
-      youtubeVideoDuration: el.getAttribute('data-track-video-duration')
-    };
-  }
-
-  return Constructor;
-}]);
+module.exports = el => new Constructor(el);
