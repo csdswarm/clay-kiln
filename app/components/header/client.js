72824655845c77d4aa793699bd1575beffc84628
Tiffany Chow <tiffachow@gmail.com>
2018-08-15 16:31:55 -0400

    ON-10 ON-11 Add header template, js and styling

---
+++ app/components/header/client.js
@@ -0,0 +1,107 @@
+let isMobile = false;
+let activeHamburger = false;
+const toggleHamburger = function(toggleHamburgerOnly) {
+  isMobile =
+    document.getElementsByClassName("radiocom-nav-placeholder")[0].offsetWidth >
+    1023
+      ? false
+      : true;
+  activeHamburger = !activeHamburger;
+  let bars = document.getElementsByClassName("bar");
+  let navDrawer = document.getElementsByClassName("nav-drawer--mobile")[0];
+  if (activeHamburger && isMobile) {
+    for (let bar of bars) {
+      bar.classList.add("active");
+    }
+    navDrawer.classList.add("nav-drawer--active");
+  } else {
+    for (let bar of bars) {
+      bar.classList.remove("active");
+    }
+    navDrawer.classList.remove("nav-drawer--active");
+  }
+  if (!toggleHamburgerOnly) toggleDrawer(navDrawer, activeHamburger, true);
+};
+const toggleDrawer = function(event, show) {
+  let navDrawer;
+  isMobile =
+    document.getElementsByClassName("radiocom-nav-placeholder")[0].offsetWidth >
+    1023
+      ? false
+      : true;
+  let classes = ["nav-drawer--sub-nav-active", "nav-drawer--active"];
+  let navDrawers = document.getElementsByClassName("nav-drawer");
+  for (let drawer of navDrawers) {
+    drawer.classList.remove(...classes);
+  }
+  if (!isMobile) {
+    navDrawer = event.currentTarget.querySelector(".nav-drawer");
+    if (show && ["listen", "download"].indexOf(navDrawer.classList[1]) == -1) {
+      navDrawer.classList.add(...classes);
+    }
+  } else {
+    navDrawer = document.getElementsByClassName("nav-drawer--mobile")[0];
+    if (show) {
+      navDrawer.classList.add(...classes);
+    } else {
+      navDrawer.classList.remove(...classes);
+    }
+  }
+};
+const setCategory = function(event) {
+  // Add active class to section
+  const activeMobileClass = "nav-drawer__sub-nav--active";
+  if (event.currentTarget.classList.contains(activeMobileClass)) {
+    event.currentTarget.classList.remove(activeMobileClass);
+  } else {
+    for (let navSection of mobileNavSections) {
+      navSection.classList.remove(activeMobileClass);
+    }
+    event.currentTarget.classList.add(activeMobileClass);
+  }
+};
+document.getElementById("hamburger").addEventListener("click", toggleHamburger);
+const mobileNavSections = document.getElementsByClassName(
+  "nav-drawer__sub-nav"
+);
+for (let navSection of mobileNavSections) {
+  // Toggle Mobile Section Navs
+  navSection.addEventListener("click", function(e) {
+    setCategory(e);
+  });
+}
+const navSections = document.getElementsByClassName(
+  "radiocom-nav__category-button"
+);
+for (let navSection of navSections) {
+  const activeClass = "radiocom-nav__category-button--active";
+  // Highlight Section Button in Nav if on Section Page
+  // document.getElementsByClassName("radiocom-nav__category-button--entertainment")[0].classList.add(activeClass);
+
+  // Add Listeners to Section Buttons on Hover to Toggle Drawers
+  if (
+    !navSection.classList.contains("radiocom-nav__category-button--download") &&
+    !navSection.classList.contains("radiocom-nav__category-button--listen")
+  ) {
+    navSection.addEventListener("mouseover", function(e) {
+      toggleDrawer(e, true);
+    });
+    navSection.addEventListener("mouseout", function(e) {
+      toggleDrawer(e, false);
+    });
+  }
+}
+// Add Listener on Window Resize to Remove Mobile Nav When Not on Mobile
+window.addEventListener("resize", function() {
+  let navDrawer = document.getElementsByClassName("nav-drawer--mobile")[0];
+  isMobile =
+    document.getElementsByClassName("radiocom-nav-placeholder")[0].offsetWidth >
+    1023
+      ? false
+      : true;
+  let classes = ["nav-drawer--active", "nav-drawer--sub-nav-active"];
+  if (!isMobile) {
+    navDrawer.classList.remove(...classes);
+    toggleHamburger(true);
+  }
+});
