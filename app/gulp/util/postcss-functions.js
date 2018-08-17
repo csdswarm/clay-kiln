73c3eb6d1eb39ef2460f7037b8d6eed74076e9fb
Markham F Rollins IV <markham@swarmsolutions.com>
2018-08-17 08:47:19 -0400

    Remove console.log

--- app/gulp/util/postcss-functions.js
+++ app/gulp/util/postcss-functions.js
@@ -1,6 +1,5 @@
 module.exports = {
   em: function(pixels, browserContext) {
-    console.log('in em');
     var browserContext = parseInt(browserContext, 10) || 16;
     var pixels = parseInt(pixels, 10);
     return (pixels / browserContext) + 'em';
