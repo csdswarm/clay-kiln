9e7c31024ec9e87dac7ba8715681e5eac4ca94db
ncarmine <nathanacarmine@gmail.com>
2018-08-21 12:03:56 -0600

    ON-69-Social Fix copying URL to clipboard

--- app/components/share/client.js
+++ app/components/share/client.js
@@ -1,6 +1,12 @@
-function copyToClipboard(text) {
+/**
+ * Copy text from a button event to the clipboard
+ * Adopted from https://stackoverflow.com/a/30810322
+ * @function copyToClipboard
+ * @param {Object} e - HTML event emitted on button click
+*/
+function copyToClipboard(e) {
   // Create text area for copying
-  const textArea = document.createElement("linktextarea");
+  const textArea = document.createElement("textarea");
 
   // Hide textArea
   textArea.style.position = 'fixed';
@@ -9,17 +15,15 @@ function copyToClipboard(text) {
 
   textArea.width = '2em';
   textArea.height = '2em';
-
   textArea.style.padding = 0;
 
   textArea.border = 'none';
   textArea.outline = 'none';
   textArea.boxShadow = 'none';
-
   textArea.style.background = 'transparent';
 
   // Prepare text and textArea for copying
-  textArea.value = text;
+  textArea.value = e.path[0].formAction; // get url from event
   document.body.appendChild(textArea);
 
   textArea.focus();
@@ -36,3 +40,18 @@ function copyToClipboard(text) {
   // Remove textArea
   document.body.removeChild(textArea);
 }
+
+/**
+ * Add event listener for all .copy-link elements
+ * @function addCopyEventListener
+*/
+function addCopyEventListener() {
+  const copyLinks = document.getElementsByClassName('copy-link');
+
+  // Convert copyLinks to an array, and add a click event listener for each
+  Array.from(copyLinks, cl => {
+    cl.addEventListener('click', e => copyToClipboard(e));
+  });
+}
+
+addCopyEventListener()
