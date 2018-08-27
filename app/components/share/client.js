81c59aed0053bdf5db5c577cdcd56135482c1b10
ncarmine <nathanacarmine@gmail.com>
2018-08-27 11:00:16 -0600

    ON-69-Social Convert client.js to exportable

--- app/components/share/client.js
+++ app/components/share/client.js
@@ -1,56 +1,64 @@
-/**
- * Copy text from a button event to the clipboard
- * Adopted from https://stackoverflow.com/a/30810322
- * @function copyToClipboard
- * @param {Object} e - HTML event emitted on button click
-*/
-function copyToClipboard(e) {
-  // Create text area for copying
-  const textArea = document.createElement("textarea");
+'use strict';
 
-  // Hide textArea
-  textArea.style.position = 'fixed';
-  textArea.top = 0;
-  textArea.left = 0;
+function Constructor(el) {
+  this.addCopyEventListener();
+}
 
-  textArea.width = '2em';
-  textArea.height = '2em';
-  textArea.style.padding = 0;
+Constructor.prototype = {
+  /**
+  * Copy text from a button event to the clipboard
+  * Adopted from https://stackoverflow.com/a/30810322
+  * @function copyToClipboard
+  * @param {Object} e - HTML event emitted on button click
+  */
+  copyToClipboard: function (e) {
+    // Create text area for copying
+    const textArea = document.createElement("textarea");
 
-  textArea.border = 'none';
-  textArea.outline = 'none';
-  textArea.boxShadow = 'none';
-  textArea.style.background = 'transparent';
+    // Hide textArea
+    textArea.style.position = 'fixed';
+    textArea.top = 0;
+    textArea.left = 0;
 
-  // Prepare text and textArea for copying
-  textArea.value = e.target.baseURI; // get url from event
-  document.body.appendChild(textArea);
+    textArea.width = '2em';
+    textArea.height = '2em';
+    textArea.style.padding = 0;
 
-  textArea.focus();
-  textArea.select();
+    textArea.border = 'none';
+    textArea.outline = 'none';
+    textArea.boxShadow = 'none';
+    textArea.style.background = 'transparent';
 
-  try {
-    // Copy text
-    const successful = document.execCommand('copy');
-  } catch (err) {
-    console.log('Unable to copy');
+    // Prepare text and textArea for copying
+    textArea.value = e.target.baseURI; // get url from event
+    document.body.appendChild(textArea);
+
+    textArea.focus();
+    textArea.select();
+
+    try {
+      // Copy text
+      const successful = document.execCommand('copy');
+    } catch (err) {
+      console.log('Unable to copy');
+    }
+
+    // Remove textArea
+    document.body.removeChild(textArea);
+  },
+
+  /**
+  * Add event listener for all .copy-link elements
+  * @function addCopyEventListener
+  */
+  addCopyEventListener: function () {
+    const copyLinks = document.getElementsByClassName('copy-link');
+
+    // Convert copyLinks to an array, and add a click event listener for each
+    Array.from(copyLinks, cl => {
+      cl.addEventListener('click', e => this.copyToClipboard(e));
+    });
   }
-
-  // Remove textArea
-  document.body.removeChild(textArea);
 }
 
-/**
- * Add event listener for all .copy-link elements
- * @function addCopyEventListener
-*/
-function addCopyEventListener() {
-  const copyLinks = document.getElementsByClassName('copy-link');
-
-  // Convert copyLinks to an array, and add a click event listener for each
-  Array.from(copyLinks, cl => {
-    cl.addEventListener('click', e => copyToClipboard(e));
-  });
-}
-
-addCopyEventListener()
+module.exports = el => new Constructor(el);
