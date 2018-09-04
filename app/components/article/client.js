430b72a87a15ffa223660fe48e170353014a0cc5
Tiffany Chow <tiffachow@gmail.com>
2018-09-04 12:07:01 -0400

    ON-68 Reposition right rail below article headers

---
+++ app/components/article/client.js
@@ -0,0 +1,11 @@
+function repositionRightRail() {
+	const sidebar = document.getElementsByClassName("content__sidebar")[0]
+	const articleBody = document.getElementsByClassName("article__body")[0]
+	sidebar.style.top = articleBody.offsetTop + "px"
+	sidebar.style.position = "relative"
+	sidebar.style.visibility = "visible"
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+	repositionRightRail()
+}, false)
