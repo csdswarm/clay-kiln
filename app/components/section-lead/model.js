b315a0f06093a37664f28fdd45a1aba83392d5e6
Markham F Rollins IV <markham@swarmsolutions.com>
2018-10-02 13:46:21 -0400

    ON-156: Updated section lead to be dynamic

--- app/components/section-lead/model.js
+++ app/components/section-lead/model.js
@@ -1,3 +1,96 @@
 'use strict';
 
-// @TODO: ON-156 - Write render and save functions to handle articles similar to inline-related
+const queryService = require('../../services/server/query'),
+  _ = require('lodash'),
+  recircCmpt = require('../../services/universal/recirc-cmpt'),
+  toPlainText = require('../../services/universal/sanitize').toPlainText,
+  { isComponent } = require('clayutils'),
+  elasticIndex = 'published-articles',
+  elasticFields = [
+    'primaryHeadline',
+    'pageUri',
+    'canonicalUrl',
+    'feedImgUrl'
+  ],
+  maxItems = 3;
+
+/**
+ * @param {string} ref
+ * @param {object} data
+ * @param {object} locals
+ * @returns {Promise}
+ */
+module.exports.save = (ref, data, locals) => {
+  if (!data.items.length || !locals) {
+    return data;
+  }
+
+  return Promise.all(_.map(data.items, (item) => {
+    item.urlIsValid = item.ignoreValidation ? 'ignore' : null;
+
+    return recircCmpt.getArticleDataAndValidate(ref, item, locals, elasticFields)
+      .then((result) => {
+        const article = Object.assign(item, {
+          primaryHeadline: item.overrideTitle || result.primaryHeadline,
+          pageUri: result.pageUri,
+          urlIsValid: result.urlIsValid,
+          canonicalUrl: result.canonicalUrl,
+          feedImgUrl: result.feedImgUrl
+        });
+
+        if (article.title) {
+          article.plaintextTitle = toPlainText(article.title);
+        }
+
+        return article;
+      });
+  }))
+    .then((items) => {
+      data.items = items;
+
+      return data;
+    });
+};
+
+/**
+ * @param {string} ref
+ * @param {object} data
+ * @param {object} locals
+ * @returns {Promise}
+ */
+module.exports.render = function (ref, data, locals) {
+  console.log('test');
+  const query = queryService.newQueryWithCount(elasticIndex, data.fill, locals);
+  let cleanUrl;
+
+  // items are saved from form, articles are used on FE
+  data.articles = data.items;
+
+  if (!data.sectionTitle || !locals) {
+    return data;
+  }
+console.log('hello');
+  queryService.withinThisSiteAndCrossposts(query, locals.site);
+  queryService.onlyWithTheseFields(query, elasticFields);
+  queryService.addShould(query, { match: { articleType: data.sectionTitle }});
+  queryService.addMinimumShould(query, 1);
+  queryService.addSort(query, {date: 'desc'});
+
+  // exclude the current page in results
+  if (locals.url && !isComponent(locals.url)) {
+    cleanUrl = locals.url.split('?')[0].replace('https://', 'http://');
+    queryService.addMustNot(query, { match: { canonicalUrl: cleanUrl } });
+  }
+
+  return queryService.searchByQuery(query)
+    .then(function (results) {
+
+      data.articles = data.items.concat(_.take(results, maxItems)).slice(0, maxItems); // show a maximum of maxItems links
+      console.log(data.articles);
+      return data;
+    })
+    .catch(e => {
+      queryService.logCatch(e, ref);
+      return data;
+    });
+};
