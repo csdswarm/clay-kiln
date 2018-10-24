3c5522c4768239133792dfd47570ae17612c8982
Tiffany Chow <tiffachow@gmail.com>
2018-10-24 18:58:12 -0400

    ON-73 Stations Carousel -- Add pagination logic, replace stations with clientside query using geo api

--- app/components/stations-carousel/client.js
+++ app/components/stations-carousel/client.js
@@ -1,54 +1,135 @@
 'use strict';
-const db = require('../../services/client/db'),
-  rest = require('../../services/universal/rest'),
-  radioApi = 'https://api.radio.com/v1/',
+const radioApi = 'https://api.radio.com/v1/',
   geoApi = 'https://geo.radio.com/markets',
-  localStorage = window.localStorage;
+  localStorage = window.localStorage,
+  _defaults = require('lodash/defaults');
+
+// global
+require('isomorphic-fetch');
+
+/**
+ * check status after doing http calls
+ * note: this is necessary because fetch doesn't reject on errors,
+ * only on network failure or incomplete requests
+ * @param  {object} res
+ * @return {object}
+ * @throws {Error} on non-2xx status
+ */
+function restCheckStatus (res) {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  } else {
+    const error = new Error(res.statusText);
+
+    error.response = res;
+    throw error;
+  }
+}
+/**
+ * GET
+ * @param {string} url
+ * @param {object} opts See https://github.github.io/fetch/#options
+ * @return {Promise}
+ */
+function restGet (url, opts) {
+  const conf = _defaults({method: 'GET'}, opts);
+
+  return fetch(url, conf).then(restCheckStatus).then(function (res) { return res.json(); });
+};
 
 function Constructor() {
   console.log("stations carousel client js works");
-  const stationsCarousel = document.querySelector('.component--stations-carousel'),
-    filterStationsBy = stationsCarousel.getAttribute('data-filter-stations-by-track'),
-    genre = stationsCarousel.getAttribute('data-genre-track'),
-    sectionFront = stationsCarousel.getAttribute('data-section-front-track'),
-    windowWidth = window.outerWidth,
-    paginationDots = document.querySelector('.carousel__pagination-dots'),
-    leftArrow = document.querySelector('.stations-carousel__arrow--left'),
-    rightArrow = document.querySelector('.stations-carousel__arrow--right');
+  this.stationsCarousel = document.querySelector('.component--stations-carousel');
+  this.stationsList = this.stationsCarousel.querySelector('ul');
+  this.filterStationsBy = this.stationsCarousel.getAttribute('data-filter-stations-by-track');
+  this.genre = this.stationsCarousel.getAttribute('data-genre-track');
+  this.sectionFront = this.stationsCarousel.getAttribute('data-section-front-track');
+  this.windowWidth = window.outerWidth;
+  this.paginationDots = document.querySelector('.carousel__pagination-dots');
+  this.dotClass = 'pagination-dots__dot';
+  this.leftArrow = document.querySelector('.stations-carousel__arrow--left');
+  this.rightArrow = document.querySelector('.stations-carousel__arrow--right');
+  this.marketID = localStorage.getItem('marketID');
+  this.filterByValue = null;
+  this.pageSize = 1; // Number of stations to move left/right when navigating
+  this.imageSize = 140 + 20;
+  this.pageNum = 1;
 
-  let marketID = localStorage.getItem('marketID'),
-    pageSize = 300,
-    pageNum = 1;
-
-  if (windowWidth <= 1023) {
-    if (windowWidth >= 481) {
-      pageSize = 3;
-    } else if (windowWidth >= 361) {
-      pageSize = 2;
+  if (this.windowWidth >= 1280) {
+    this.layoutWidth = '1100px';
+  } else if (this.windowWidth >= 1024) {
+    this.layoutWidth = '940px';
+  } else {
+    this.layoutWidth = '728px';
+    if (this.windowWidth < 788) {
+      this.layoutWidth = '100%';
+    }
+    if (this.windowWidth >= 481) {
+      this.pageSize = 3;
+      this.imageSize = 222 + 20;
+    } else if (this.windowWidth >= 361) {
+      this.pageSize = 2;
+      this.imageSize = 209 + 20;
+    } else {
+      this.pageSize = 2;
+      this.imageSize = 150 + 20;
     }
   }
 
-  switch (filterStationsBy) {
+  switch (this.filterStationsBy) {
     case 'section-front':
-      filterByValue = sectionFront;
-      if (sectionFront == 'entertainment') filterByValue = 'music';
+      this.filterByValue = this.sectionFront;
+      if (this.sectionFront == 'entertainment') this.filterByValue = 'music';
       break;
     case 'genre':
-      filterByValue = genre;
+      this.filterByValue = this.genre;
       break;
     default:
   }
 
-  stationsCarousel.setAttribute('style',`
+  this.stationsCarousel.setAttribute('style',`
     width: ${document.body.clientWidth}px;
-    margin-left: calc((${document.body.clientWidth}px - 1100px) / -2);
+    margin-left: calc((${document.body.clientWidth}px - ${this.layoutWidth}) / -2);
   `);
-  this.updateStations(filterStationsBy, filterValue, pageSize, pageNum);
-  leftArrow.addEventListener('click', this.getPrevStation);
-  rightArrow.addEventListener('click', this.getNextStation);
+  this.updateStations();
+  this.leftArrow.addEventListener('click', this.getPrevStation);
+  this.rightArrow.addEventListener('click', this.getNextStation);
+  window.addEventListener('resize', function(e){this.restyleCarousel(e, this)}.bind(this));
 }
 
 Constructor.prototype = {
+  restyleCarousel: function (event, _this) {
+    _this.windowWidth = window.outerWidth;
+    _this.pageSize = 1; // Number of stations to move left/right when navigating
+    _this.imageSize = 140 + 20;
+    _this.pageNum = 1;
+
+    if (_this.windowWidth >= 1280) {
+      _this.layoutWidth = '1100px';
+    } else if (_this.windowWidth >= 1024) {
+      _this.layoutWidth = '940px';
+    } else {
+      _this.layoutWidth = '728px';
+      if (_this.windowWidth < 788) {
+        _this.layoutWidth = '100%';
+      }
+      if (_this.windowWidth >= 481) {
+        _this.pageSize = 3;
+        _this.imageSize = 222 + 20;
+      } else if (_this.windowWidth >= 361) {
+        _this.pageSize = 2;
+        _this.imageSize = 209 + 20;
+      } else {
+        _this.pageSize = 2;
+        _this.imageSize = 150 + 20;
+      }
+    }
+    _this.stationsCarousel.setAttribute('style',`
+      width: ${document.body.clientWidth}px;
+      margin-left: calc((${document.body.clientWidth}px - ${_this.layoutWidth}) / -2);
+    `);
+    _this.createPaginationDots();
+  },
   getPrevStation: function () {
 
   },
@@ -56,25 +137,43 @@ Constructor.prototype = {
     // transform: translateX(-160px);
   },
   createPaginationDots: function () {
+    this.totalPages = this.stationsData.count / this.pageSize;
+    while (this.paginationDots.lastChild) {
+      this.paginationDots.removeChild(this.paginationDots.lastChild);
+    }
     for (let page = 1; page <= this.totalPages; page++) {
       let dot = document.createElement('div');
-      dot.setAttribute('class', 'pagination-dots__dot');
+      dot.setAttribute('class', this.dotClass);
       dot.setAttribute('data-page', page);
-      dot.addEventListener('click', this.getPage);
-      paginationDots.appendChild(dot);
+      dot.addEventListener('click', function(e){this.getPage(e, this)}.bind(this));
+      this.paginationDots.appendChild(dot);
     }
+    this.updatePaginationDots();
   },
-  getPage: function (event) {
-    this.pageNum = event.currentTarget.getAttribute('data-page');
-    this.updateStations(this.filterStationsBy, this.filterValue, this.pageSize, this.pageNum);
+  updatePaginationDots: function () {
+    let allDots = document.querySelectorAll(`.${this.dotClass}`);
+    for(let d = 0; d < allDots.length; d++) {
+      allDots[d].classList.remove('dot--active');
+    };
+    document.querySelector(`.${this.dotClass}[data-page='${this.pageNum}']`).classList.add('dot--active');
+  },
+  getPage: function (event, _this) {
+    _this.pageNum = event.currentTarget.getAttribute('data-page');
+    _this.updatePaginationDots();
+    let pageStationsLocation = (_this.pageNum - 1) * _this.pageSize * _this.imageSize;
+    if (this.windowWidth < 1024) {
+      this.stationsList.setAttribute('style',`transform: translateX(-${pageStationsLocation}px);`);
+    } else {
+
+    }
   },
   /**
    * Get user's local market ID with geo api & set in browser storage
    * @function
    */
   getMarket: function () {
-    if (!marketID) {
-      return rest.get(geoApi).then(marketData => {
+    if (!this.marketID) {
+      return restGet(geoApi).then(marketData => {
         if (marketData.Markets) {
           console.log("updated market in client:", marketData);
           localStorage.setItem('marketID', marketData.Markets[0].id);
@@ -84,97 +183,67 @@ Constructor.prototype = {
         }
       });
     } else {
-      return marketID;
+      return Promise.resolve(this.marketID);
     }
   },
-  updateStations: function (filterStationsBy, filterValue, pageSize, pageNum) {
-    return this.getMarket().then(marketID => {
-      return this.getStationsByFilter(filterStationsBy, marketID, filterValue, pageSize, pageNum).then(stationsData => {
+  updateStations: function () {
+    return this.getMarket().then(() => {
+      return this.getFilteredStationsFromApi().then(stationsData => {
         console.log("updated stations in client:", stationsData);
-        this.totalPages = stationsData.count / pageSize;
-        if (windowWidth <= 1023) this.createPaginationDots();
-        return stationsData.data;
+        this.stationsData = stationsData;
+        if (this.windowWidth <= 1023) this.createPaginationDots();
+        while (this.stationsList.lastChild) {
+          this.stationsList.removeChild(this.stationsList.lastChild);
+        }
+        for (let i = 0; i < stationsData.count; i++) {
+          let station = document.createElement('li'),
+            stationData = stationsData.stations[i];
+
+          station.innerHTML = `
+            <a href='${stationData.listen_live_url}' target='_blank'>
+              <img class='thumb'
+                  srcset='${stationData.square_logo_large}?width=222&dpr=1.5 1.5x,
+                    ${stationData.square_logo_large}?width=222&dpr=2 2x'
+                  src='${stationData.square_logo_large}?width=222'
+              />
+              <span>${stationData.name}</span>
+            </a>
+          `
+          this.stationsList.appendChild(station);
+        }
       }).catch((e) => {
         console.log("Update query error: ", e);
-        return [];
       });
     }).catch(e => {
       console.log("Market query error: ", e);
-      return [];
     });
   },
-  getStationsByFilter: function (filterStationsBy, marketID, filterByValue, pageSize, pageNum) {
-    // Get stations from redis cache
-    return db.get(`stations_filterBy_${filterStationsBy}_${filterByValue}_marketID_${marketID}`)
-      .then(stationsData => {
-        // Update data if older than 5 minutes
-        if (stationsData.dateUpdated < Date.now() - 6 * 1000) {
-          return this.updateStationsObject(filterStationsBy, marketID, filterByValue, pageSize, pageNum, stationsData).then(newData => {
-            console.log('get updated stations from api: ', newData.data);
-            return newData;
-          });
-        } else {
-          console.log('get stored stations from redis: ', stationsData.data);
-          return stationsData;
-        }
-      }).catch(() => {
-        // No stations found in redis so get from API
-        return this.updateStationsObject(filterStationsBy, marketID, filterByValue, pageSize, pageNum);
-      });
-  },
-  getFilteredStationsFromApi: function (filterStationsBy, marketID, filterByValue, pageSize, pageNum) {
-    let params = `?sort=-popularity&filter[market_id]=${marketID}`;
+  getFilteredStationsFromApi: function () {
+    let params = `?sort=-popularity&filter[market_id]=${this.marketID}&page[size]=300`;
 
-    if (pageSize) {
-      params += `&page[size]=${pageSize}`
-    } else {
-      params += '&page[size]=300'
-    }
-
-    if (pageNum) {
-      params += `&page[number]=${pageNum}`
-    } else {
-      params += '&page[number]=1'
-    }
-
-    switch (filterStationsBy) {
+    switch (this.filterStationsBy) {
       case 'section-front':
-        params += `&filter[category]=${filterByValue}`;
+        params += `&filter[category]=${this.filterByValue}`;
         break;
       case 'genre':
-        params += `&filter[genre_id]=${filterByValue}`;
+        params += `&filter[genre_id]=${this.filterByValue}`;
         break;
       default:
     }
     console.log("params: ", params);
-    return rest.get(`${radioApi}stations${params}`).then(response => {
+    return restGet(`${radioApi}stations${params}`).then(response => {
       if (response.data) {
-        let newData = {
-          dateUpdated: Date.now(),
-          data: response.data.map(station => {
+        let stationsData = {
+          stations: response.data.map(station => {
             return station.attributes;
           }),
           count: response.meta.count
         };
-
-        // Store stations in redis cache
-        db.put(`stations_filterBy_${filterStationsBy}_${filterByValue}_marketID_${marketID}`, JSON.stringify(newData));
-        return newData;
+        return stationsData;
       } else {
         return Promise.reject();
       }
     });
-  },
-  updateStationsObject: function (filterStationsBy, marketID, filterByValue, pageSize, pageNum, stationsData) {
-    // If stations object exists, update the timestamp so we dont make multiple API requests
-    if (stationsData) {
-      db.put(`stations_filterBy_${filterStationsBy}_${filterByValue}_marketID_${marketID}`, JSON.stringify(stationsData)).then(() =>
-      {
-        return this.getFilteredStationsFromApi(filterStationsBy, marketID, filterByValue, pageSize, pageNum);
-      });
-    } else {
-      return this.getFilteredStationsFromApi(filterStationsBy, marketID, filterByValue, pageSize, pageNum);
-    }
   }
 };
 
