4dbb0c571f0aa78debca19b7cdf89b6c21885b3c
Tiffany Chow <tiffachow@gmail.com>
2018-10-26 19:20:29 -0400

    ON-73 Stations Carousel -- Refactor, add comments, add centering logic, lint, fix bugs

--- app/components/stations-carousel/client.js
+++ app/components/stations-carousel/client.js
@@ -5,38 +5,35 @@ const radioApi = 'https://api.radio.com/v1/',
   localStorage = window.localStorage;
 
 function Constructor() {
-  console.log("stations carousel client js works");
   this.allStationsCount = 303;
   this.stationsCarouselClass = '.component--stations-carousel';
   this.stationsCarousel = document.querySelector(this.stationsCarouselClass);
-  this.stationsList = document.querySelector(`${this.stationsCarouselClass} ul`);
   this.filterStationsBy = this.stationsCarousel.getAttribute('data-filter-stations-by-track');
   this.genre = this.stationsCarousel.getAttribute('data-genre-track');
   this.sectionFront = this.stationsCarousel.getAttribute('data-section-front-track');
-  this.windowWidth = window.outerWidth;
-  this.paginationDots = document.querySelector('.carousel__pagination-dots');
-  this.dotClass = 'pagination-dots__dot';
+  this.stationsList = document.querySelector(`${this.stationsCarouselClass} ul`);
   this.leftArrow = document.querySelector('.stations-carousel__arrow--left');
   this.rightArrow = document.querySelector('.stations-carousel__arrow--right');
+  this.paginationDots = document.querySelector('.carousel__pagination-dots');
+  this.dotClass = 'pagination-dots__dot';
   this.marketID = localStorage.getItem('marketID');
-  this.filterByValue = null;
   this.pageSize = 1; // Number of stations to move left/right when navigating
   this.gutterWidth = 20;
   this.imageSize = 140 + this.gutterWidth;
   this.pageNum = 1;
+  this.windowWidth = window.outerWidth;
+  this.windowSizes = {
+    large: 1280,
+    medium: 1024,
+    beforeMediumSmall: 788,
+    mediumSmall: 480
+  };
 
-  switch (this.filterStationsBy) {
-    case 'section-front':
-      this.filterByValue = this.sectionFront;
-      if (this.sectionFront == 'entertainment') this.filterByValue = 'music';
-      break;
-    case 'genre':
-      this.filterByValue = this.genre;
-      break;
-    default:
-  }
+  if (this.filterStationsBy == 'section-front') {
+    this.filterByValue = this.sectionFront;
+    if (this.sectionFront == 'entertainment') this.filterByValue = 'music';
+  } else if (this.filterStationsBy == 'genre') this.filterByValue = this.genre;
   this.updateStations();
-  window.addEventListener('resize', function(e){this.restyleCarousel(e, this)}.bind(this));
 }
 
 Constructor.prototype = {
@@ -45,46 +42,41 @@ Constructor.prototype = {
    * @function
    */
   setImageAndPageDims: function () {
-    if (this.windowWidth >= 1280) {
+    if (this.windowWidth >= this.windowSizes.large) {
       this.layoutWidth = '1100px';
-      this.stationsViewable = 7;
-    } else if (this.windowWidth >= 1024) {
+      this.stationsVisible = 7;
+    } else if (this.windowWidth >= this.windowSizes.medium) {
       this.layoutWidth = '940px';
-      this.stationsViewable = 6;
-    } else { // window width < 1024
+      this.stationsVisible = 6;
+    } else {
       this.layoutWidth = '728px';
       this.gutterWidth = 31;
       this.pageSize = 3;
-      this.stationsViewable = 3;
+      this.stationsVisible = 3;
       this.imageSize = 222 + this.gutterWidth;
-      if (this.windowWidth < 788) {
+      if (this.windowWidth < this.windowSizes.beforeMediumSmall) {
         this.layoutWidth = '100%';
         this.gutterWidth = 20;
-        if (this.windowWidth < 480) {
+        if (this.windowWidth < this.windowSizes.mediumSmall) {
           this.pageSize = 2;
-          this.stationsViewable = 2;
+          this.stationsVisible = 2;
         }
-        let calculatedImageSize = (document.body.clientWidth - 40 - this.gutterWidth * (this.stationsViewable - 1)) / this.stationsViewable;
-        console.log(calculatedImageSize, document.body.clientWidth, this.gutterWidth, this.stationsViewable);
+        let calculatedImageSize = (document.body.clientWidth - 40 - this.gutterWidth * (this.stationsVisible - 1)) / this.stationsVisible;
+
         this.imageSize = calculatedImageSize + this.gutterWidth;
-        console.log(this.stationsNodes);
-        for (let i = 0; i < this.stationsNodes.length; i++) {
-          this.stationsNodes[i].setAttribute('style', `width: ${calculatedImageSize}px; height: ${calculatedImageSize}px;`);
-        };
+        // set image size dependent on window size
+        for (let i = 0; i < this.stationsNodes.length; i++) this.stationsNodes.thumbs[i].setAttribute('style', `width: ${calculatedImageSize}px; height: ${calculatedImageSize}px;`);
       }
     }
-    if (this.windowWidth >= 1024) {
-      // reset image size when resizing window after switching from small to large window widths
-      for (let i = 0; i < this.stationsNodes.length; i++) {
-        this.stationsNodes[i].setAttribute('style', '');
-      };
-    }
+    // reset image size style after switching to large window width (image sizes are fixed at this window width)
+    if (this.windowWidth >= this.windowSizes.medium) for (let i = 0; i < this.stationsNodes.length; i++) this.stationsNodes.thumbs[i].setAttribute('style', '');
   },
   /**
    * Set width of carousel to full window width
    * @function
    */
   setCarouselWidth: function () {
+    // set width of carousel to full width and display it to prevent flashing
     this.stationsCarousel.setAttribute('style',`
       display: inline-flex;
       width: ${document.body.clientWidth}px;
@@ -103,9 +95,9 @@ Constructor.prototype = {
     _this.gutterWidth = 20;
     _this.imageSize = 140 + _this.gutterWidth;
     _this.setImageAndPageDims();
-    _this.totalPages = _this.stationsData.count / _this.pageSize;
+    _this.totalPages = Math.ceil(_this.stationsData.count / _this.pageSize);
     _this.setCarouselWidth();
-    if (this.windowWidth <= 1023) _this.createPaginationDots();
+    if (_this.windowWidth < _this.windowSizes.medium) _this.createPaginationDots();
     _this.getPage(null, _this);
   },
   /**
@@ -113,17 +105,44 @@ Constructor.prototype = {
    * @function
    */
   hideOrShowEndArrows: function () {
-    if (this.windowWidth > 1023) {
+    if (this.windowWidth >= this.windowSizes.medium) {
+      let visible = 'visibility: visible',
+        hidden = 'visibility: hidden';
+
       if (this.pageNum <= 1) {
-        this.leftArrow.setAttribute('style', 'visibility: hidden;')
+        this.leftArrow.setAttribute('style', hidden);
+        this.rightArrow.setAttribute('style', visible);
       } else {
-        this.leftArrow.setAttribute('style', 'visibility: visible;');
-        if (this.pageNum > this.totalPages - this.stationsViewable) {
-          this.rightArrow.setAttribute('style', 'visibility: hidden;')
-        } else {
-          this.rightArrow.setAttribute('style', 'visibility: visible;')
-        }
+        this.leftArrow.setAttribute('style', visible);
+        this.rightArrow.setAttribute('style', visible);
       }
+      if (this.pageNum > this.totalPages - this.stationsVisible) this.rightArrow.setAttribute('style', hidden);
+    }
+  },
+  /**
+   * Center station results on first or last page if they don't fill up the page
+   * @function
+   */
+  centerPageResults: function () {
+    let remainderStations = this.stationsData.count % this.pageSize;
+
+    if (this.totalPages * this.pageSize <= this.stationsVisible) { // Center stations if fills short of first page
+      this.stationsList.classList.add('align-center');
+      for (let i = 0; i < this.stationsNodes.length - remainderStations; i++) this.stationsNodes[i].style.visibility = 'visible'; // reset visibility
+    } else if (this.pageNum == this.totalPages) { // Center stations if fill short of last page
+      if (remainderStations > 0) {
+        // hide previous page results
+        for (let i = 0; i < this.stationsNodes.length - remainderStations; i++) this.stationsNodes[i].style.visibility = 'hidden';
+        // center stations on last page
+        let centeredLocation = - (this.pageStationsLocation - (this.stationsVisible - remainderStations) * this.imageSize / 2);
+
+        this.stationsList.setAttribute('style',`transform: translateX(${centeredLocation}px);`);
+      }
+    } else {
+      // unhide other stations & unset centering if not on first/last page
+      for (let i = 0; i < this.stationsNodes.length - remainderStations; i++) this.stationsNodes[i].style.visibility = 'visible'; // reset visibility
+      this.stationsList.setAttribute('style',`transform: translateX(-${this.pageStationsLocation}px);`); // uncenter last page
+      this.stationsList.classList.remove('align-center'); // uncenter first page
     }
   },
   /**
@@ -131,15 +150,17 @@ Constructor.prototype = {
    * @function
    */
   createPaginationDots: function () {
-    while (this.paginationDots.lastChild) {
-      this.paginationDots.removeChild(this.paginationDots.lastChild);
-    }
-    for (let page = 1; page <= this.totalPages; page++) {
-      let dot = document.createElement('div');
-      dot.setAttribute('class', this.dotClass);
-      dot.setAttribute('data-page', page);
-      dot.addEventListener('click', function(e){this.getPage(e, this)}.bind(this));
-      this.paginationDots.appendChild(dot);
+    // Remove existing pagination
+    while (this.paginationDots.lastChild) this.paginationDots.removeChild(this.paginationDots.lastChild);
+    if (this.totalPages > 1) { // Create pagination dots if more than one page
+      for (let page = 1; page <= this.totalPages; page++) {
+        let dot = document.createElement('div');
+
+        dot.setAttribute('class', this.dotClass);
+        dot.setAttribute('data-page', page);
+        dot.addEventListener('click', function (e) { this.getPage(e, this); }.bind(this));
+        this.paginationDots.appendChild(dot);
+      }
     }
     this.updatePaginationDots();
   },
@@ -148,11 +169,13 @@ Constructor.prototype = {
    * @function
    */
   updatePaginationDots: function () {
-    let allDots = document.querySelectorAll(`.${this.dotClass}`);
-    for(let d = 0; d < allDots.length; d++) {
-      allDots[d].classList.remove('dot--active');
-    };
-    document.querySelector(`.${this.dotClass}[data-page='${this.pageNum}']`).classList.add('dot--active');
+    if (this.totalPages > 1) {
+      let allDots = document.querySelectorAll(`.${this.dotClass}`);
+
+      // Remove active styling for previous active page dot & set active class for currently active page dot
+      for (let d = 0; d < allDots.length; d++) allDots[d].classList.remove('dot--active');
+      document.querySelector(`.${this.dotClass}[data-page='${this.pageNum}']`).classList.add('dot--active');
+    }
   },
   /**
    * Show new page results on click of arrows or pagination dots
@@ -161,27 +184,22 @@ Constructor.prototype = {
    * @function
    */
   getPage: function (event, _this) {
-    let pageStationsLocation;
-
-    if (_this.windowWidth < 1024) { // nav using pagination dots
-      if (event) _this.pageNum = event.currentTarget.getAttribute('data-page');
-      pageStationsLocation = (_this.pageNum - 1) * _this.pageSize * _this.imageSize;
+    if (_this.windowWidth < _this.windowSizes.medium) { // nav using pagination dots
+      if (event) _this.pageNum = Number(event.currentTarget.getAttribute('data-page')); // get page number of clicked dot
       _this.updatePaginationDots();
     } else { // nav using left/right arrows
-      console.log('total pages: ', _this.totalPages, _this.stationsData.count, _this.pageSize);
       // reset page number if on nonexistent page after switching from dots pagination to arrow navigation
-      if (_this.pageNum > _this.totalPages - _this.stationsViewable) _this.pageNum = 1;
-      if (event) {
-        if (event.currentTarget.getAttribute('data-direction') == 'left') {
-          if (_this.pageNum !== 1) _this.pageNum = _this.pageNum - 1;
-        } else {
-          if (_this.pageNum <= _this.totalPages - _this.stationsViewable) _this.pageNum = _this.pageNum + 1;
-        }
+      if (_this.pageNum > _this.totalPages + 1 - _this.stationsVisible) _this.pageNum = 1;
+      if (event) { // if arrow clicked, update page number
+        if (event.currentTarget.getAttribute('data-direction') == 'left') _this.pageNum = _this.pageNum - 1;
+        else if (_this.pageNum <= _this.totalPages - _this.stationsVisible) _this.pageNum = _this.pageNum + 1;
       }
       _this.hideOrShowEndArrows();
-      pageStationsLocation = (_this.pageNum - 1) * _this.pageSize * _this.imageSize;
     }
-    _this.stationsList.setAttribute('style',`transform: translateX(-${pageStationsLocation}px);`);
+    // Calculate location of page in all station results and slide results to that page
+    _this.pageStationsLocation = (_this.pageNum - 1) * _this.pageSize * _this.imageSize;
+    _this.stationsList.setAttribute('style',`transform: translateX(-${_this.pageStationsLocation}px);`);
+    _this.centerPageResults();
   },
   /**
    * Get user's local market ID with geo api & set in browser storage
@@ -192,16 +210,11 @@ Constructor.prototype = {
     if (!this.marketID) {
       return rest.get(geoApi).then(marketData => {
         if (marketData.Markets.length > 0) {
-          console.log("updated market in client:", marketData);
-          localStorage.setItem('marketID', marketData.Markets[0].id);
-          return marketData.Markets[0].id;
-        } else {
-          return 14; // national
-        }
+          localStorage.setItem('marketID', marketData.Markets[0].id); // Store market in browser
+          this.marketID = localStorage.getItem('marketID'); // Store market in var
+        } else this.marketID = 14; // National market if no results from geo API
       });
-    } else {
-      return Promise.resolve(this.marketID);
-    }
+    } else return Promise.resolve();
   },
   /**
    * Get stations from api using market ID and filters
@@ -211,39 +224,29 @@ Constructor.prototype = {
   getFilteredStationsFromApi: function () {
     let params = `?sort=-popularity&filter[market_id]=${this.marketID}&page[size]=${this.allStationsCount}`;
 
-    switch (this.filterStationsBy) {
-      case 'section-front':
-        params += `&filter[category]=${this.filterByValue}`;
-        break;
-      case 'genre':
-        params += `&filter[genre_id]=${this.filterByValue}`;
-        break;
-      default:
-    }
+    if (this.filterStationsBy == 'section-front') params += `&filter[category]=${this.filterByValue}`;
+    else if (this.filterStationsBy == 'genre') params += `&filter[genre_id]=${this.filterByValue}`;
+
     return rest.get(`${radioApi}stations${params}`).then(response => {
       if (response.data) {
         let stationsData = {
           stations: response.data.map(station => {
             return station.attributes;
           }),
-          count: response.meta.count
+          count: response.meta.count // Store total count of station results to determine pagination
         };
+
         return stationsData;
-      } else {
-        return Promise.reject();
-      }
+      } else return Promise.reject();
     });
   },
   /**
    * Insert new payload of stations into DOM
    * @function
-   * @returns {Promise}
    */
   updateStationsDOM: function () {
-    while (this.stationsList.lastChild) {
-      this.stationsList.removeChild(this.stationsList.lastChild);
-    }
-    for (let i = 0; i < this.stationsData.count; i++) {
+    while (this.stationsList.lastChild) this.stationsList.removeChild(this.stationsList.lastChild); // Remove loader
+    for (let i = 0; i < this.stationsData.count; i++) { // Create & append station nodes into DOM
       let station = document.createElement('li'),
         stationData = this.stationsData.stations[i];
 
@@ -259,7 +262,10 @@ Constructor.prototype = {
       `;
       this.stationsList.appendChild(station);
     }
-    this.stationsNodes = document.querySelectorAll(`${this.stationsCarouselClass} .thumb`);
+    // Store for stations centering when stations do not fill up page
+    this.stationsNodes = document.querySelectorAll(`${this.stationsCarouselClass} li`);
+    // Store for image resizing when window width is medium or smaller
+    this.stationsNodes.thumbs = document.querySelectorAll(`${this.stationsCarouselClass} .thumb`);
   },
   /**
    * Initial function - retrieve new payload of stations into DOM and enable navigation
@@ -269,23 +275,18 @@ Constructor.prototype = {
   updateStations: function () {
     return this.getMarket().then(() => {
       return this.getFilteredStationsFromApi().then(stationsData => {
-        console.log("updated stations in client:", stationsData);
         this.stationsData = stationsData;
         this.updateStationsDOM();
         this.setImageAndPageDims();
         this.setCarouselWidth();
-        this.totalPages = this.stationsData.count / this.pageSize;
+        this.totalPages = Math.ceil(this.stationsData.count / this.pageSize);
         this.hideOrShowEndArrows();
-        if (this.windowWidth <= 1023) this.createPaginationDots();
-
-        this.leftArrow.addEventListener('click', function(e){this.getPage(e, this)}.bind(this));
-        this.rightArrow.addEventListener('click', function(e){this.getPage(e, this)}.bind(this));
+        this.centerPageResults();
+        if (this.windowWidth < this.windowSizes.medium) this.createPaginationDots();
+        this.leftArrow.addEventListener('click', function (e) { this.getPage(e, this); }.bind(this));
+        this.rightArrow.addEventListener('click', function (e) { this.getPage(e, this); }.bind(this));
+        window.addEventListener('resize', function (e) { this.restyleCarousel(e, this); }.bind(this));
         return stationsData;
-      }).catch((e) => {
-        this.stationsData = [];
-        this.totalPages = 0;
-        this.hideOrShowEndArrows();
-        console.log("Update stations query error: ", e);
       });
     });
   }
--- app/components/stations-carousel/model.js
+++ app/components/stations-carousel/model.js
@@ -7,16 +7,14 @@
  * @returns {Promise}
  */
 module.exports.save = (ref, data, locals) => {
+  if (!locals) return data;
   switch (data.filterStationsBy) {
     case 'market':
       data.title = 'stations near you' || data.overrideTitle;
       break;
     case 'section-front':
-      if (data.sectionFront == 'entertainment') {
-        data.title = 'music stations near you' || data.overrideTitle;
-      } else {
-        data.title = `${data.sectionFront} stations near you` || data.overrideTitle;
-      }
+      if (data.sectionFront == 'entertainment') data.title = 'music stations near you' || data.overrideTitle;
+      else data.title = `${data.sectionFront} stations near you` || data.overrideTitle;
       break;
     case 'genre':
       data.title = `${data.genre} stations near you` || data.overrideTitle;
