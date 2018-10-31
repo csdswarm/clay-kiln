1de5dd699f5c18c520a4f2877735a26651f4bc34
Nick Bryant <sbryant31@gmail.com>
2018-10-31 09:57:36 -0400

    move to async await

--- app/components/latest-content/model.js
+++ app/components/latest-content/model.js
@@ -2,10 +2,8 @@
 const queryService = require('../../services/server/query'),
   _ = require('lodash'),
   recircCmpt = require('../../services/universal/recirc-cmpt'),
-  {
-    isComponent
-  } = require('clayutils'),
-  // tag = require('../tags/model.js'),
+  { isComponent } = require('clayutils'),
+  tag = require('../tags/model.js'),
   elasticIndex = 'published-articles',
   elasticFields = [
     'primaryHeadline',
@@ -15,43 +13,38 @@ const queryService = require('../../services/server/query'),
   ],
   maxItems = 3;
 /**
+ * For each section's override items (0 through 3), look up the associated
+ * articles and save them in redis.
+ *
  * @param {string} ref
  * @param {object} data
  * @param {object} locals
  * @returns {Promise}
  */
 
-module.exports.save = (ref, data, locals) => {
-  if (!data.entertainmentItems.length || !locals) {
-    return data;
-  }
-  return Promise.all(_.map(data.entertainmentItems, (item) => {
-    item.urlIsValid = item.ignoreValidation ? 'ignore' : null;
-    return recircCmpt.getArticleDataAndValidate(ref, item, locals, elasticFields)
-      .then((result) => {
-        const article = Object.assign(item, {
-          primaryHeadline: item.overrideTitle || result.primaryHeadline,
-          pageUri: result.pageUri,
-          urlIsValid: result.urlIsValid,
-          canonicalUrl: item.url || result.canonicalUrl,
-          feedImgUrl: item.overrideImage || result.feedImgUrl
-        });
+module.exports.save = async function (ref, data, locals) {
+  for (let section of data.sectionFronts) {
+    const items = data[`${section}Items`]
+    if (!items.length || !locals) {
+      continue;
+    }
 
-        return article;
+    // for each item, look up the associated article and save that.
+    data[`${section}Items`] = await Promise.all(items.map(async (item) => {
+      item.urlIsValid = item.ignoreValidation ? 'ignore' : null;
+      const result = await recircCmpt.getArticleDataAndValidate(ref, item, locals, elasticFields)
+      const article = Object.assign(item, {
+        primaryHeadline: item.overrideTitle || result.primaryHeadline,
+        pageUri: result.pageUri,
+        urlIsValid: result.urlIsValid,
+        canonicalUrl: item.url || result.canonicalUrl,
+        feedImgUrl: item.overrideImage || result.feedImgUrl
       });
-  }))
-    .then((entertainmentItems) => {
-      data.entertainmentItems = entertainmentItems;
-      return data;
-    })
-    .then((sportsItems) => {
-      data.sportsItems = sportsItems;
-      return data;
-    })
-    .then((newsItems) => {
-      data.newsItems = newsItems;
-      return data;
-    });
+
+      return article;
+    }))
+  }
+  return data;
 };
 /**
  * @param {string} ref
@@ -59,109 +52,35 @@ module.exports.save = (ref, data, locals) => {
  * @param {object} locals
  * @returns {Promise}
  */
-module.exports.render = function (ref, data, locals) {
+module.exports.render = async function (ref, data, locals) {
+  for (let section of data.sectionFronts) {
+    const items = data[`${section}Items`],
+      query = queryService.newQueryWithCount(elasticIndex, maxItems, locals),
+      cleanUrl = locals.url.split('?')[0].replace('https://', 'http://');
 
-  const entertainmentQuery = queryService.newQueryWithCount(elasticIndex, maxItems, locals),
-    sportsQuery = queryService.newQueryWithCount(elasticIndex, maxItems, locals),
-    newsQuery = queryService.newQueryWithCount(elasticIndex, maxItems, locals);
+    queryService.withinThisSiteAndCrossposts(query, locals.site);
+    queryService.onlyWithTheseFields(query, elasticFields);
+    queryService.addMinimumShould(query, 1);
+    queryService.addSort(query, {date: 'desc'});
 
-  let cleanUrl;
-
-  // Entertainment
-  queryService.withinThisSiteAndCrossposts(entertainmentQuery, locals.site);
-  queryService.onlyWithTheseFields(entertainmentQuery, elasticFields);
-  queryService.addShould(entertainmentQuery, {
-    match: {
-      articleType: 'entertainment'
+    // exclude the current page in results
+    if (locals.url && !isComponent(locals.url)) {
+      queryService.addMustNot(query, { match: { canonicalUrl: cleanUrl } });
     }
-  });
 
-  queryService.addMinimumShould(entertainmentQuery, 1);
-  queryService.addSort(entertainmentQuery, {
-    date: 'desc'
-  });
-
-  // Sports
-
-  queryService.withinThisSiteAndCrossposts(sportsQuery, locals.site);
-  queryService.onlyWithTheseFields(sportsQuery, elasticFields);
-  queryService.addShould(sportsQuery, {
-    match: {
-      articleType: 'sports'
+    // exclude the curated content from the results
+    if (items && !isComponent(locals.url)) {
+      items.forEach(item => {
+        queryService.addMustNot(query, { match: { canonicalUrl: cleanUrl } });
+      });
     }
-  });
 
-  queryService.addMinimumShould(sportsQuery, 1);
-  queryService.addSort(sportsQuery, {
-    date: 'desc'
-  });
-
-  // News
-
-  queryService.withinThisSiteAndCrossposts(newsQuery, locals.site);
-  queryService.onlyWithTheseFields(newsQuery, elasticFields);
-  queryService.addShould(newsQuery, {
-    match: {
-      articleType: 'news'
-    }
-  });
-
-  queryService.addMinimumShould(newsQuery, 1);
-  queryService.addSort(newsQuery, {
-    date: 'desc'
-  });
-
-  // exclude the current page in results
-  // if (locals.url && !isComponent(locals.url)) {
-  //   cleanUrl = locals.url.split('?')[0].replace('https://', 'http://');
-  //   queryService.addMustNot(query, { match: { canonicalUrl: cleanUrl } });
-  // }
-
-  // exclude the curated content from the results
-
-  if (data.entertainmentItems && !isComponent(locals.url)) {
-    data.entertainmentItems.forEach(item => {
-      cleanUrl = item.canonicalUrl.split('?')[0].replace('https://', 'http://');
-      queryService.addMustNot(entertainmentQuery, {
-        match: {
-          canonicalUrl: cleanUrl
-        }
-      });
-      queryService.addMustNot(sportsQuery, {
-        match: {
-          canonicalUrl: cleanUrl
-        }
-      });
-      queryService.addMustNot(newsQuery, {
-        match: {
-          canonicalUrl: cleanUrl
-        }
-      });
-    });
-  }
-
-  return queryService.searchByQuery(entertainmentQuery)
-    .then(function (results) {
-      data.entertainmentArticles = data.entertainmentItems.concat(_.take(results, maxItems)).slice(0, maxItems); // show a maximum of maxItems links
-      queryService.searchByQuery(sportsQuery)
-        .then(function (results) {
-          data.sportsArticles = data.sportsItems.concat(_.take(results, maxItems)).slice(0, maxItems); // show a maximum of maxItems links
-          queryService.searchByQuery(newsQuery)
-            .then(function (results) {
-              data.newsArticles = data.newsItems.concat(_.take(results, maxItems)).slice(0, maxItems); // show a maximum of maxItems links
-            })
-            .catch(e => {
-              queryService.logCatch(e, ref);
-              return data;
-            });
-        })
-        .catch(e => {
-          queryService.logCatch(e, ref);
-          return data;
-        });
-    })
-    .catch(e => {
+    try {
+      const results = await queryService.searchByQuery(query)
+      data[`${section}Articles`] = items.concat(_.take(results, maxItems)).slice(0, maxItems); // show a maximum of maxItems links
+    } catch(e) {
       queryService.logCatch(e, ref);
-      return data;
-    });
+    }
+  }
+  return data;
 };
