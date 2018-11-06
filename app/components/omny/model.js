ff7e817d8ea93b07cb6ae07da9a1b5ccb0955718
Tiffany Chow <tiffachow@gmail.com>
2018-11-06 16:05:31 -0500

    ON-57 Omny Embed -- Parse url params

--- app/components/omny/model.js
+++ app/components/omny/model.js
@@ -1,8 +1,29 @@
 'use strict';
 
 /**
+ * Determine embed style from clip URL's params
+ * @param {object} data
+ */
+function setEmbedSize(data) {
+  if (data.clipURL) {
+    if (data.clipURL.split('style=')[1].indexOf('artwork') !== -1) {
+      data.embedSize = 'wide-simple';
+    } else if (data.clipURL.split('style=')[1].indexOf('cover') !== -1) {
+      if (data.clipURL.indexOf('size=square') !== -1) {
+        data.embedSize = 'square';
+      } else {
+        data.embedSize = 'wide-image';
+      }
+    } else {
+      data.embedSize = 'default';
+    }
+  }
+  return data;
+}
+
+/**
  * Strip clip URL of anything other than show and clip title
- * @param {string} URL
+ * @param {object} data
  */
 function trimClipURL(data) {
   data.clipURL = data.clipURL ? data.clipURL.split('/embed')[0].split('?')[0] : '';
@@ -10,6 +31,7 @@ function trimClipURL(data) {
 }
 
 module.exports.save = (uri, data, locals) => {
+  setEmbedSize(data);
   trimClipURL(data);
   return data;
 };
