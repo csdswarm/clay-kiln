b718cb136f6a06dcfb3069c3836335139257b4f3
Markham F Rollins IV <markham@swarmsolutions.com>
2018-11-06 17:39:43 -0500

    ON-209: Add market, station, genre, cat, tag, pid and authors to ad targeting.

--- app/components/google-ad-manager/client.js
+++ app/components/google-ad-manager/client.js
@@ -2,14 +2,19 @@
 
 let adMapping = require('./adMapping'),
   adSizes = adMapping.adSizes,
-  refreshCount = 0;
+  refreshCount = 0,
+  googleDefinedSlots = [];
 const doubleclickPrefix = '21674100491',
   doubleclickBannerTag = 'NTL.RADIO',
   doubleclickPageTypeTagArticle = 'article',
   doubleclickPageTypeTagSection = 'sectionfront',
+  doubleclickPageTypeTagTag = 'tag',
   adRefreshInterval = '120000', // Time in milliseconds for ad refresh
   adSlots = document.getElementsByClassName('google-ad-manager__slot'),
-  googleDefinedSlots = [];
+  targetingMarket = require('../../services/client/market'),
+  targetingNationalRadioStation = 'natlrc',
+  targetingGenre = 'aaa',
+  targetingCategory = 'music';
 
 // On page load set up sizeMappings
 adMapping.setupSizeMapping();
@@ -19,7 +24,10 @@ document.addEventListener('google-ad-manager-mount', function () {
   // code to run when vue mounts/updates
   setAdsIDs();
 });
+
 document.addEventListener('google-ad-manager-dismount', function () {
+
+  googleDefinedSlots = [];
   googletag.cmd.push(function () {
     googletag.destroySlots();
   });
@@ -43,33 +51,63 @@ function setAdsIDs() {
  * use ids of ad slots on page to create google ad slots and display them
  */
 function setAds() {
-  let page, articleType,
-    siteZone = doubleclickPrefix.concat('/',doubleclickBannerTag,'/');
+  let page,
+    pageName,
+    siteZone = doubleclickPrefix.concat('/', doubleclickBannerTag),
+    urlPathname = window.location.pathname.replace(/\/$/, ''),
+    targetingTags = [],
+    targetingPageId = '',
+    targetingAuthors = [];
 
   if (document.getElementsByTagName('article').length > 0) {
-    page = 'article';
-    articleType = document.getElementsByTagName('article')[0].getAttribute('data-article-type');
+    page = pageName = 'article';
   } else {
-    page = 'homepage';
+    if (urlPathname === '') {
+      page = 'homepage';
+    } else if (urlPathname.indexOf('/tags/') !== -1) {
+      page = 'tagPage';
+      pageName = urlPathname.replace('/tags/', '');
+    } else {
+      page = 'genrePage';
+      pageName = urlPathname;
+    }
   }
+
+  // Set up targeting and ad paths based on current page
   switch (page) {
     case 'article':
-      siteZone = siteZone.concat(articleType,'/',doubleclickPageTypeTagArticle);
+      targetingTags = [doubleclickPageTypeTagArticle];
+      [...document.querySelectorAll('.component--tags .tags__item')].forEach(tag => {
+        targetingTags.push(tag.getAttribute('data-tag'));
+      });
+      targetingPageId = doubleclickPageTypeTagArticle + '_' + urlPathname.split('/').pop().substring(0, 35);
+      [...document.querySelectorAll('.component--article .author')].forEach(tag => {
+        targetingAuthors.push(tag.getAttribute('data-author').replace(/\s/, '-').toLowerCase());
+      });
+      siteZone = siteZone.concat('/', pageName, '/', doubleclickPageTypeTagArticle);
       break;
     case 'homepage':
-      siteZone = siteZone.concat('home','/',doubleclickPageTypeTagSection);
+      targetingTags = [doubleclickPageTypeTagSection, page];
+      targetingPageId = page;
+      siteZone = siteZone.concat('home', '/', doubleclickPageTypeTagSection);
       break;
     case 'genrePage':
-      siteZone = siteZone.concat('categories','/',doubleclickPageTypeTagSection);
+      targetingTags = [doubleclickPageTypeTagArticle, `${pageName}`];
+      targetingPageId = pageName;
+      siteZone = siteZone.concat(pageName, '/article');
       break;
     case 'tagPage':
-      siteZone = siteZone.concat('tags','/',doubleclickPageTypeTagSection);
+      targetingTags = [doubleclickPageTypeTagTag, doubleclickPageTypeTagSection, `${pageName}`];
+      targetingPageId = doubleclickPageTypeTagTag + '_' + pageName;
+      siteZone = siteZone.concat('tags', '/', pageName, '/', doubleclickPageTypeTagSection);
       break;
     default:
   }
-  googletag.cmd.push(function () {
+
+  googletag.cmd.push(async function () {
     for (let ad of adSlots) {
-      let adSize = ad.getAttribute('data-adSize'),
+      let marketName = await targetingMarket.getName(),
+        adSize = ad.getAttribute('data-adSize'),
         sizeMapping = adMapping.sizeMapping[adSize],
         slot = googletag.defineSlot(
           siteZone,
@@ -79,7 +117,17 @@ function setAds() {
           .defineSizeMapping(sizeMapping)
           .addService(googletag.pubads())
           .setCollapseEmptyDiv(true)
-          .setTargeting('refresh', refreshCount.toString());
+          .setTargeting('refresh', refreshCount.toString())
+          .setTargeting('market', marketName.replace(' ','').split(',')[0].toLowerCase())
+          .setTargeting('station', targetingNationalRadioStation)
+          .setTargeting('genre', targetingGenre)
+          .setTargeting('cat', targetingCategory)
+          .setTargeting('tag', targetingTags)
+          .setTargeting('pid', targetingPageId);
+
+      if (targetingAuthors.length) {
+        slot.setTargeting('author', targetingAuthors);
+      }
 
       googleDefinedSlots.push(slot);
       googletag.display(ad.id);
