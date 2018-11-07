03998661102bb1f8f38e54f0b5ddc2c8dbbdaa6e
Brien Hodges <brien@swarmsolutions.com>
2018-11-07 08:43:04 -0500

    ON-75 | brien | cleaned up the model

--- app/components/latest-content/model.js
+++ app/components/latest-content/model.js
@@ -62,8 +62,7 @@ module.exports.render = async function (ref, data, locals) {
       query = queryService.newQueryWithCount(elasticIndex, maxItems, locals),
       cleanUrl = locals.url.split('?')[0].replace('https://', 'http://');
 
-    // TODO add this back btu ask what locals.site is
-    // queryService.withinThisSiteAndCrossposts(query, locals.site);
+    queryService.withinThisSiteAndCrossposts(query, locals.site);
     queryService.onlyWithTheseFields(query, elasticFields);
     queryService.addMinimumShould(query, 1);
     queryService.addSort(query, {date: 'desc'});
@@ -85,7 +84,6 @@ module.exports.render = async function (ref, data, locals) {
       const results = await queryService.searchByQuery(query),
         // combine the curated articles (entertainmentItems, newsItems, sportsItems, etc.) with the query results
         articles = items.concat(_.take(results, maxItems)).slice(0, maxItems); // show a maximum of maxItems links
-      // data.articles = [{ entertainment: [...articles]}, {news: [...articles] }, ....]
 
       data.articles.push({ section, articles });
     } catch (e) {
