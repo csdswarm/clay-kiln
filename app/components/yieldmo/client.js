19f1d1f737bf65fbdad36aa4de12fa785623b097
Nick Bryant <sbryant31@gmail.com>
2018-11-13 10:11:27 -0500

    ON-208 yieldmo

---
+++ app/components/yieldmo/client.js
@@ -0,0 +1,1 @@
+!function(e,t){if(void 0===t._ym){var a=Math.round(5*Math.random()/3)+'';t._ym='';var m=e.createElement('script');m.type='text/javascript',m.async=!0,m.src='//static.yieldmo.com/ym.'+a+'.js',(e.getElementsByTagName('head')[0]||e.getElementsByTagName('body')[0]).appendChild(m)}else t._ym instanceof String||void 0===t._ym.chkPls||t._ym.chkPls()}(document,window);
