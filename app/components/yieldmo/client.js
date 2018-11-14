a0ea2c7f9d85405867e6d892ef9e4053c033d651
Nick Bryant <sbryant31@gmail.com>
2018-11-14 17:12:45 -0500

    ON-208-yieldmo feedback from markham

--- app/components/yieldmo/client.js
+++ app/components/yieldmo/client.js
@@ -6,15 +6,21 @@
  */
 function injectYieldmo() {
   const socialBar = document.getElementsByClassName('info--right-bottom')[0],
-    yieldmo1 = document.getElementById('ym_2071731061939406471'),
+    yieldmo1 = document.getElementsByClassName('placement-1')[0],
     tagsSection = document.getElementsByClassName('component--tags')[0],
-    yieldmo2 = document.getElementById('ym_2071731315686409865');
+    yieldmo2 = document.getElementsByClassName('placement-2')[0];
 
   socialBar.appendChild(yieldmo1);
   tagsSection.after(yieldmo2);
 
-  // a bundle i got from yieldmo, doesn't pass lint but nothing I can do about it.
-  !function(e,t){if(void 0===t._ym){var a=Math.round(5*Math.random()/3)+'';t._ym='';var m=e.createElement('script');m.type='text/javascript',m.async=!0,m.src='//static.yieldmo.com/ym.'+a+'.js',(e.getElementsByTagName('head')[0]||e.getElementsByTagName('body')[0]).appendChild(m)}else t._ym instanceof String||void 0===t._ym.chkPls||t._ym.chkPls()}(document,window);
+  // if the yieldmo script has already been loaded, request a spa pageview, else initialize it.
+  if (window._ym) {
+    window._ym.requestPageView([yieldmo1.id, yieldmo2.id]);
+  } else {
+    /* eslint-disable */
+    !function(e,t){if(void 0===t._ym){var a=Math.round(5*Math.random()/3)+'';t._ym='';var m=e.createElement('script');m.type='text/javascript',m.async=!0,m.src='//static.yieldmo.com/ym.'+a+'.js',(e.getElementsByTagName('head')[0]||e.getElementsByTagName('body')[0]).appendChild(m)}else t._ym instanceof String||void 0===t._ym.chkPls||t._ym.chkPls()}(document,window);
+    /* eslint-enable */
+  }
 }
 
 document.addEventListener('yieldmo-mount', injectYieldmo);
