c00ce8bd90d29da2538f4c1e07a167fd1c571554
Reid Mayo <reid@swarmsolutions.com>
2018-11-28 17:16:09 -0500

    ON-238 Setting Vuex store base state BEFORE injecting store into vue and instantiating SPA instead of AFTER instantiating and mounting SPA.

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -6,28 +6,8 @@
 
 <script>
 
-import * as mutationTypes from '@/vuex/mutationTypes'
-import { handlebars } from '../config/initHandlebars'
-import { Base64 } from 'js-base64'
-
 export default {
   name: 'App',
-  created: function () {
-    // Load SPA Payload
-    if (window.spaPayload) {
-      // Decode payload from base64 into JSON and parse for loading into store.
-      const jsonPayload = Base64.decode(window.spaPayload)
-      const payload = JSON.parse(jsonPayload)
-
-      this.$store.commit(mutationTypes.LOAD_SPA_PAYLOAD_LOCALS, payload.locals)
-      this.$store.commit(mutationTypes.LOAD_SPA_PAYLOAD, payload)
-    } else {
-      throw new Error('SPA Payload failed to load.')
-    }
-
-    // Init and load Handlebars instance
-    this.$store.commit(mutationTypes.LOAD_HANDLEBARS, { handlebars })
-  },
   computed: {},
   methods: {},
   components: {}
--- spa/src/store.js
+++ spa/src/store.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as mutationTypes from './vuex/mutationTypes'
+import { handlebars } from '../config/initHandlebars'
+import { Base64 } from 'js-base64'
 
 Vue.use(Vuex)
 
@@ -29,3 +31,36 @@ export default new Vuex.Store({
 
   }
 })
+
+/**
+ *
+ * This sets the initial state of the vuex store before the
+ * store is injected into a new Vue SPA instance.
+ *
+ * Content related data is pulled of of window.spaPayload, which is
+ * supplied by the SSR HTML.
+ *
+ * @param {object} store - A fresh vuex store instance
+ */
+export function setVuexStoreBaseState (store) {
+  let spaPayload = null
+
+  // Load SPA Payload from window.
+  if (window.spaPayload) {
+    // Decode payload from base64 into JSON and parse for loading into store.
+    const jsonPayload = Base64.decode(window.spaPayload)
+    spaPayload = JSON.parse(jsonPayload)
+  } else {
+    throw new Error('SPA Payload failed to load.')
+  }
+
+  // Set base state data.
+  const baseState = {
+    handlebars: handlebars,
+    spaPayload: spaPayload,
+    spaPayloadLocals: JSON.parse(JSON.stringify(spaPayload.locals)),
+    setupRan: false
+  }
+
+  store.replaceState(baseState)
+}
