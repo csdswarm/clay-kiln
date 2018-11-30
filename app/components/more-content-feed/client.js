2e97c8fd2b154c82816a6cd0eae685a7e6d1abd7
Markham F Rollins IV <markham@swarmsolutions.com>
2018-11-30 16:29:44 -0500

    ON-237: Always ensure that .html is on the url

--- app/components/more-content-feed/client.js
+++ app/components/more-content-feed/client.js
@@ -4,7 +4,7 @@ class MoreContentFeed {
   constructor(el) {
     this.moreContentFeed = el;
     this.loadMore = el.querySelector('.links__link--loadmore');
-    this.moreContentUrl = '//' + this.moreContentFeed.getAttribute('data-uri').replace('@published', '.html');
+    this.moreContentUrl = '//' + this.moreContentFeed.getAttribute('data-uri').replace('@published', '') + '.html';
 
     this.currentPage = 1;
     this.tag = '';
@@ -45,6 +45,7 @@ class MoreContentFeed {
 
         // Recreate the listener for the new button
         this.loadMore = this.moreContentFeed.querySelector('.links__link--loadmore');
+        console.log(this.loadMore);
         this.loadMore.onclick = this.handleLoadMoreContent.bind(this);
       });
   }
