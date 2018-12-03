bcfe68a34c6a2151ea5eddffcb04d7c813e36e33
Tiffany Chow <tiffachow@gmail.com>
2018-12-03 13:51:28 -0500

    ON-312 Yieldmo -- Fix location; Remove second instance

--- app/components/yieldmo/client.js
+++ app/components/yieldmo/client.js
@@ -5,19 +5,22 @@
  * Inject those placements into the social bar and tags sections of the layout.
  */
 function injectYieldmo() {
-  const socialBar = document.getElementsByClassName('info--right-bottom')[0],
-    yieldmo1 = document.getElementsByClassName('placement-1')[0],
+  const yieldmo1 = document.getElementsByClassName('placement-1')[0]
     bodyContent = document.getElementsByClassName('body__content')[0],
-    yieldmo2 = document.getElementsByClassName('placement-2')[0];
+    firstParagraph = bodyContent.querySelector('p'),
+    secondParagraph = bodyContent.querySelector('p:nth-of-type(2)');
 
-  socialBar.appendChild(yieldmo1);
   if (bodyContent) {
-    bodyContent.appendChild(yieldmo2);
+    if (secondParagraph) {
+      secondParagraph.insertAdjacentElement('afterend', yieldmo1);
+    } else if (firstParagraph) {
+      firstParagraph.insertAdjacentElement('afterend', yieldmo1);
+    }
   }
 
   // if the yieldmo script has already been loaded, request a spa pageview, else initialize it.
   if (window._ym) {
-    window._ym.requestPageView([yieldmo1.id, yieldmo2.id]);
+    window._ym.requestPageView([yieldmo1.id]);
   } else {
     /* eslint-disable */
     !function(e,t){if(void 0===t._ym){var a=Math.round(5*Math.random()/3)+'';t._ym='';var m=e.createElement('script');m.type='text/javascript',m.async=!0,m.src='//static.yieldmo.com/ym.'+a+'.js',(e.getElementsByTagName('head')[0]||e.getElementsByTagName('body')[0]).appendChild(m)}else t._ym instanceof String||void 0===t._ym.chkPls||t._ym.chkPls()}(document,window);
