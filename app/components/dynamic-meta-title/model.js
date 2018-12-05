a41cec5870001684dc761f8e80c4ed91fb162a42
Reid Mayo <reid@swarmsolutions.com>
2018-12-05 12:16:02 -0500

    ON-342 Added dynamic meta tags on dynamic pages for SSR html.

--- app/components/dynamic-meta-title/model.js
+++ app/components/dynamic-meta-title/model.js
@@ -3,6 +3,12 @@
 module.exports.render = (ref, data, locals) => {
   if (data.routeParam && locals && locals.params) {
     data.paramValue = locals.params[data.routeParam];
+
+    // Set first character to upper case.
+    if (data.paramValue && data.paramValue.length) {
+      data.paramValue = data.paramValue.replace(/\b\w/g, l => l.toUpperCase());
+    }
+
   }
 
   return data;
