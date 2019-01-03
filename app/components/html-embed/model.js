548c4c52fa32517b62b8010e4e2c18804435051d
mrollinsiv-swarm <markham@swarmsolutions.com>
2019-01-03 14:27:35 -0500

    ON-420: ESLint fixes

--- app/components/html-embed/model.js
+++ app/components/html-embed/model.js
@@ -6,7 +6,7 @@
  * @param {object} locals
  * @returns {Promise}
  */
-module.exports.render = function (ref, data, locals) {
+module.exports.render = function (ref, data, locals) { // eslint-disable-line no-unused-vars
   data.isIframe = data.text.indexOf('<iframe') !== -1;
 
   return data;
