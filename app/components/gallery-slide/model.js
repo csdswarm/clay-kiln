d5ce8d3cd0183af1007066517f730ed16ffe9916
Reid Mayo <reid@swarmsolutions.com>
2019-01-15 17:59:00 -0500

    ON-165 Completed slide deep hash link ID.

--- app/components/gallery-slide/model.js
+++ app/components/gallery-slide/model.js
@@ -1,22 +1,28 @@
 'use strict';
 
-const utils = require('../../services/universal/utils');
+const utils = require('../../services/universal/utils'),
+  { getComponentInstance } = require('clayutils');
 
 module.exports.save = (uri, data) => {
 
   // Process hashLinkSuffix property.
   if (utils.isFieldEmpty(data.hashLinkSuffix)) {
-
     // If hashLinkSuffix not set, default hash link to lowercase alphanumeric hyphenated spaces version of title string.
     data.hashLinkSuffix = data.title.replace(/\W/g, '-').replace(/[^0-9a-z-]/gi, '').toLowerCase();
-
-    return data;
   } else {
-
     // Sanitize hashLinkSuffix as url safe lowercase alphanumeric hyphenated spaces
     data.hashLinkSuffix = data.hashLinkSuffix.replace(/\W/g, '-').replace(/[^0-9a-z-]/gi, '').toLowerCase();
-
-    return data;
   }
+
+  return data;
+  
+};
+
+module.exports.render = (ref, data, locals) => {
+
+  // Create slide direct hash link by appending hashLinkSuffix to the slide instance cuid.
+  data.hashLink = `Slide-${getComponentInstance(ref)}-${data.hashLinkSuffix}`;
+
+  return data;
   
 };
