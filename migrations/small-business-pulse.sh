3fc7e51fb217351d2eccdb79dec2c383b2185cbc
Brien Hodges <brien@swarmsolutions.com>
2019-01-22 11:44:30 -0500

    ON-428 | brien | finally understood how the pagetitle fits and what needed to be done to make it work

--- migrations/small-business-pulse.sh
+++ migrations/small-business-pulse.sh
@@ -9,9 +9,8 @@ if [ "$#" -ne 2 ]; then
 else
     [[ $1 = "clay" ]] && http="http" || http="https"
     echo "Using $http://$1.radio.com"
-#    if [[ $(curl "$http://$1.radio.com/_components/section-front/instances" 2>&1) == *"small-business-pulse"* ]];
-#    then echo "Small Business Pulse page already exists";
-#    else
-cat "$(dirname "$0")/small-business-pulse.json" | clay import -k $2 --publish "$1.radio.com";
-#   fi
+    if [[ $(curl "$http://$1.radio.com/_components/section-front/instances" 2>&1) == *"small-business-pulse"* ]];
+    then echo "Small Business Pulse page already exists";
+    else cat "$(dirname "$0")/small-business-pulse.json" | clay import -k $2 --publish "$1.radio.com";
+   fi
 fi
