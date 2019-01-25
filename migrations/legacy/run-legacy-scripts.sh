f6c7f59eae90722f0ee47072e9a0a5f0526be910
mrollinsiv-swarm <markham@swarmsolutions.com>
2019-01-25 17:27:33 -0500

    ON-472: Create a simple (ugly) script to automatically run legacy migrations after make bootstrap is run. It is not set up for dev/stg/prd because they should be run manually during launch by the developer.

---
+++ migrations/legacy/run-legacy-scripts.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+mydir="$(dirname "${0}")"
+
+## Legacy scripts should be listed here to run in order they are intended to
+
+if [ "$1" != "" ]; then
+  if [ "$1" == "clay.radio.com" ]; then
+    es="$1" && http="http";
+  elif [ "$1" == "dev-clay.radio.com" ]; then
+    es="http://dev-es.radio-dev.com" && http="https";
+  elif [ "$1" == "stg-clay.radio.com" ]; then
+    es="http://es.radio-stg.com" && http="https";
+  elif [ "$1" == "radio.com" ]; then
+    es="http://es.radio-prd.com" && http="https";
+  fi
+  printf "Updating environment $http://$1\n"
+else
+  set "clay.radio.com" && http="http" && es="$1";
+  printf "No environment specified. Updating environment $http://$1\n"
+fi
+
+# List your script and any params it needs to run
+
+$mydir/7x1-ad-unit/migration.sh $1
