bfbcdc8c7c6460d7d68e1617c5da01b32e675bdb
Nick Bryant <sbryant31@gmail.com>
2019-01-29 12:35:39 -0500

    ON-367 remove render - useless

--- app/components/meta-url/model.js
+++ app/components/meta-url/model.js
@@ -1,12 +1,6 @@
 'use strict';
 const helpers = require('./helpers');
 
-module.exports.render = (ref, data, locals) => {
-  helpers.fixSyndicatedUrl(data);
-  helpers.fixHttpUrl(data);
-  return data;
-};
-
 module.exports.save = (ref, data, locals) => {
   helpers.setFromLocals(data, locals);
   helpers.fixHttpUrl(data);
