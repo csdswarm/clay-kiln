9ed0d0aae960db802d374a9df1dd06aafd28aac6
mrollinsiv-swarm <markham@swarmsolutions.com>
2019-01-29 17:51:41 -0500

    ON-428: Modified files committed due to being missed in last PR

--- migrations/legacy/20190128120000-SBP/SBP.sh
+++ migrations/legacy/20190128120000-SBP/SBP.sh
@@ -1,11 +1,27 @@
 #! /bin/bash
 
+if [ "$1" != "" ]; then
+  if [ "$1" == "clay.radio.com" ]; then
+    es="$1" && http="http";
+  elif [ "$1" == "dev-clay.radio.com" ]; then
+    es="http://dev-es.radio-dev.com" && http="https";
+  elif [ "$1" == "stg-clay.radio.com" ]; then
+    es="http://es.radio-stg.com" && http="https";
+  elif [ "$1" == "www.radio.com" ]; then
+    es="http://es.radio-prd.com" && http="https";
+  fi
+  printf "Updating environment $http://$1\n"
+else
+  set "clay.radio.com" && http="http" && es="$1";
+  printf "No environment specified. Updating environment $http://$1\n"
+fi
+
 printf "\nUpdating Section Fronts for Small Business Pulse...\n\n\n"
 
 printf "Updating page...\n"
-cd ./migrations/legacy/SBP && cat ./_pages.yml | clay import -k demo -y radio.com
+cat ./_pages.yml | clay import -k demo -y $1
 
 printf "\n\nUpdating component instance...\n\n"
-curl -X PUT https://radio.com/_components/section-lead/instances/new -H 'Authorization: token accesskey' -H 'Content-Type: application/json'
+curl -X PUT $http://$1/_components/section-lead/instances/new -H 'Authorization: token accesskey' -H 'Content-Type: application/json'
 
 printf "\n\n\n\n"
