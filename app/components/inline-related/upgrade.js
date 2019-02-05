440b91cc8b17dced889e513f8035191e7dd36384
mrollinsiv-swarm <markham@swarmsolutions.com>
2019-02-05 12:49:48 -0500

    ON-115: Duplicate semicolon

--- app/components/inline-related/upgrade.js
+++ app/components/inline-related/upgrade.js
@@ -5,7 +5,7 @@ module.exports['1.0'] = function (uri, data) {
   let newData = Object.assign({}, data);
 
   // Replace articleType with sectionFront
-  newData.sectionFront = data.sectionFront || data.articleType;;
+  newData.sectionFront = data.sectionFront || data.articleType;
   delete newData.articleType;
 
   return newData;
