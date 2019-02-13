f01deeeb86e9f7ab817f42f77536cb9ae80c29d9
mrollinsiv-swarm <markham@swarmsolutions.com>
2019-02-13 10:46:02 -0500

    ON-474: BC updates for autoplay * Scrolling out of view will not stop the player * Playing a new BC video while another plays will pause it * Lede's will autoplay muted on desktop

--- app/components/brightcove/client.js
+++ app/components/brightcove/client.js
@@ -1,14 +1,60 @@
 'use strict';
 
-let videos = [];
+// Polyfill
+require('intersection-observer');
+const videoObserver = new IntersectionObserver(videoIsInView, {}),
+  { isMobileWidth } = require('../../services/client/mobile');
+
+let activePlayers = [];
 
 // Listener for dismount to delete videos
 document.addEventListener('brightcove-dismount', () => {
-  videos = [];
+  activePlayers = [];
 });
 
-function Constructor(el) {
-  let videoPlayer = el.querySelector('video-js'),
+/**
+ * Check if the video has gone out of view
+ *
+ * @param changes
+ */
+function videoIsInView(changes) {
+  changes.forEach(change => {
+    if (change.intersectionRatio === 0) {
+      pausePlayer(videojs.getPlayer(change.target.getAttribute('id')));
+    }
+  });
+};
+
+/**
+ * Pause the player
+ *
+ * @param player
+ */
+function pausePlayer(player) {
+  if (typeof player.ima3.adsManager !== 'undefined') {
+    player.ima3.adsManager.pause();
+  }
+  player.pause();
+}
+
+/**
+ * Loop over all players on page and pause them if it's not the video in question
+ *
+ * @param player
+ */
+function pauseOtherActivePlayers() {
+  let player = this;
+
+  // Loop over all players on the current page
+  activePlayers.forEach((playerOnPage) => {
+    if (player.id() !== playerOnPage.id()) {
+      pausePlayer(playerOnPage);
+    }
+  });
+}
+
+function Constructor(brightcoveComponent) {
+  let videoPlayer = brightcoveComponent.querySelector('video-js'),
     id = videoPlayer.getAttribute('id'),
     brightcoveAccount = videoPlayer.getAttribute('data-account'),
     brightcovePlayerId = videoPlayer.getAttribute('data-player');
@@ -19,15 +65,26 @@ function Constructor(el) {
   // Add the script tag to the document
   document.body.appendChild(s);
   // Call a function to play the video once player's JavaScript loaded
-  s.onload = function () {
+  s.onload = () => {
     const player = bc(id);
 
-    // autoplay muted
-    player.on('loadedmetadata', () => {
-      player.muted(true);
-      player.play();
-    });
-    videos.push(player);
+    // Keep track of all videos on page
+    activePlayers.push(player);
+
+    // Observe video for if it goes out of view
+    videoObserver.observe(player.el(), {threshold: 0});
+
+    // When a video begins playing trigger a stop on all others on page (must track video and ad events)
+    player.on('play', pauseOtherActivePlayers);
+    player.on('ads-play', pauseOtherActivePlayers);
+
+    if (brightcoveComponent.closest('.body__header .lead') && !isMobileWidth()) {
+      // autoplay muted
+      player.on('loadedmetadata', () => {
+        player.muted(true);
+        player.play();
+      });
+    }
   };
 }
 
