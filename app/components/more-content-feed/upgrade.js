f80a15773271b1ce7266071da5b147de89684a13
mrollinsiv-swarm <markham@swarmsolutions.com>
2019-02-14 09:45:24 -0500

    ON-423: Update section-lead and more-content-feed to have checkboxes for filterSecondaryArticleType and fix bootstrap files.

--- app/components/more-content-feed/upgrade.js
+++ app/components/more-content-feed/upgrade.js
@@ -7,3 +7,18 @@ module.exports['1.0'] = function (uri, data) {
 
   return data;
 };
+
+module.exports['2.0'] = function (uri, data) {
+  // Clone so we don't lose value by reference
+  let newData = Object.assign({}, data);
+
+  newData.filterSecondaryArticleTypes = {};
+
+  // Replace articleType with sectionFront
+  if (data.filterSecondaryArticleType) {
+    newData.filterSecondaryArticleTypes[data.filterSecondaryArticleType] = true;
+  }
+  delete newData.filterSecondaryArticleType;
+
+  return newData;
+};
