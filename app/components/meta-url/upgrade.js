7f0c73593f64fda66adbcd1d4a52bc7eea178f6a
Brien Hodges <brien@swarmsolutions.com>
2019-02-20 09:57:33 -0500

    NOJIRA | brien | fix lint errors that made it to master

--- app/components/meta-url/upgrade.js
+++ app/components/meta-url/upgrade.js
@@ -1,9 +1,9 @@
 'use strict';
-const helpers = require('./helpers')
+const helpers = require('./helpers');
 
-module.exports['1.0'] = function (uri, data, locals) { // eslint-disable-line no-unused-vars
-  helpers.fixHttpUrl(data)
-  helpers.fixSyndicatedUrl(data)
+module.exports['1.0'] = function (uri, data) {
+  helpers.fixHttpUrl(data);
+  helpers.fixSyndicatedUrl(data);
 
   return data;
 };
