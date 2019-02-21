3921565e20875a896f8aae6d944dbd853be5f103
Nick Bryant <sbryant31@gmail.com>
2019-02-21 15:24:49 -0500

    ON-491 lint

--- app/components/tweet/model.js
+++ app/components/tweet/model.js
@@ -9,28 +9,23 @@ function getRequestUrl(data) {
   const hideMedia = data.showMedia === false ? '&hide_media=true' : '',
     hideThread = data.showThread === false ? '&hide_thread=true' : '';
 
-  console.log("got request url")
   return TWITTER_ENDPOINT + `?url=${encodeURI(data.url)}&omit_script=true${hideThread}${hideMedia}`;
 }
 
 module.exports.save = async (ref, data) => {
-
-  console.log('save 1')
   if (utils.isFieldEmpty(data.url)) {
     delete data.html;
     return data;
   }
 
-  console.log('save 2')
   // first, wrangle the url
   data.url = data.url.match(/(https?:\/\/twitter\.com\/\w+?\/status\/\d+)\/?/)[1];
 
   try {
     // note: we're using the un-authenticated api endpoint. don't abuse this
-    const res = await rest.getJSONP(getRequestUrl(data))
-    console.log('save 3')
+    const res = await rest.getJSONP(getRequestUrl(data));
+
     if (_get(res, 'errors.length')) {
-      console.log('error', res.errors)
       data.error = true;
       data.html = '';
       return data;
@@ -39,8 +34,7 @@ module.exports.save = async (ref, data) => {
     data.html = res.html;
     // update component instance with new html
     return data;
-  } catch(e) {
-    console.log('error2', e)
+  } catch (e) {
     if (utils.isFieldEmpty(data.html)) {
       // if we've never grabbed html for this tweet and we can't fetch it from the api, throw an error
       data.error = true;
