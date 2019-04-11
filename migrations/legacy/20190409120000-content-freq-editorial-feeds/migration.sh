f812b245199397dab9b39c843da20226612f0f60
Tiffany Chow <tiffachow@gmail.com>
2019-04-11 13:03:44 -0400

    ON-617 Content Editorial Feeds -- Update migration script to create new index with new fields

--- migrations/legacy/20190409120000-content-freq-editorial-feeds/migration.sh
+++ migrations/legacy/20190409120000-content-freq-editorial-feeds/migration.sh
@@ -20,3 +20,44 @@ printf "\n\nCreating Frequency Editorial Feeds List...\n\n\n"
 cat ./_lists.yml | clay import -k demo -y $1
 
 printf "\n\n\n\n"
+
+# Updating existing field mappings
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html#_updating_existing_field_mappings
+
+newProps="\"featured\": {\"type\": \"boolean\"}, \"editorialFeeds\": {\"type\": \"object\", \"dynamic\": \"true\"}";
+
+index=$(curl -X GET "$es:9200/_cat/indices?pretty&s=index:desc" 2>/dev/null | sed -n 's/.*\(published-content[^ \n]*\).*/\1/p' | sed q);
+num=$(echo $index | sed 's/[^0-9]*//g');
+newIndex="$(echo $index | sed 's/[0-9]*//g')$((num+1))";
+alias=$(echo $index | sed 's/\(.*\)_.*/\1/g');
+mappings=$(curl -X GET "$es:9200/$index/_mappings" 2>/dev/null | sed -n "s/\"properties\":{/\"properties\":{$newProps,/p" | sed -n "s/{\"$index\":{\(.*\)}}/\1/p");
+
+printf "\n\nCreating new index ($newIndex)...\n\n"
+curl -X GET "$es:9200/$index/_settings" > ./settings.json;
+node ./settings-update.js "$1";
+settings=$(cat ./settings.txt);
+curl -X PUT "$es:9200/$newIndex" -H 'Content-Type: application/json' -d "{$settings,$mappings}";
+rm ./settings.json;
+rm ./settings.txt;
+
+printf "\r\n\r\nCopying old index data ($index) to new index ($newIndex)...\n\n"
+curl -X POST "$es:9200/_reindex" -H 'Content-Type: application/json' -d "
+{
+  \"source\": {
+    \"index\": \"$index\"
+  },
+  \"dest\": {
+    \"index\": \"$newIndex\"
+  }
+}";
+
+sleep 1;
+
+printf "\n\nRemoving old alias and adding new ($alias)...\n\n"
+curl -X POST "$es:9200/_aliases" -H 'Content-Type: application/json' -d "
+{
+    \"actions\" : [
+        { \"remove\" : { \"index\" : \"$index\", \"alias\" : \"$alias\" } },
+        { \"add\" : { \"index\" : \"$newIndex\", \"alias\" : \"$alias\" } }
+    ]
+}";
