867c3c3fd0694e1e2576d83f809ae2b1d3640368
Brien Hodges <brien@swarmsolutions.com>
2019-04-12 14:53:06 -0400

    ON-566 | brien | fix routing after modal closes and added example of returning current local data from model

--- app/components/top-nav/model.js
+++ app/components/top-nav/model.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports.render = (ref, data, locals) => {
+  data.radiumUser = locals.radiumUser
+
   data.headerLinks = data.headerLinks.map((headerLink) => {
     headerLink.current = locals.url.includes(headerLink.url);
     return headerLink;
