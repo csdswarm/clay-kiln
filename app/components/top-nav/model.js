d3066f22bbf10f22f5213fd63ba9c5973a2fc03e
Tiffany Chow <tiffachow@gmail.com>
2019-04-15 18:02:28 -0400

    ON-655 Header external links -- open in new tab

--- app/components/top-nav/model.js
+++ app/components/top-nav/model.js
@@ -1,6 +1,28 @@
 'use strict';
 
+const externalLinkReg = /player.radio.com|\/audio/;
+
+/**
+ * Determine whether links are external -- recursive for lists within lists
+ * @function setExternalLinks
+ * @param {Object[]} linkList - Array of objects containing link data
+ */
+function setExternalLinks(linkList) {
+  return linkList.map((link) => {
+    if (link.url) {
+      link.externalLink = externalLinkReg.test(link.url);
+    }
+
+    if (link.list) {
+      link.list = setExternalLinks(link.list); //recursive
+    }
+
+    return link;
+  });
+}
+
 module.exports.render = (ref, data, locals) => {
+  setExternalLinks(data.headerLinks);
   data.headerLinks = data.headerLinks.map((headerLink) => {
     headerLink.current = locals.url.includes(headerLink.url);
     return headerLink;
