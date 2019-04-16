b15a77c2cbdef212b36ff9eb1d23055a78a203a7
Tiffany Chow <tiffachow@gmail.com>
2019-04-16 18:51:21 -0400

    ON-297 Global Sponsor Logos -- Fix eslint error

--- app/components/google-ad-manager/model.js
+++ app/components/google-ad-manager/model.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports.render = function (ref, data, locals) {
+module.exports.render = function (ref, data) {
   return {...data, doubleclickBannerTag: process.env.DOUBLECLICK_BANNER_TAG};
 };
