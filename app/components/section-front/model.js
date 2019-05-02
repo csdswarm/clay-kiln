748b478d192a98c088bd80e6eecdaec392417f84
Tiffany Chow <tiffachow@gmail.com>
2019-05-02 18:22:18 -0400

    ON-681 Init commit -- Add primary section fronts list, change section front title field to text input

--- app/components/section-front/model.js
+++ app/components/section-front/model.js
@@ -1,8 +1,22 @@
 'use strict';
 
+const rest = require('../../services/universal/rest');
+
 module.exports.render = (uri, data, locals) => {
   if (data.title) {
     locals.sectionFront = data.title;
+  }
+  return data;
+};
+
+module.exports.save = async (uri, data, locals) => {
+  if (data.title) {
+    /* @todo:
+    /* fetch current data from https://www.radio.com/_lists/primary-section-fronts
+    /* on publish add to lists, on unpublish remove */
+    await rest.get(`${locals.site.protocol}://${locals.site.host}/_lists/primary-section-fronts`).then((primarySectionFronts)=>{
+
+    })
   }
   return data;
 };
