a5aef1df8b177b332f1fa233135454c85f16fb5e
Jeffrey Pope <jeffreypope@Jeffreys-MacBook-Pro.local>
2019-05-07 16:17:32 -0400

    ON-543: add comment to template

--- app/components/meta-tags/model.js
+++ app/components/meta-tags/model.js
@@ -5,27 +5,25 @@ const _each = require('lodash/each');
 module.exports.render = (ref, data, locals) => {
   data.metaTags = [];
 
-  if (data.contentType) data.metaTags.push({ property: 'og:type', value: data.contentType });
+  if (data.contentType) data.metaTags.push({ property: 'og:type', content: data.contentType });
   if (data.authors.length > 0) {
     _each(data.authors, author => {
-      data.metaTags.push({ property: 'article:author:name', value: author.text });
+      data.metaTags.push({ property: 'article:author:name', content: author.text });
     });
   }
-  if (data.publishDate) data.metaTags.push({ property: 'article:published_time', value: data.publishDate });
-  if (data.sectionFront) data.metaTags.push({ name: 'cXenseParse:recs:category', value: data.sectionFront });
-  if (data.secondaryArticleType) data.metaTags.push({ name: 'cXenseParse:recs:category', value: data.secondaryArticleType });
+  if (data.publishDate) data.metaTags.push({ property: 'article:published_time', content: data.publishDate });
+  if (data.sectionFront) data.metaTags.push({ name: 'cXenseParse:recs:category', content: data.sectionFront });
+  if (data.secondaryArticleType) data.metaTags.push({ name: 'cXenseParse:recs:category', content: data.secondaryArticleType });
 
   if (locals.station) {
-    if (locals.station.category) data.metaTags.push({ name: 'cXenseParse:recs:category', value: locals.station.category });
-    if (locals.station.callsign) data.metaTags.push({ name: 'station-call-letters', value: locals.station.callsign });
+    // toLowerCase to normalize category with sectionFront which is lowercase 
+    if (locals.station.category) data.metaTags.push({ name: 'cXenseParse:recs:category', content: locals.station.category.toLowerCase() });
+    if (locals.station.callsign) data.metaTags.push({ name: 'station-call-letters', content: locals.station.callsign });
   }
 
   // add editorial radio
   // add station call letters
     // only pass NATL-RC on radio.com pages?
-
-  console.log(data);
-  console.log(locals);
 
   return data;
 };
