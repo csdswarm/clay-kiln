affc67cf34b7f4c6e8ee74685a28f7f21d64a97c
Jeffrey Pope <jeffreypope@Jeffreys-MacBook-Pro.local>
2019-05-08 16:15:50 -0400

    ON-543: update SPA to update meta tags on page load

--- app/components/meta-tags/model.js
+++ app/components/meta-tags/model.js
@@ -1,39 +1,61 @@
 'use strict';
 
 module.exports.render = (ref, data, locals) => {
-  data.metaTags = [];
+  const OG_TYPE = 'og:type',
+    AUTHOR_NAME = 'article:author:name',
+    PUB_TIME = 'article:published_time',
+    CATEGORY = 'cXenseParse:recs:category',
+    STATION_CALL_LETTERS = 'station-call-letters',
+    categories = [];
 
-  if (data.contentType) data.metaTags.push({ property: 'og:type', content: data.contentType });
+  // save these for SPA to easily be able to create or delete tags without knowing property / names
+  // lets us only have to update meta-tags component when adding / removing meta tags in the future
+  data.metaTags = [];
+  data.unusedTags = [];
+
+  // add content type tag
+  if (data.contentType) {
+    data.metaTags.push({ property: OG_TYPE, content: data.contentType });
+  } else {
+    data.unusedTags.push({ type: 'property', property: OG_TYPE });
+  }
+
+  // add author tag
   if (data.authors.length > 0) {
     const authors = data.authors.map(a => a.text).join(', ');
 
-    data.metaTags.push({ property: 'article:author:name', content: authors });
+    data.metaTags.push({ property: AUTHOR_NAME, content: authors });
+  } else {
+    data.unusedTags.push({ type: 'property', property: AUTHOR_NAME });
   }
 
-  // this is already done in meta-url -- do we need it in both places?
+  // add pub date tag
   if (data.publishDate) {
-    data.metaTags.push({ property: 'article:published_time', content: data.publishDate });
+    data.metaTags.push({ property: PUB_TIME, content: data.publishDate });
   } else if (data.automatedPublishDate) {
-    data.metaTags.push({ property: 'article:published_time', content: data.automatedPublishDate });
+    data.metaTags.push({ property: PUB_TIME, content: data.automatedPublishDate });
+  } else {
+    data.unusedTags.push({ type: 'property', property: PUB_TIME });
   }
-
-  const categories = [];
 
   // normalizing categories to be lowercase (sectionFront is usally already lowercase)
   if (data.sectionFront) categories.push(data.sectionFront.toLowerCase());
   if (data.secondaryArticleType) categories.push(data.secondaryArticleType.toLowerCase());
+  if (locals.station && locals.station.category) categories.push(locals.station.category.toLowerCase());
   if (categories.length > 0) {
-    data.metaTags.push({ name: 'cXenseParse:recs:category', content: categories.join(', ') });
+    data.metaTags.push({ name: CATEGORY, content: categories.join(', ') });
+  } else {
+    data.unusedTags.push({ type: 'name', name: CATEGORY });
   }
 
-  if (locals.station) {
-    // toLowerCase to normalize category with sectionFront which is lowercase
-    if (locals.station.category) data.metaTags.push({ name: 'cXenseParse:recs:category', content: locals.station.category.toLowerCase() });
-    if (locals.station.callsign) data.metaTags.push({ name: 'station-call-letters', content: locals.station.callsign });
+  // add station call letters tag
+  if (locals.station && locals.station.callsign) {
+    data.metaTags.push({ name: STATION_CALL_LETTERS, content: locals.station.callsign });
+  } else {
+    data.unusedTags.push({ type: 'name', name: STATION_CALL_LETTERS });
   }
 
   // add editorial radio
-  // add station call letters
   // only pass NATL-RC on radio.com pages?
 
   return data;
