44aaf295411aedd9187d069dbbc6ff8db1c7a3cb
Brien Hodges <brien@swarmsolutions.com>
2019-05-14 10:54:36 -0400

    ON-542 | brien | migration to remove youtube-player-head

---
+++ migrations/legacy/20190509140600-universal-media/layouts-update.js
@@ -0,0 +1,45 @@
+const fs = require('fs'),
+  YAML = require('../../../app/node_modules/yamljs'),
+  host = process.argv.slice(2)[0],
+  componentType = process.argv.slice(2)[1],
+  instanceType = process.argv.slice(2)[2];
+
+if (!host) {
+  throw new Error('Missing host');
+}
+
+if (!componentType) {
+  throw new Error('Missing component type.')
+}
+
+if (!instanceType) {
+  throw new Error('Missing instance type.')
+}
+
+// Get current JSON
+let data = require(`${__dirname}/${componentType}-${instanceType}.json`)
+
+// Strip youtube-player-head component from headLayout.
+data.headLayout = data.headLayout.filter((item) => {
+  if (item._ref.includes('youtube-player-head')) {
+    return false;
+  } else {
+    return true;
+  }
+})
+
+// Create correct clay data structure
+const payload = {
+  '_components' : {
+    [componentType]: {
+      instances: {
+        [instanceType]: data
+      }
+    }
+  }
+};
+
+fs.writeFile(`${__dirname}/${componentType}-${instanceType}.yml`, YAML.stringify(payload, 6, 2), 'utf8', function(err) {
+    if (err) throw err;
+  }
+);
--- migrations/legacy/20190509140600-universal-media/migration.sh
+++ migrations/legacy/20190509140600-universal-media/migration.sh
@@ -16,13 +16,70 @@ else
   printf "No environment specified. Updating environment $http://$1\n"
 fi
 
-printf "Add branch text me the app integration to apps html-embed instance...\n"
-# _components/section-front/instances/sports
-componentType="html-embed"
-instanceType="static-apps"
+printf "Delete youtube-player-head component from layouts."
+
+# _components/one-column-layout/instances/general
+componentType="one-column-layout"
+instanceType="general"
 printf "\n\nUpdating component $componentType instance $instanceType...\n\n"
 curl -X GET -H "Accept: application/json" $http://$1/_components/$componentType/instances/$instanceType > ./$componentType-$instanceType.json
-node ./apps-instance-update.js "$1" "$componentType" "$instanceType";
+node ./layouts-update.js "$1" "$componentType" "$instanceType";
+cat ./$componentType-$instanceType.yml | clay import -k demo -y $1 -p
+rm ./$componentType-$instanceType.json
+rm ./$componentType-$instanceType.yml
+printf "\n\n\n\n"
+
+# _components/one-column-layout/instances/bare
+componentType="one-column-layout"
+instanceType="bare"
+printf "\n\nUpdating component $componentType instance $instanceType...\n\n"
+curl -X GET -H "Accept: application/json" $http://$1/_components/$componentType/instances/$instanceType > ./$componentType-$instanceType.json
+node ./layouts-update.js "$1" "$componentType" "$instanceType";
+cat ./$componentType-$instanceType.yml | clay import -k demo -y $1 -p
+rm ./$componentType-$instanceType.json
+rm ./$componentType-$instanceType.yml
+printf "\n\n\n\n"
+
+
+# _components/one-column-layout/instances/article
+componentType="one-column-layout"
+instanceType="article"
+printf "\n\nUpdating component $componentType instance $instanceType...\n\n"
+curl -X GET -H "Accept: application/json" $http://$1/_components/$componentType/instances/$instanceType > ./$componentType-$instanceType.json
+node ./layouts-update.js "$1" "$componentType" "$instanceType";
+cat ./$componentType-$instanceType.yml | clay import -k demo -y $1 -p
+rm ./$componentType-$instanceType.json
+rm ./$componentType-$instanceType.yml
+printf "\n\n\n\n"
+
+# _components/one-column-full-width-layout/instances/bare
+componentType="one-column-full-width-layout"
+instanceType="bare"
+printf "\n\nUpdating component $componentType instance $instanceType...\n\n"
+curl -X GET -H "Accept: application/json" $http://$1/_components/$componentType/instances/$instanceType > ./$componentType-$instanceType.json
+node ./layouts-update.js "$1" "$componentType" "$instanceType";
+cat ./$componentType-$instanceType.yml | clay import -k demo -y $1 -p
+rm ./$componentType-$instanceType.json
+rm ./$componentType-$instanceType.yml
+printf "\n\n\n\n"
+
+# _components/two-column-layout/instances/article
+componentType="two-column-layout"
+instanceType="article"
+printf "\n\nUpdating component $componentType instance $instanceType...\n\n"
+curl -X GET -H "Accept: application/json" $http://$1/_components/$componentType/instances/$instanceType > ./$componentType-$instanceType.json
+node ./layouts-update.js "$1" "$componentType" "$instanceType";
+cat ./$componentType-$instanceType.yml | clay import -k demo -y $1 -p
+rm ./$componentType-$instanceType.json
+rm ./$componentType-$instanceType.yml
+printf "\n\n\n\n"
+
+# _components/two-column-layout/instances/station
+componentType="two-column-layout"
+instanceType="station"
+printf "\n\nUpdating component $componentType instance $instanceType...\n\n"
+curl -X GET -H "Accept: application/json" $http://$1/_components/$componentType/instances/$instanceType > ./$componentType-$instanceType.json
+node ./layouts-update.js "$1" "$componentType" "$instanceType";
 cat ./$componentType-$instanceType.yml | clay import -k demo -y $1 -p
 rm ./$componentType-$instanceType.json
 rm ./$componentType-$instanceType.yml
