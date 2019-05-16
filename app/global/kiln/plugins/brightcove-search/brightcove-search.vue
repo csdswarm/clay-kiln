d2840366081b086f3cb2e110ebf165d9acd63717
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-05-16 11:19:33 -0500

    ON-651 setup brightcove-search component

---
+++ app/global/kiln/plugins/brightcove-search/brightcove-search.vue
@@ -0,0 +1,42 @@
+<!--  Brightcove Search -->
+<docs>
+    # Brightcove Search
+</docs>
+<template>
+    <div class="brightcove-search">
+            <ui-autocomplete
+                floating-label
+                label="Brightcove Search"
+                name="brightcove-search"
+                placeholder="Search"
+
+                :suggestions="searchResults"
+                @input="populateBrightcove"
+                v-model="selectedVideo"
+            ></ui-autocomplete>
+    </div>
+</template>
+<script>
+    const UiAutocomplete = window.kiln.utils.components.UiAutocomplete;
+    export default {
+        props: ['name', 'data', 'schema', 'args'],
+        data() {
+            return {
+                videoName: '',
+                searchResults: ['something', 'something else'],
+                selectedVideo: ''
+            };
+        },
+        methods: {
+            populateBrightcove() {
+                console.log('this is where I need to do the search')
+                console.log(this.selectedVideo);
+            }
+        },
+        components: {
+            UiAutocomplete
+        }
+    }
+</script>
+
+
