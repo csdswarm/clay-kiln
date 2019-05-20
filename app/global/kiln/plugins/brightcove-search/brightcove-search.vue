e70e5d491210ecfc143e13e8baf0832e47691533
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-05-20 15:16:58 -0500

    ON-651 connect to brightcove api

--- app/global/kiln/plugins/brightcove-search/brightcove-search.vue
+++ app/global/kiln/plugins/brightcove-search/brightcove-search.vue
@@ -4,6 +4,53 @@
 </docs>
 <template>
     <div class="brightcove-search">
+        <ui-textbox
+            floating-label
+            :label="args.webLabel"
+            name="brightcove-search"
+            placeholder="Search Text"
+            help="Use quotes to match whole phrases, and + to indicate required words"
+
+            @input="populateSearchResults"
+            v-model="query"
+        ></ui-textbox>
+        <div class="brightcove-search__date-range">
+            <div class="ui-textbox__label-text">Last Updated: </div>
+            <ui-datepicker
+                error="Start date must be before end date"
+                icon="events"
+                placeholder="Start"
+
+                :invalid="!validDateRange"
+
+                @input="populateSearchResults"
+                v-model="startDate"
+            ></ui-datepicker>
+            <ui-datepicker
+                icon="events"
+                placeholder="End"
+
+                :invalid="!validDateRange"
+
+                @input="populateSearchResults"
+                v-model="endDate"
+            ></ui-datepicker>
+            <ui-button
+                type="primary"
+                @click="clearDateRange"
+            >Clear</ui-button>
+        </div>
+        <div v-if="showResults" class="brightcove-search__search-results">
+            <ul v-if="!loading">
+                <li :key="result.id"
+                    class="brightcove-search__search-result"
+                    v-for="result in searchResults"
+                    @click="selectBrightcove(result)">
+                    {{ result.name }}
+                </li>
+            </ul>
+            <ui-progress-circular v-show="loading"></ui-progress-circular>
+        </div>
         <div v-if="video.id" class="brightcove-video-preview">
             <div class="brightcove-video-preview__info">
                 <strong>{{video.name}}</strong>
@@ -11,37 +58,44 @@
             </div>
             <img class="brightcove-video-preview__image" :src="video.imageUrl" />
         </div>
-        <ui-autocomplete
-            floating-label
-            :label="args.webLabel"
-            name="brightcove-search"
-            placeholder="Search"
-            :keys="keys"
-
-            :suggestions="searchResults"
-            @input="populateSearchResults"
-            @select="selectBrightcove"
-            v-model="query"
-        ></ui-autocomplete>
     </div>
 </template>
 <script>
 
     import axios from 'axios';
 
-    const UiAutocomplete = window.kiln.utils.components.UiAutocomplete;
+    const _debounce = require('lodash/debounce');
+    const UiButton = window.kiln.utils.components.UiButton;
+    const UiDatepicker = window.kiln.utils.components.UiDatepicker;
+    const UiProgressCircular = window.kiln.utils.components.UiProgressCircular;
+    const UiTextbox = window.kiln.utils.components.UiTextbox;
+
     export default {
         props: ['name', 'data', 'schema', 'args'],
         data() {
             return {
                 video: {id: this.data},
                 searchResults: [],
-                keys: {
-                  label: 'name',
-                  value: 'id'
-                },
-                query: ''
+                loading: false,
+                query: '',
+                startDate: null,
+                endDate: null
             };
+        },
+        computed: {
+            params: function () {
+                const {endDate, query, startDate} = this;
+                if (endDate && startDate && endDate < startDate) {
+                    return {query};
+                }
+                return {query, endDate, startDate};
+            },
+            showResults: function () {
+                return this.loading || this.searchResults.length !== 0;
+            },
+            validDateRange: function () {
+                return !this.endDate || !this.startDate || this.endDate > this.startDate;
+            }
         },
         async created() {
             if (this.data) {
@@ -54,21 +108,41 @@
             }
         },
         methods: {
-            populateSearchResults() {
-                const params = {query: this.query};
-                
-                axios.get('/brightcove/search', {params}).then(response => {
-                    this.searchResults = response.data;
-                });
+            clearDateRange(event) {
+                event.preventDefault();
+                this.startDate = this.endDate = null;
+                this.populateSearchResults();
+            },
+            debouncedPopulateSearchResults: _debounce(function() {
+                const {query, endDate, startDate} = this.params
+
+                if (query || endDate || startDate) {
+                    axios.get('/brightcove/search', {params: {query, endDate, startDate}}).then(response => {
+                        this.searchResults = response.data;
+                        this.loading = false;
+                    });
+                } else {
+                    this.searchResults = [];
+                    this.loading = false;
+                }
+            }, 1000),
+            populateSearchResults: function() {
+                this.loading = true;
+
+                this.debouncedPopulateSearchResults();
             },
             selectBrightcove(suggestion) {
                 this.video = suggestion;
                 this.query = ''
+                this.searchResults = [];
                 this.$store.commit('UPDATE_FORMDATA', { path: this.name, data: this.video.id })
             }
         },
         components: {
-            UiAutocomplete
+            UiButton,
+            UiDatepicker,
+            UiProgressCircular,
+            UiTextbox
         }
     }
 </script>
--- app/services/startup/brightcove.js
+++ app/services/startup/brightcove.js
@@ -2,25 +2,27 @@
 
 const brightcoveApi = require('../universal/brightcoveApi'),
   _get = require('lodash/get'),
-  buildQuery = (query) => {
-    return `id:${query}%20name:${query}`;
+  moment = require('moment'),
+  buildQuery = ({query, endDate, startDate}) => {
+    const start = startDate ? moment(startDate).startOf('day').toISOString() : '',
+      end = endDate ? moment(endDate).endOf('day').toISOString() : 'NOW',
+      updatedAtQuery = startDate || endDate ? `%20%2Bupdated_at:${start}..${end}` : '';
+
+    return `${encodeURIComponent(query)}${updatedAtQuery}`;
   },
-  transformSearchResults = (results = []) => {
-    return results.map(({name, images, id}) => {
-      return {name, id, imageUrl: _get(images, 'thumbnail.src', '')};
-    });
-  },
+  transformSearchResults = (results) => (results || []).map(({name, images, id, updated_at}) => {
+    return {name, id, imageUrl: _get(images, 'thumbnail.src', ''), updated_at};
+  }),
   search = async (req, res) => {
     try {
-      const { query } = req.query;
- 
-      return brightcoveApi.request('GET', 'videos', {q: buildQuery(query), limit: 10})
+      return brightcoveApi.request('GET', 'videos', {q: buildQuery(req.query), limit: 10})
         .then(transformSearchResults)
         .then((tResults => {
           return res.send(tResults);
         }))
         .catch(e => {
           console.error(e);
+          res.send(e);
         });
     } catch (e) {
       console.error(e);
