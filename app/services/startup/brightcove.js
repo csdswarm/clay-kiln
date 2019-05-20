d1dd3eab37913556ca346ded61654b72d6ffdf33
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-05-20 15:29:34 -0500

    ON-651 add JSDocs

--- app/services/startup/brightcove.js
+++ app/services/startup/brightcove.js
@@ -3,23 +3,43 @@
 const brightcoveApi = require('../universal/brightcoveApi'),
   _get = require('lodash/get'),
   moment = require('moment'),
-  buildQuery = ({query, endDate, startDate}) => {
-    const start = startDate ? moment(startDate).startOf('day').toISOString() : '',
+  /**
+   *  Create a query string that works with brightcove api standards
+   *
+   * @param {object} params includes query, endDate, and startDate
+   * @returns {string} a brightcove friendly query string
+   */
+  buildQuery = (params) => {
+    const {query, endDate, startDate} = params,
+      start = startDate ? moment(startDate).startOf('day').toISOString() : '',
       end = endDate ? moment(endDate).endOf('day').toISOString() : 'NOW',
       updatedAtQuery = startDate || endDate ? `%20%2Bupdated_at:${start}..${end}` : '';
 
     return `${encodeURIComponent(query)}${updatedAtQuery}`;
   },
-  transformSearchResults = (results) => (results || []).map(({name, images, id, updated_at}) => {
-    return {name, id, imageUrl: _get(images, 'thumbnail.src', ''), updated_at};
+  /**
+   * Return only the needed fields to the UI
+   *
+   * @param {Array} results array of video objects
+   * @returns {Array} an array of video objects with only needed fields
+   */
+  transformSearchResults = (results) => (results || []).map(({name, images, id}) => {
+    return {name, id, imageUrl: _get(images, 'thumbnail.src', '')};
   }),
+  /**
+   * Returns an array of Brightcove videos based on the provided query
+   *
+   * @param {object} req
+   * @param {object} res
+   * @returns {Promise}
+   */
   search = async (req, res) => {
     try {
       return brightcoveApi.request('GET', 'videos', {q: buildQuery(req.query), limit: 10})
         .then(transformSearchResults)
-        .then((tResults => {
-          return res.send(tResults);
-        }))
+        .then(results => {
+          return res.send(results);
+        })
         .catch(e => {
           console.error(e);
           res.send(e);
@@ -29,6 +49,11 @@ const brightcoveApi = require('../universal/brightcoveApi'),
       res.send(e);
     }
   },
+  /**
+   * Add Brightcove routes to the express app
+   *
+   * @param {object} app the express app
+   */
   inject = (app) => {
     app.use('/brightcove/search', search);
   };
