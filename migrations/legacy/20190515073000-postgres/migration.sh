07cc92d7421627e232bd6e5e9112fa86648ba8b1
Markham F Rollins IV <markham@swarmsolutions.com>
2019-05-21 19:50:49 -0400

    ON-438: Create new field in pages mapping

--- migrations/legacy/20190515073000-postgres/migration.sh
+++ migrations/legacy/20190515073000-postgres/migration.sh
@@ -27,3 +27,71 @@ else
     []
     ';
 fi
+
+printf "\nmaking curl GET to $es:9200/_cat/indices?pretty&s=index:desc\n";
+indices=$(curl -X GET "$es:9200/_cat/indices?pretty&s=index:desc" 2>/dev/null);
+# sometimes the currentIndex isn't necessarily the largest. and query brings back in alphabetical order, so 2 > 10
+largestIndex=$(node largest-index "$1" "$indices");
+
+printf "\n$largestIndex\n"
+
+# find out which index is currently being used
+printf "\nmaking curl GET to $es:9200/pages/_alias\n";
+curl -X GET "$es:9200/pages/_alias" > ./aliases.json;
+currentIndex=$(node alias "$1");
+
+# get settings to be able to create new index with the same settings
+printf "\nmaking curl GET to $es:9200/$currentIndex/_settings\n";
+curl -X GET "$es:9200/$currentIndex/_settings" > ./settings.json;
+
+# get mappings to be able to update mappings,
+printf "\nMaking GET to $es:9200/$currentIndex/_mappings\n";
+curl -X GET "$es:9200/$currentIndex/_mappings" > ./mappings.json;
+
+# create new index settings / mappings and save in new-index.json
+printf "\ncreating new index settings & mappings\n";
+node create-new-index "$1" "$currentIndex";
+
+# file only exists if a reindex needs to occur
+if [ -f ./new-index.json ]
+then
+  echo "Reindexing..."
+  # increment largetIndex by 1 to create newIndex
+  num=$(echo $largestIndex | sed 's/[^0-9]*//g');
+  newIndex="$(echo $largestIndex | sed 's/[0-9]*//g')$((num+1))";
+
+  printf "largestIndex: $largestIndex\n";
+  printf "currentIndex: $currentIndex\n";
+  printf "newIndex: $newIndex\n";
+
+  # create new index and remove all temp files needed to create it
+  curl -X PUT "$es:9200/$newIndex" -H 'Content-Type: application/json' -d @./new-index.json;
+
+  # reindex, converting lead from Array[String] to Array[{_ref, data}]
+  reindex=$(sed s/currentIndex/$currentIndex/ ./reindex.json | sed s/newIndex/$newIndex/)
+  printf "\nreindex: $reindex";
+
+  curl -X POST "$es:9200/_reindex" -H 'Content-Type: application/json' -d "$reindex"
+
+  sleep 1;
+
+  # update alias to make pages point to our new index
+  printf "\n\nRemoving old alias and adding new (pages)...\n\n"
+  curl -X POST "$es:9200/_aliases" -H 'Content-Type: application/json' -d "
+      {
+          \"actions\" : [
+              { \"remove\" : { \"index\" : \"$currentIndex\", \"alias\" : \"pages\" } },
+              { \"add\" : { \"index\" : \"$newIndex\", \"alias\" : \"pages\" } }
+          ]
+      }";
+
+  rm ./new-index.json;
+else
+  echo "Skipping reindex, mapping for urlHistory already exists.";
+fi
+
+rm ./settings.json;
+rm ./aliases.json;
+rm ./mappings.json;
+
+
