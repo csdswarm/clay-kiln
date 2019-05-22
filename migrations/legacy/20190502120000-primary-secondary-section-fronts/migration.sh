1dbbe02240e2287fd12a3dc964654eaa4e03abec
Tiffany Chow <tiffachow@gmail.com>
2019-05-22 17:19:04 -0400

    ON-682 Update migration to add sf to new pages

--- migrations/legacy/20190502120000-primary-secondary-section-fronts/migration.sh
+++ migrations/legacy/20190502120000-primary-secondary-section-fronts/migration.sh
@@ -16,5 +16,17 @@ else
   printf "No environment specified. Updating environment $http://$1\n"
 fi
 
-printf "Add new podcast module instances...\n"
+printf "Add new section front lists...\n"
 cat ./_lists.yml | clay import -k demo -y $1
+
+printf "\nUpdating New Pages to Add Section Front...\n\n"
+
+# _lists/new-pages
+listType="new-pages"
+printf "\n\nUpdating _lists instance $listType...\n\n"
+curl -X GET -H "Accept: application/json" $http://$1/_lists/$listType > ./lists-$listType.json
+node ./new-pages-update.js "$1" "$listType";
+cat ./lists-$listType.yml | clay import -k demo -y $1
+rm ./lists-$listType.json
+rm ./lists-$listType.yml
+printf "\n\n\n\n"
