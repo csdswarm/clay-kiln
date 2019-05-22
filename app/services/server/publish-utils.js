ac37dff9014399f13ff9600216b9469cdba683b3
Tiffany Chow <tiffachow@gmail.com>
2019-05-22 15:09:42 -0400

    ON-683 Add secondary sf to content urls

--- app/services/server/publish-utils.js
+++ app/services/server/publish-utils.js
@@ -170,7 +170,8 @@ function getUrlOptions(component, locals, pageType) {
     
 
   urlOptions.prefix = getUrlPrefix(locals.site);
-  urlOptions.sectionFront = slugifyService(component.sectionFront || component.title);
+  urlOptions.sectionFront = slugifyService(component.sectionFront || component.title) || null;
+  urlOptions.secondarySectionFront = slugifyService(component.secondarySectionFront) || null;
   urlOptions.primarySectionFront = component.primary && component.primarySectionFront ? null : slugifyService(component.primarySectionFront);
   urlOptions.contentType = component.contentType || null;
   urlOptions.yyyy = date.format('YYYY') || null;
@@ -201,7 +202,10 @@ module.exports.getPublishDate = getPublishDate;
 // URL patterns below need to be handled by the site's index.js
 module.exports.dateUrlPattern = o => `${o.prefix}/${o.sectionFront}/${o.slug}.html`; // e.g. http://vulture.com/music/x.html - modified re: ON-333
 module.exports.articleSlugPattern = o => `${o.prefix}/${o.sectionFront}/${o.slug}`; // e.g. http://radio.com/music/eminem-drops-new-album-and-its-fire - modified re: ON-333
+module.exports.articleSecondarySectionFrontSlugPattern = o => `${o.prefix}/${o.sectionFront}/${o.secondarySectionFront}/${o.slug}`;
+articleSecondarySectionFrontSlugPattern
 module.exports.gallerySlugPattern = o => `${o.prefix}/${o.sectionFront}/gallery/${o.slug}`; // e.g. http://radio.com/music/gallery/grammies
+module.exports.gallerySecondarySectionFrontSlugPattern = o => `${o.prefix}/${o.sectionFront}/${o.secondarySectionFront}/gallery/${o.slug}`;
 module.exports.sectionFrontSlugPattern = o => `${o.prefix}/${o.sectionFront}`; // e.g. http://radio.com/music
 module.exports.secondarySectionFrontSlugPattern = o => `${o.prefix}/${o.primarySectionFront}/${o.sectionFront}`; // e.g. http://radio.com/music/pop
 module.exports.putComponentInstance = putComponentInstance;
