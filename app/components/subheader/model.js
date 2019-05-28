b8e1ac01f7f6748fc5472631b0bfe7567016d8fa
Chris Strolia-Davis <chris.strolia-davis@swarmsolutions.com>
2019-05-28 08:26:28 -0400

    ON-435 | csd | PR fixes

---
+++ app/components/subheader/model.js
@@ -0,0 +1,18 @@
+'use strict';
+const slugifyService = require('../../services/universal/slugify'),
+  { toPlainText } = require('../../services/universal/sanitize');
+
+/**
+ *
+ * @param {string} uri
+ * @param {Object} data
+ * @param {Object} locals
+ * @returns {{text:string, anchorId:string}}
+ */
+module.exports.save = (uri, data) => {
+  if (data.text) {
+    data.anchorId = 'ip-' + slugifyService(toPlainText(data.text));
+  }
+  console.log(JSON.stringify(data,  null, 2))
+  return data;
+};
