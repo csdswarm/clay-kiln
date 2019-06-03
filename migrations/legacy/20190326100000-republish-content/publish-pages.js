378295f09e60b080e3c1bfbd4cf1deb9c53a37d0
Jeffrey Pope <jeffreypope@Jeffreys-MacBook-Pro.local>
2019-06-03 13:09:32 -0400

    ON-605: cleanup

--- migrations/legacy/20190326100000-republish-content/publish-pages.js
+++ migrations/legacy/20190326100000-republish-content/publish-pages.js
@@ -50,21 +50,22 @@ async function makeRequest(path, method, data) {
   });
 }
 
+function isContent(page) {
+  const hash = page.match(/_pages\/(?<unity>[a-zA-Z0-9]{25})?(?<imported>\d+|sbp-\d+)?(?<other>.+)?/);
+
+  // only republish content, skip other pages
+  return hash && (hash.groups.unity || hash.groups.imported);
+}
+
 async function republishPages() {
   const publishedPagesRes = await makeRequest('/_pages/@published', 'GET'),
     publishedPages = JSON.parse(publishedPagesRes);
 
   return h(publishedPages)
-    .map((page) => {
-      let hash = page.match(/_pages\/(?<unity>[a-zA-Z0-9]{25})?(?<imported>\d+|sbp-\d+)?(?<other>.+)?/);
-
-      // only republish content, skip other pages
-      if (hash && (hash.groups.unity || hash.groups.imported)) {
-        const slug = hash.groups.unity || hash.groups.imported;
-        return h(makeRequest(`/_pages/${slug}@published`, 'PUT'));
-      } else {
-        return h.of(page);
-      }
+    .filter(isContent)
+    .map(page => {
+      const uri = page.split('.com')[1];
+      return h(makeRequest(uri, 'PUT'));
     })
     .parallel(4)
     // for some reason it doesn't run the map fn unless this each is here after
