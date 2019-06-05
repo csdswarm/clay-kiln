4938c21c8fae92e885f0dbacc785f9b43f46daec
Tiffany Chow <tiffachow@gmail.com>
2019-06-05 15:41:56 -0400

    ON-775 Add news stations to news-talk tab

--- app/components/stations-directory/model.js
+++ app/components/stations-directory/model.js
@@ -2,6 +2,7 @@
 const radioApiService = require('../../services/server/radioApi'),
   slugifyService = require('../../services/universal/slugify'),
   SPORTS_SLUG = 'sports',
+  NEWS_SLUG = 'news',
   NEWSTALK_SLUG = 'news-talk',
   url = require('url');
 
@@ -31,24 +32,30 @@ function getAllMarkets() {
 /**
  * fetch all music genres from
  * radio api into an array
+ * @param {boolean} newsTalk
  * @returns {Promise<array>}
  */
-function getAllMusicGenres() {
+function getAllMusicGenres(newsTalk) {
   const route = 'genres',
     params = {
       'page[size]': 100
     };
+  
+  if (newsTalk) {
+    params['sort'] = 'name';
+  }
 
   return radioApiService.get(route, params).then(response => {
     if (response.data) {
-      const onlyMusicGenres = genre => ![SPORTS_SLUG, NEWSTALK_SLUG].includes(genre.attributes.slug),
+      const onlyMusicGenres = genre => ![SPORTS_SLUG, NEWS_SLUG, NEWSTALK_SLUG].includes(genre.attributes.slug),
+        onlyNewsTalkGenres = genre => [NEWS_SLUG, NEWSTALK_SLUG].includes(genre.attributes.slug),
         addSlugAttribute = data => {
           data.attributes.slug = slugifyService(data.attributes.name);
           return data;
         };
 
       return response.data
-        .filter(onlyMusicGenres)
+        .filter(newsTalk ? onlyNewsTalkGenres : onlyMusicGenres)
         .map(addSlugAttribute);
     }
     return [];
@@ -62,6 +69,7 @@ module.exports.render = async (uri, data, locals) => {
 
   data.location = locals.allMarkets = await getAllMarkets();
   data.music = locals.allMusic = await getAllMusicGenres();
+  locals.allNewsTalk = await getAllMusicGenres(true);
   let path = url.parse(locals.url).pathname;
 
   if (path.includes('location') || path.includes('music')) {
