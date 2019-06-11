99b6908198d1803d7c3b895262e79b55895b1b7a
Tiffany Chow <tiffachow@gmail.com>
2019-06-11 12:55:55 -0400

    ON-682 Set primary sf in locals if secondary sf

--- app/components/section-front/model.js
+++ app/components/section-front/model.js
@@ -58,6 +58,7 @@ module.exports.render = (uri, data, locals) => {
     if (data.primary) {
       locals.sectionFront = data.title.toLowerCase();
     } else {
+      locals.sectionFront = data.primarySectionFront;
       locals.secondarySectionFront = data.title.toLowerCase();
     }
   }
