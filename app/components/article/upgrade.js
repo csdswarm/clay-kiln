ba171e3658b761c112012239d084b75adef9f61b
Jeffrey Pope <jeffreypope@ip-10-0-0-23.ec2.internal>
2019-06-17 10:30:15 -0400

    ON-543: add ports back, seems like dev was the only env acting weird

--- app/components/article/upgrade.js
+++ app/components/article/upgrade.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const rest = require('../../services/universal/rest'),
-  getComponentInstance = (uri, opts) => rest.get(`http://${uri}`, opts),
-  putComponentInstance = (uri, body) => rest.put(`http://${uri}`, body, true);
+const { getComponentInstance, putComponentInstance } = require('../../services/server/publish-utils');
 
 module.exports['1.0'] = function (uri, data) {
   // Clone so we don't lose value by reference
