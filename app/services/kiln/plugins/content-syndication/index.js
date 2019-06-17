072e2d122b233e42decd51383a2c1d8fa2fd0c83
Tiffany Chow <tiffachow@gmail.com>
2019-06-17 15:34:10 -0400

    ON-797 Remove category syndication

--- app/services/kiln/plugins/content-syndication/index.js
+++ app/services/kiln/plugins/content-syndication/index.js
@@ -2,7 +2,6 @@
 
 // Require depedencies.
 const stationSelect = require('./station-select.vue'),
-  categorySelect = require('./category-select.vue'),
   genreSelect = require('./genre-select.vue');
 
 // Register plugin.
@@ -10,6 +9,5 @@ module.exports = () => {
   window.kiln = window.kiln || {};
   window.kiln.inputs = window.kiln.inputs || {};
   window.kiln.inputs['station-select'] = stationSelect;
-  window.kiln.inputs['category-select'] = categorySelect;
   window.kiln.inputs['genre-select'] = genreSelect;
 };
