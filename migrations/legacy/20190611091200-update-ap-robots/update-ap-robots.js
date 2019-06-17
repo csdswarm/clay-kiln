5912b7bf9b54e36679558cc8cc1bf31184f2f4b9
Chris Strolia-Davis <chris.strolia-davis@swarmsolutions.com>
2019-06-17 17:00:24 -0400

    ON-622 | csd | OTHER: SEO | Update migration scripts for server migration
    tests on local did not adequately reveal some scenarios that would
    occur when running migration against a server environment. This update
    reasonably fixes those issues.

--- migrations/legacy/20190611091200-update-ap-robots/update-ap-robots.js
+++ migrations/legacy/20190611091200-update-ap-robots/update-ap-robots.js
@@ -1,15 +1,42 @@
 'use strict';
 
 const
-  { parseHost, clayExport, esQuery, clayImport, _get, _chunk, clone, prettyJSON } = require('../migration-utils').v1,
+  fs = require('fs'),
+  {
+    parseHost, clayExport, esQuery, clayImport, httpGet,
+    _get, _chunk, clone, prettyJSON
+  } = require('../migration-utils').v1,
   host = process.argv[2],
-  { es, url, message: envMessage } = parseHost(host),
-  CHUNK_SIZE = 15, MAX_ARTICLES = 10000, DEFAULT_PAUSE = 25;
+  { es, url, http, message: envMessage } = parseHost(host),
+  CHUNK_SIZE = 15, MAX_ARTICLES = 10000, DEFAULT_PAUSE = 25,
+  METATAGS = 'meta-tags', ARTICLES = 'article', GALLERIES = 'gallery';
 
+let errorLog;
 
 async function wait(millis) {
   return new Promise(resolve => setTimeout(resolve, millis));
 }
+
+function chunkUrls(urls){
+  return _chunk(urls, CHUNK_SIZE);
+}
+
+function logError(message, error) {
+  console.error(message);
+  console.error('See `errors.log` in migration folder (20190611091200-update-ap-robots) for more details');
+  if (!errorLog) {
+    errorLog = fs.createWriteStream('./errors.log', { flags: 'a' });
+  }
+  errorLog.write('\n' + '-'.repeat(60) + '\n' + message + ':\n' + '-'.repeat(60) + '\n');
+  try {
+    errorLog.write(prettyJSON({ error }) + '-'.repeat(60) + '\n\n');
+  } catch (e) {
+    // handle cyclic dependency (won't show as detailed of an error, but may provide some useful info)
+    errorLog.write(`${error}` + '-'.repeat(60) + '\n\n');
+  }
+  console.log('\n');
+}
+
 
 async function getListOfComponentsWithAPSource() {
   const info = await esQuery({
@@ -18,25 +45,44 @@ async function getListOfComponentsWithAPSource() {
     size: MAX_ARTICLES,
     ...es
   });
-  return mapAsMetaTagUrls(info.data.hits.hits)
+  return mapAsUnpublished(info.data.hits.hits)
 }
 
-function mapAsMetaTagUrls(componentsList) {
-  return componentsList.map(({ _id }) => _id
-    .replace('@published', '')
-    .replace('/article/', '/meta-tags/')
-    .replace('/gallery/', '/meta-tags/')
-  );
+function mapAsUnpublished(componentsList) {
+  return componentsList.map(({ _id }) => _id.replace('@published', ''));
 }
 
-async function updateAllMetaTags(metaTagUrls) {
-  const metaTagChunks = _chunk(metaTagUrls, CHUNK_SIZE);
-  const totalChunks = metaTagChunks.length;
-  for (const [index, chunk] of metaTagChunks.entries()) {
-    console.log(`Processing next ${chunk.length} items. Set ${index + 1} of ${totalChunks}`);
-    const metaTags = await getMetaTagsFromClay(chunk);
-    const metaTagInstances = await mergeInstances(metaTags);
-    const updates = await fixNoIndexNoFollow(metaTagInstances);
+async function makeSureAllAreUpToDate(componentUrlChunks) {
+  // goes through all the page urls for the articles/galleries we want to update and touches them, so that they
+  // run through their upgrade scripts. That way, we will have meta-tags available to update.
+  const allRequests = [];
+  console.log(`Updating publish version of target components`);
+  for (const chunk of componentUrlChunks) {
+    for (const url of chunk) {
+      try {
+        // give a little pause to make sure that the server isn't overwhelmed.
+        await wait(DEFAULT_PAUSE);
+        const pageUrl = url.replace(/^.*\/instances\//, `${host}/_pages/`) + '.html';
+        // don't really need anything back, just need to touch the target page, so it runs it's upgrades.
+        allRequests.push(httpGet({ http, url: pageUrl })
+          .then(() => console.log(`Ensured: ${http}://${pageUrl} is up-to-date`)));
+      } catch (error) {
+        logError(`There was a problem publishing ${hostUrl}`, error);
+      }
+    }
+  }
+  // Do need to wait for these to be done before moving on.
+  await Promise.all(allRequests);
+  console.log('Done updating target components.\n\n');
+  return componentUrlChunks;
+}
+
+async function updateAllComponents(componentUrlChunks) {
+  const totalChunks = componentUrlChunks.length;
+  for (const [index, chunk] of componentUrlChunks.entries()) {
+    console.log(`Updating next ${chunk.length} items. Set ${index + 1} of ${totalChunks}`);
+    const components = await getComponentsFromClay(chunk);
+    const updates = await mergeInstances(components);
     const result = await applyUpdates(updates);
     await logResult(result);
   }
@@ -44,48 +90,69 @@ async function updateAllMetaTags(metaTagUrls) {
 
 }
 
-async function getMetaTagsFromClay(metaTagUrls) {
-  const metaTagData = [];
+async function getComponentsFromClay(componentUrls) {
+  const componentPromises = [];
 
   // server might not appreciate getting too many simultaneous requests. Let's not
   // launch our own DOS attack. Put some time in between requests, but don't wait
   // for them to all get back right away either - CSD
-  for (const componentUrl of metaTagUrls) {
+  for (const componentUrl of componentUrls) {
+    const type = componentUrl.includes(ARTICLES) ? ARTICLES : GALLERIES;
+    const metaTagUrl = componentUrl.replace(type, METATAGS);
+
     await wait(DEFAULT_PAUSE);
     const gotComponent = clayExport({ componentUrl })
-      .catch(error => console.error(`Problem trying to get component: ${componentUrl}\n`, { error }));
-    metaTagData.push(gotComponent)
+      .catch(error => logError(`Problem trying to get component: ${componentUrl}`, error));
+    const gotMetaTag = clayExport({ componentUrl: metaTagUrl })
+      .catch(error => logError(`Problem trying to get component: ${metaTagUrl}`, error));
+    componentPromises.push({
+      type,
+      promise: Promise.all([gotComponent, gotMetaTag])
+    })
   }
 
   // Here's where we will wait for everything that is in process to complete
-  return await Promise.all(metaTagData);
+  return componentPromises.map(async ({ type, promise }) => {
+    const [componentData, metaTagData] = await promise;
+    return { type, componentData, metaTagData };
+  })
 }
 
-function mergeInstances(metaTags) {
-  return metaTags.reduce((
-    obj,
-    metaTag) => ({ ...obj, ..._get(metaTag, 'data._components.meta-tags.instances', {}) }),
-    {})
+function mergeByType(type, components) {
+  const dataProp = type === METATAGS ? 'metaTagData' : 'componentData';
+
+  return components
+    .filter(c => type === METATAGS || c.type === type)
+    .reduce((obj, component) => (
+      { ...obj, ..._get(component[dataProp], `data._components.${type}.instances`, {}) }
+    ), {})
 }
 
-function fixNoIndexNoFollow(metaTagInstances) {
-  return Object.entries(metaTagInstances)
-    .filter(entry => entry[1].noIndexNoFollow) // only fix it if it's broke
+function mergeInstances(components) {
+  return {
+    [ARTICLES]: { instances: fixNoIndexNoFollow(mergeByType(ARTICLES, components)) },
+    [GALLERIES]: { instances: fixNoIndexNoFollow(mergeByType(GALLERIES, components)) },
+    [METATAGS]: { instances: fixNoIndexNoFollow(mergeByType(METATAGS, components)) },
+  };
+}
+
+function fixNoIndexNoFollow(componentInstances) {
+  return Object.entries(componentInstances)
+    .filter(entry => !entry[1].noIndexNoFollow) // only fix it if it's broke
     .reduce((obj, [key, value]) => ({
       ...obj,
       [key]: { ...clone(value), noIndexNoFollow: true }
     }), {})
 }
 
-function applyUpdates(updates) {
+async function applyUpdates(updates) {
   const payload = {
     _components: {
-      'meta-tags': {
-        instances: updates
-      }
-    }
+      ...updates,
+    },
   };
 
+  await wait(DEFAULT_PAUSE);
   return clayImport({ payload, hostUrl: url, publish: true });
 }
 
@@ -97,10 +164,21 @@ function logResult(result) {
   throw prettyJSON({ error: 'There was a problem saving in 20190611091200-update-ap-robots', result }) + '\n\n\n';
 }
 
+function listErrors() {
+  if (errorLog) {
+    errorLog.end();
+    console.error(
+      'Errors were encountered. Please see errors.log in migration 20190611091200-update-ap-robots for more detail')
+  }
+}
+
 console.log('\n\nUpdating meta-tags for noIndexNoFollow on AP articles');
 console.log(envMessage);
 
 getListOfComponentsWithAPSource()
-  .then(updateAllMetaTags)
-  .catch(console.error)
+  .then(chunkUrls)
+  .then(makeSureAllAreUpToDate)
+  .then(updateAllComponents)
+  .catch(logError)
+  .then(listErrors)
 ;
--- migrations/legacy/migration-utils.js
+++ migrations/legacy/migration-utils.js
@@ -92,7 +92,7 @@ function esQuery(params) {
       yaml && searchParams.push('format=yaml');
 
       const options = {
-        method: 'GET',
+        method: 'POST',
         port: '9200',
         path: `${index || ''}/_search${searchParams.length ? '?' + searchParams.join('&') : ''}`,
         hostname,
@@ -218,6 +218,43 @@ function clayExport(params) {
 }
 
 
+/**
+ * Basically a simple http GET, to retrieve data or markup from a web node. Assumes results are text.
+ * @param {Object} params
+ * @param {string} params.url - the url to retrieve
+ * @param {protocol} params.http - the http (or https) protocol to use
+ * @returns {Promise<string>}
+ */
+function httpGet(params) {
+  const {url, http} = params;
+  return new Promise((resolve, reject) => {
+    try {
+      const conn = require(http);
+
+      // TODO: fix assumption that protocol will not be in URL - CSD
+      const req = conn.get(`${http}://${url}`, res=> {
+        const chunks = [];
+
+        res.on('data', function (chunk) {
+          chunks.push(chunk);
+        });
+
+        res.on('end', function () {
+          resolve(Buffer.concat(chunks).toString());
+        });
+
+        res.on('error', error => reject({ result: 'fail', params, error }))
+      });
+
+      req.on('error', error => reject({ result: 'fail', params, error }))
+
+    }
+    catch (error) {
+      reject({ result: 'fail', params, error });
+    }
+  });
+}
+
 /*******************************************************************************************
  *                                     Version 1.0                                         *
  *******************************************************************************************/
@@ -231,6 +268,7 @@ const v1 = {
   esQuery,
   clayImport,
   clayExport,
+  httpGet,
 };
 
 module.exports = {
