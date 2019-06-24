2b69a67f0aca97518d9a987ecdf07b5818e40c55
Jeffrey Pope <jeffreypope@Jeffreys-MacBook-Pro.local>
2019-06-24 12:03:30 -0400

    ON-431: final touches

--- app/components/brightcove/model.js
+++ app/components/brightcove/model.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const brightcoveApi = require('../../services/universal/brightcoveApi');
+const brightcoveApi = require('../../services/universal/brightcoveApi'),
+  moment = require('moment');
 
-module.exports.render = async (ref, data) => {
+module.exports.render = async (ref, data, locals) => {
   const { videoId } = data,
-    // cache this response for 5-10 min
     videoData = await brightcoveApi.getVideoDetails(videoId),
-    // cache this response for 5-10 min
     analyticsData = await brightcoveApi.getVideoAnalytics(videoId);
 
   if (videoData) {
@@ -17,14 +16,20 @@ module.exports.render = async (ref, data) => {
     data.createdAt = videoData.created_at;
     data.publishedAt = videoData.published_at;
     data.updatedAt = videoData.updated_at;
-    data.duration = videoData.duration;
+    // for some reason this was getting parsed back to millisecond integer value when not wrapped in a string
+    data.duration = `${moment.duration(videoData.duration)}`;
+    data.link = videoData.link;
   }
+
+  // for some reason, api returns null for link... if this happens, give the article url
+  data.seoContentUrl = data.contentUrl || locals.url;
+  data.seoHeadline = data.description || data.name;
+  data.seoDescription = data.longDescription || data.description || data.name;
+  data.seoEmbedUrl = `https://players.brightcove.net/${locals.site.brightcoveAccountId}/${locals.site.brightcovePlayerId}_default/index.html?videoId=${videoId}`;
 
   if (analyticsData && analyticsData.items && analyticsData.items.length == 1) {
     data.views = analyticsData.items[0].video_view;
   }
-
-  // TODO: still need contentUrl & embedUrl
 
   return data;
 };
