067c362535e87b6901604f14d2496f5a93982967
Benjamin <ben.leffler@swarmsolutions.com>
2019-06-28 16:47:59 -0400

    ON-791: Tweak target check logic  - If target equals _blank then set target to _blank doesn't make sense

--- spa/src/lib/VueTranspiler.js
+++ spa/src/lib/VueTranspiler.js
@@ -42,8 +42,8 @@ export default class {
 
       if ($href) {
         const linkParts = new URL($href)
-        if (linkParts.hostname === window.location.hostname && linkParts.pathname !== '/audio' && $target !== '_blank') {
-          if (!$link.hasClass('outbound-link')) {
+        if (linkParts.hostname === window.location.hostname && linkParts.pathname !== '/audio') {
+          if (!$link.hasClass('outbound-link') && $target !== '_blank') {
             $link.addClass('spa-link')
           }
         } else {
