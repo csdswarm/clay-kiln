0ec0294779fb7e9c86fe2ef5db8335d0da805600
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-07-03 09:32:59 -0500

    ON-841 refactor to do everything server side

--- app/components/share/client.js
+++ app/components/share/client.js
@@ -1,17 +1,8 @@
 'use strict';
 
-const qs = require('querystring');
-
 function Constructor(el) {// eslint-disable-line no-unused-vars
   this.url = window.location.href;
-  this.stationCallSign = el.getAttribute('data-station-call-sign');
-  this.domain = el.getAttribute('data-domain');
-  this.title = el.getAttribute('data-title');
-  this.twitterHandle = el.getAttribute('data-twitter-handle');
   this.addCopyEventListener();
-  this.addFacebookUrl();
-  this.addTwitterUrl();
-  this.addEmailShare();
 }
 
 Constructor.prototype = {
@@ -44,45 +35,6 @@ Constructor.prototype = {
 
     // Remove textArea
     document.body.removeChild(textArea);
-  },
-
-  addUtmTracking: function (url, params = {}) {
-    return encodeURIComponent(`${url}?${qs.stringify({
-      utm_campaign: 'sharebutton',
-      utm_medium: 'social',
-      utm_term: this.stationCallSign,
-      ...params
-    })}`);
-  },
-
-  addFacebookUrl: function () {
-    const facebook = document.getElementsByClassName('share-link--facebook'),
-      facebookUrl = this.addUtmTracking(this.url, {utm_source: 'facebook.com'});
-
-    Array.from(facebook, fb => {
-      fb.href = `http://www.facebook.com/sharer/sharer.php?u=${facebookUrl}`;
-    });
-  },
-
-  /**
-   * If the URL isnt set on the twitter link, set it
-   */
-  addTwitterUrl: function () {
-    const twitter = document.getElementsByClassName('share-link--twitter'),
-      twitterUrl = this.addUtmTracking(this.url, { utm_source: 'twitter.com'});
-
-    Array.from(twitter, t => {
-      t.href = `https://twitter.com/share?text=${this.title}&via=${this.twitterHandle}&url=${twitterUrl}`;
-    });
-  },
-
-  addEmailShare: function () {
-    const email = document.getElementsByClassName('share-link--email'),
-      emailBody = this.addUtmTracking(this.url, {utm_source: this.domain, utm_medium: 'email'});
-
-    Array.from(email, e => {
-      e.href = `mailto:?subject=${this.title}&body=${emailBody}`;
-    });
   },
 
   /**
--- app/components/share/model.js
+++ app/components/share/model.js
@@ -1,10 +1,28 @@
 'use strict';
-const _get = require('lodash/get');
+const _get = require('lodash/get'),
+  qs = require('querystring'),
+  striptags = require('striptags'),
+  buildUTMUrl = (url, params = {}) => {
+    return encodeURIComponent(`${url}?${qs.stringify({
+      utm_campaign: 'sharebutton',
+      utm_medium: 'social',
+      utm_term: this.stationCallSign,
+      ...params
+    })}`);
+  };
 
 module.exports.render = (ref, data, locals) => {
+  console.log({data, locals, ref});
+  const title = encodeURIComponent(striptags(data.shortTitle || data.title)),
+    domain = _get(locals, 'site.host'),
+    stationCallSign = _get(locals, 'station.callsign');
+  
   return {
     ...data,
-    domain: _get(locals, 'site.host'),
-    stationCallSign: _get(locals, 'station.callsign')
+    emailUrl: `mailto:?subject=${title}&body=${buildUTMUrl(locals.url, {utm_source: domain, utm_medium: 'email', utm_term: stationCallSign})}`,
+    facebookUrl: `http://www.facebook.com/sharer/sharer.php?u=${buildUTMUrl(locals.url, {utm_source: 'facebook.com', utm_term: stationCallSign})}`,
+    twitterUrl: `https://twitter.com/share?text=${title}&via=${data.twitterHandle}&url=${buildUTMUrl(locals.url, { utm_source: 'twitter.com', utm_term: stationCallSign})}`,
+    domain,
+    stationCallSign
   };
 };
