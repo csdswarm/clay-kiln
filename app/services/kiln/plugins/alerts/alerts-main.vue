c8fc8d2a1cc2b69c81675b2b65eb33f764c9dd43
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-07-10 12:16:54 -0500

    ON-722 display alerts

--- app/services/kiln/plugins/alerts/alerts-main.vue
+++ app/services/kiln/plugins/alerts/alerts-main.vue
@@ -11,25 +11,102 @@
                 @select="loadAlerts"
                 
                 v-for="tab in tabs">
-                <div>
-                    New {{ tab.name }} alert
-                </div>
+            </ui-tab>
+            <ui-button @click="openModal('alertModal')">Add Alert</ui-button>
+            <div class="page-list-headers">
+                <span class="page-list-header page-list-headers__start">Start</span>
+                <span class="page-list-header page-list-headers__end">End</span>
+                <span class="page-list-header page-list-headers__message">Message</span>
+                <span class="page-list-header page-list-headers__breaking">Breaking</span>
+            </div>
+            <div class="page-list-readout">
+                <div
+                    :key="alert.message"
+                    class="page-list-item"
 
-                </ui-tab>
+                    v-for="alert in formattedAlerts"
+                >
+                    <span class="page-list-item__start">{{alert.start}}</span>
+                    <span class="page-list-item__end">{{alert.end}}</span>
+                    <span class="page-list-item__message">{{alert.message}}</span>
+                    <span class="page-list-item__breaking">{{alert.breaking}}</span>
+                </div>
+            </div>
         </ui-tabs>
+        <ui-modal
+            title="Add New Alert"
+            ref="alertModal"
+        >
+                <div>
+                    <ui-textbox
+                        label="Message"
+                        multi-line
+                        
+                        v-model="message"
+                    ></ui-textbox>
+                    <div class="alerts-manager__time-picker">
+                        <ui-datepicker
+                            placeholder="Select the start date"
+                            :maxDate:="endDate"
+                            
+                            v-model="startDate"
+                        >Start Date</ui-datepicker>
+                        <ui-textbox
+                            label="Start Time"
+                            type="time"
+
+                            v-model="startTime"
+                        ></ui-textbox>
+                    </div>
+                    <div class="alerts-manager__time-picker">
+                        <ui-datepicker
+                            placeholder="Select the end date"
+                            :minDate="startDate"
+                            
+                            v-model="endDate"
+                        >End Date</ui-datepicker>
+                        <ui-textbox
+                            label="End Time"
+                            type="time"
+
+                            v-model="endTime"
+                        ></ui-textbox>
+                    </div>
+                    <ui-checkbox
+                        v-model="breaking"
+                    >Breaking</ui-checkbox>
+                </div>
+                <ui-button
+                    @click="addAlert">Add Alert</ui-button>
+        </ui-modal>
     </div>
 </template>
 
 <script>
-    const UiIconButton = window.kiln.utils.components.UiIconButton;
-    const UiProgressCircular = window.kiln.utils.components.UiProgressCircular;
-    const UiTabs = window.kiln.utils.components.UiTabs;
-    const UiTab = window.kiln.utils.components.UiTab;
-    const UiTextbox = window.kiln.utils.components.UiTextbox;
+    const moment = require('moment');
+    const { UiButton, UiCheckbox, UiDatepicker, UiTabs, UiTab, UiTextbox, UiModal } = window.kiln.utils.components;
+    const allStationsCallsigns = window.kiln.locals.allStationsCallsigns || ['NATL-RC', 'KMOX', 'KROX'];
+    const mockAlerts = [{
+        message: 'Something is going on',
+        breaking: true,
+        end: 1563328800000,
+        start: 1562770860000
+    }, {
+        message: 'Something else is going on',
+        breaking: true,
+        end: 1583358800000,
+        start: 1572770860000
+    }]
 
     export default {
         data() {
             return {
+                alerts: [],
+                endDate: '',
+                endTime: '',
+                startDate: '',
+                startTime: '',
+                stationCallsigns: allStationsCallsigns,
                 tabs: [{
                     id: 'global',
                     name: 'Global',
@@ -41,14 +118,64 @@
                 }]
             }
         },
+        computed: {
+            end: function () {
+                const [hour, minute] = this.endTime.split(':');
+
+                if (!this.endDate || !hour || !minute) {
+                    return '';
+                }
+
+                return moment.utc(this.endDate).hour(hour).minute(minute).second(0).milliseconds(0).valueOf(); 
+            },
+            start: function () {
+                const [hour, minute] = this.startTime.split(':');
+
+                if (!this.startDate || !hour || !minute) {
+                    return '';
+                }
+
+                return moment.utc(this.startDate).hour(hour).minute(minute).second(0).milliseconds(0).valueOf();            
+            },
+            formattedAlerts: function() {
+                return this.alerts.map(alert => {
+                    return {
+                        ...alert,
+                        start: moment.utc(alert.start).format('llll'),
+                        end: moment.utc(alert.end).format('llll')
+                    };
+                });
+            }
+        },
         methods: {
             async loadAlerts(value) {
-                console.log(`Should load all ${value} alerts`);
+                console.log('load alerts');
+                this.alerts = mockAlerts;
+            },
+            async addAlert() {
+                const {
+                    breaking,
+                    message,
+                    start,
+                    end
+                } = this;
+
+                console.log('should add alert', {
+                    breaking, message, start, end
+                });
+            },
+            openModal(ref) {
+                this.$refs[ref].open();
             }
         },
         components: {
+            UiButton,
+            UiCheckbox,
+            UiDatepicker,
             UiTabs,
-            UiTab
+            UiTab,
+            UiTextbox,
+            UiModal
         }
     }
 </script>
