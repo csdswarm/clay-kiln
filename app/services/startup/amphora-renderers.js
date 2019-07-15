c111bd592216493a6470d4080aad2f3e7c81a33c
Brien Hodges <brien@swarmsolutions.com>
2019-07-15 08:07:59 -0400

    ON-674 | brien | pass in as postRender plugin

--- app/services/startup/amphora-renderers.js
+++ app/services/startup/amphora-renderers.js
@@ -6,23 +6,25 @@ const pkg = require('../../package.json'),
   helpers = require('../universal/helpers'),
   resolveMediaService = require('../server/resolve-media'),
   { prepare } = require('../universal/spaLink'),
-  cheerio = require('cheerio');
-
-amphoraHtml.configureRender({
-  editAssetTags: true,
-  cacheBuster: pkg.version,
-  transformHtml: html => {
+  cheerio = require('cheerio'),
+  transformHtml = html => {
     const $ = cheerio.load(html);
 
     prepare($);
 
     return $.html();
-  }
+  };
+
+
+amphoraHtml.configureRender({
+  editAssetTags: true,
+  cacheBuster: pkg.version
 });
 
 amphoraHtml.addResolveMedia(resolveMediaService);
 amphoraHtml.addHelpers(helpers);
 amphoraHtml.addEnvVars(require('../../client-env.json'));
+amphoraHtml.addPlugins([{ postRender: transformHtml }])
 
 module.exports = {
   default: 'html',
