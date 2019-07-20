9f4927cc9a419da8e62aa1c7bf158ef2b6feb6fa
Tiffany Chow <tiffachow@gmail.com>
2019-07-19 20:13:32 -0400

    ON-800 Finish up desktop styling

--- app/components/station-nav/client.js
+++ app/components/station-nav/client.js
@@ -12,25 +12,19 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
 
   /**
    * Toggle mobile nav arrow direction & mobile nav on click of arrow
-   * 
+   *
    * @param {boolean} toggleArrowOnly - Toggles arrow without toggling mobile nav
    */
   toggleMobileDrawer = toggleArrowOnly => {
     mobileNavToggle.classList.toggle(active);
-    
+
     // Toggle Mobile Nav Drawer
     if (!toggleArrowOnly) {
       mobileNavDrawer.classList.toggle(active);
     }
   },
 
-  /**
-   * Toggle desktop or mobile nav drawer/dropdown
-   * @param {Object} event - Event from event listener
-   */
-  toggleNavDrawer = ({ type, currentTarget }) => {
-    const isMobile = isMobileNavWidth();
-
+  toggleNavDrawerContainer = (type, currentTarget) => {
     // toggle container for all drawers
     switch (type) {
       case 'mouseover':
@@ -40,10 +34,21 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
         navDrawersContainer.classList.remove(active);
         break;
       case 'click':
-        navDrawersContainer.classList.toggle(active);
+        if (currentTarget !== navDrawersContainer) {
+          navDrawersContainer.classList.toggle(active);
+        }
         break;
       default:
     }
+  },
+  /**
+   * Toggle desktop or mobile nav drawer/dropdown
+   * @param {Object} event - Event from event listener
+   */
+  toggleNavDrawer = ({ type, currentTarget }) => {
+    const isMobile = isMobileNavWidth();
+
+    toggleNavDrawerContainer(type, currentTarget);
 
     // reset all desktop drawers
     for (let drawer of desktopNavDrawers) {
@@ -52,7 +57,6 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
 
     if (!isMobile) {
       // get desktop nav item label
-      console.log(type, currentTarget, Array.from(currentTarget.classList));
       const CLASS_SUBSTRING = 'primary--label-',
         itemLabelClass = Array.from(currentTarget.classList).filter(itemClass => {
           return itemClass.includes(CLASS_SUBSTRING);
@@ -60,8 +64,14 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
         label = itemLabelClass[0].replace(CLASS_SUBSTRING, '');
 
       console.log(label);
-      // toggle corresponding desktop drawer
-      navDrawersContainer.querySelector(`.drawer--desktop.drawer--${ label }`).classList.toggle(active);
+      // toggle corresponding desktop drawer if it exists
+      const selectedDrawer = navDrawersContainer.querySelector(`.drawer--desktop.drawer--${ label }`);
+
+      if (selectedDrawer) {
+        selectedDrawer.classList.toggle(active);
+      } else {
+        navDrawersContainer.classList.remove(active);
+      }
     } else {
       toggleMobileDrawer();
     }
@@ -70,7 +80,7 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
   /**
    * Toggle secondary nav items dropdown for primary nav items
    * on mobile on click of primary nav item
-   * 
+   *
    * @param {Object} event - Event from event listener.
    */
   toggleMobileSecondaryLinks = event => {
@@ -85,30 +95,37 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
 
   /**
    * Add event listeners to header elements to toggle drawers & images.
-   * 
+   *
    */
   addEventListeners = () => {
     // Toggle Mobile Nav
     mobileNavToggle.addEventListener('click', toggleMobileDrawer);
-    
+
     // Toggle Dropdowns on Mobile Nav Categories
     mobileNavItems.forEach(item => {
       item.addEventListener('click', toggleMobileSecondaryLinks);
     });
-    
+
     // Toggle Nav Desktop Drawers
-    console.log("09");
+    console.log("10");
     desktopNavItems.forEach(item => {
       if (item.classList.contains('primary--drawer-enabled')) {
         item.addEventListener('mouseover', e => { toggleNavDrawer(e); });
         item.addEventListener('mouseout', e => { toggleNavDrawer(e); });
       }
     });
-    
+
+    navDrawersContainer.addEventListener('mouseover', e => {
+      toggleNavDrawerContainer(e.type, e.currentTarget);
+    });
+    navDrawersContainer.addEventListener('mouseout', e => {
+      toggleNavDrawerContainer(e.type, e.currentTarget);
+    });
+
     // Remove mobile nav on resize if not on mobile
     window.addEventListener('resize', function () {
       const isMobile = isMobileNavWidth();
-      
+
       if (!isMobile) {
         mobileNavToggle.classList.remove(active);
         mobileNavDrawer.classList.remove(active);
