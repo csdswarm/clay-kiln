12fd11105894a48a31e64b3aff535ca82e64369c
Tiffany Chow <tiffachow@gmail.com>
2019-07-22 18:25:39 -0400

    ON-800 More mobile styling, add image upload for logo

--- app/components/station-nav/client.js
+++ app/components/station-nav/client.js
@@ -5,33 +5,42 @@ navDrawersContainer,
 desktopNavDrawers,
 mobileNavToggle,
 mobileNavDrawer,
-mobileNavItems;
+mobileNavItems,
+listenNavToggle;
 
 const { isMobileNavWidth } = require('../../services/client/mobile'),
   active = 'active',
 
+  toggleListenDrawer = (e) => {
+    listenNavToggle.classList.toggle(active);
+    // TODO -- Toggle listen drawer here
+  },
   /**
    * Toggle mobile nav arrow direction & mobile nav on click of arrow
    *
-   * @param {boolean} toggleArrowOnly - Toggles arrow without toggling mobile nav
+   * @param {Object} event -- contains type and currentTarget
    */
-  toggleMobileDrawer = toggleArrowOnly => {
+  toggleMobileDrawer = ({type, currentTarget}) => {
     mobileNavToggle.classList.toggle(active);
 
-    // Toggle Mobile Nav Drawer
-    if (!toggleArrowOnly) {
-      mobileNavDrawer.classList.toggle(active);
-    }
+    toggleNavDrawerContainer(type, currentTarget);
+    mobileNavDrawer.classList.toggle(active);
   },
 
   toggleNavDrawerContainer = (type, currentTarget) => {
+    const isMobile = isMobileNavWidth();
+
     // toggle container for all drawers
     switch (type) {
       case 'mouseover':
-        navDrawersContainer.classList.add(active);
+        if (!isMobile) {
+          navDrawersContainer.classList.add(active);
+        }
         break;
       case 'mouseout':
-        navDrawersContainer.classList.remove(active);
+        if (!isMobile) {
+          navDrawersContainer.classList.remove(active);
+        }
         break;
       case 'click':
         if (currentTarget !== navDrawersContainer) {
@@ -63,13 +72,14 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
         }),
         label = itemLabelClass[0].replace(CLASS_SUBSTRING, '');
 
-      console.log(label);
       // toggle corresponding desktop drawer if it exists
       const selectedDrawer = navDrawersContainer.querySelector(`.drawer--desktop.drawer--${ label }`);
 
       if (selectedDrawer) {
+        currentTarget.classList.toggle(active);
         selectedDrawer.classList.toggle(active);
       } else {
+        currentTarget.classList.remove(active);
         navDrawersContainer.classList.remove(active);
       }
     } else {
@@ -90,7 +100,7 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
         item.classList.remove(active);
       }
     }
-    event.currentTarget.classList.add(active);
+    event.currentTarget.classList.toggle(active);
   },
 
   /**
@@ -98,8 +108,11 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
    *
    */
   addEventListeners = () => {
+    // Toggle Listen Nav
+    listenNavToggle.addEventListener('click', e => { toggleListenDrawer(e); });
+
     // Toggle Mobile Nav
-    mobileNavToggle.addEventListener('click', toggleMobileDrawer);
+    mobileNavToggle.addEventListener('click', e => { toggleMobileDrawer(e); });
 
     // Toggle Dropdowns on Mobile Nav Categories
     mobileNavItems.forEach(item => {
@@ -107,7 +120,6 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
     });
 
     // Toggle Nav Desktop Drawers
-    console.log("10");
     desktopNavItems.forEach(item => {
       if (item.classList.contains('primary--drawer-enabled')) {
         item.addEventListener('mouseover', e => { toggleNavDrawer(e); });
@@ -122,13 +134,19 @@ const { isMobileNavWidth } = require('../../services/client/mobile'),
       toggleNavDrawerContainer(e.type, e.currentTarget);
     });
 
-    // Remove mobile nav on resize if not on mobile
+    // Remove mobile & desktop navs on resize
     window.addEventListener('resize', function () {
       const isMobile = isMobileNavWidth();
 
       if (!isMobile) {
         mobileNavToggle.classList.remove(active);
+        navDrawersContainer.classList.remove(active);
         mobileNavDrawer.classList.remove(active);
+      } else {
+        // reset all desktop drawers
+        for (let drawer of desktopNavDrawers) {
+          drawer.classList.remove(active);
+        }
       }
     });
   };
@@ -142,7 +160,8 @@ document.addEventListener('station-nav-mount', function () {
   desktopNavDrawers = navDrawersContainer.querySelectorAll('.drawer--desktop'),
   mobileNavToggle = stationNav.querySelector('.menu__mobile-toggle'),
   mobileNavDrawer = navDrawersContainer.querySelector('.drawer--mobile'),
-  mobileNavItems = mobileNavDrawer.querySelectorAll('.drawer__item');
+  mobileNavItems = mobileNavDrawer.querySelectorAll('.drawer__item'),
+  listenNavToggle = stationNav.querySelector('.menu__listen-toggle');
 
   addEventListeners();
 });
