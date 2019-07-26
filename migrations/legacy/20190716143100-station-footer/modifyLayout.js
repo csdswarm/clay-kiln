79ebd2d33c2a3b6d78b0b6c8a441bd3d2973ee86
Brien Hodges <brien@swarmsolutions.com>
2019-07-26 09:48:48 -0400

    ON-717 | brien | missed the p in splice

--- migrations/legacy/20190716143100-station-footer/modifyLayout.js
+++ migrations/legacy/20190716143100-station-footer/modifyLayout.js
@@ -5,7 +5,7 @@ const data = YAML.load(`${__dirname}/layout.yml`);
 
 const position = data._layouts['one-column-layout'].instances.station.bottom.findIndex(item => item._ref === '/_components/footer/instances/default');
 
-data._layouts['one-column-layout'].instances.station.bottom.slice(position - 1, 0, {
+data._layouts['one-column-layout'].instances.station.bottom.splice(position - 1, 0, {
   _ref: "/_components/station-footer/instances/default"
 });
 
