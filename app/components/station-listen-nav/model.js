0668a57c4f9b4d37e9cf3ab8f575b83f89e3088b
Tiffany Chow <tiffachow@gmail.com>
2019-08-02 14:40:17 -0400

    ON-716 Add default for imgs

--- app/components/station-listen-nav/model.js
+++ app/components/station-listen-nav/model.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const { playingClass } = require('../../services/universal/spaLocals'),
-  { getNowPlaying, getSchedule } = require('../../services/universal/station');
+  { getNowPlaying, getSchedule } = require('../../services/universal/station'),
+
+  /**
+   * Appends ? or & to end of img string
+   *
+   * @param {string} img
+   * @returns {string}
+  */
+  appendParamsAmpOrQuery = (img) => {
+    img = img || 'https://images.radio.com/aiu-media/og_775x515_0.jpg';
+    img = img.includes('?') ?
+      `${ img }&` :
+      `${ img }?`;
+
+    return img;
+  };
 
 module.exports.render = async (ref, data, locals) => {
   if (!locals.station && !locals.station.id) {
@@ -15,6 +30,16 @@ module.exports.render = async (ref, data, locals) => {
 
   data.playingClass = playingClass(locals, locals.station.id);
   data.station = locals.station;
+
+  data.featuredLinks.forEach((link, i, links) => {
+    links[i].image = appendParamsAmpOrQuery(link.image);
+  });
+  if (data.nowPlaying) {
+    data.nowPlaying.imageUrl = appendParamsAmpOrQuery(data.nowPlaying.imageUrl);
+  }
+  if (data.onAir) {
+    data.onAir.image = appendParamsAmpOrQuery(data.onAir.image);
+  }
 
   return data;
 };
