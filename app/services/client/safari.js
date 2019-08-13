780e0730d35043e25a4e262cb99cb122eb08402c
Micah Allen <micah.allen@swarmsolutions.com>
2019-08-13 09:09:28 -0400

    ON-973 | micah | add comment to fixAJAXImages setTimeout

--- app/services/client/safari.js
+++ app/services/client/safari.js
@@ -8,6 +8,7 @@
  */
 function fixAJAXImages(parentContainer) {
   // iOS doesn't play nice with srcset dynamically (https://github.com/metafizzy/infinite-scroll/issues/770)
+  // give the DOM time to settle before making it reparse. without this, some images may still not load
   setTimeout(() => {
     if (/iPhone|iPad/.test(navigator.userAgent)) {
       parentContainer.querySelectorAll('img').forEach((img) => {
