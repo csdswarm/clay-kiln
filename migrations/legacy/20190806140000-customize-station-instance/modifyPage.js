e4a3fce937baea441196deffae8d639fb8b042ad
Benjamin <ben.leffler@swarmsolutions.com>
2019-08-27 11:32:38 -0400

    ON-909 | Update Migration  - Rename ALL instances of 'station-front-3' to 'station-basic-music'  - Setup new instances for station-nav and station-footer  - Update station-basic-music page to create new instances of each  - Fix issue with pageHeader in basic-music page not being a list

--- migrations/legacy/20190806140000-customize-station-instance/modifyPage.js
+++ migrations/legacy/20190806140000-customize-station-instance/modifyPage.js
@@ -10,12 +10,20 @@ if (stationFront.customUrl) {
 }
 
 if (stationFront.pageHeader === 'pageHeader') {
-  stationFront.pageHeader = '/_components/topic-page-header/instances/new';
+  stationFront.pageHeader = [ '/_components/topic-page-header/instances/new' ];
 }
 
 if (stationFront.layout === '/_layouts/one-column-layout/instances/station') {
   stationFront.layout = '/_layouts/one-column-layout/instances/station-basic-music';
 }
+
+// Setup station specific components to be new instances
+stationFront.topSection = [ '/_components/station-nav/instances/new' ];
+stationFront.bottomSection = [ '/_components/station-footer/instances/new' ];
+
+// Rename other station-front-3 instance references to station-basic-music
+stationFront.head = stationFront.head.map(_ref => _ref.replace(/station\-front\-3$/, 'station-basic-music'));
+stationFront.main = stationFront.main.map(_ref => _ref.replace(/station\-front\-3$/, 'station-basic-music'));
 
 // rename to station-basic-music
 data._pages['station-basic-music'] = stationFront;
