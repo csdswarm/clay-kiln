98ba93287ee963d293cfe31aa5bdf34cbfca019f
Tiffany Chow <tiffachow@gmail.com>
2019-09-03 18:12:29 -0400

    ON-967 Add .anf file for article component

---
+++ app/components/article/anf.model.js
@@ -0,0 +1,43 @@
+// https://developer.apple.com/documentation/apple_news/component
+// https://developer.apple.com/documentation/apple_news/apple_news_format/components
+
+const { formatSimpleByline } = require('../../services/universal/byline'),
+  formatBylines = bylines => {
+    const bylineHTML = '';
+
+    bylines.forEach(byline => {
+      bylineHTML.push(`${ byline.prefix } `);
+      bylineHTML.push(formatSimpleByline(byline.names));
+      byline.sources.forEach(source => {
+        bylineHTML.push(`, ${ source.text }`);
+      });
+    });
+
+    return bylineHTML;
+  };
+
+module.exports = function (ref, data, locals) {
+  return {
+    role: 'container',
+    style: headerStyle,
+    layout: headerLayout,
+    components: [
+      {
+        role: 'title',
+        text: data.primaryHeadline,
+        layout: headlineLayout,
+        style: headlineStyle,
+        textStyle: headlineTextStyle,
+        format: 'html'
+      },
+      {
+        role: 'author',
+        text: formatBylines(data.byline),
+        layout: bylineLayout,
+        style: bylineStyle,
+        textStyle: bylineTextStyle,
+        format: 'html'
+      }
+    ]
+  };
+};
--- app/services/startup/amphora-renderers.js
+++ app/services/startup/amphora-renderers.js
@@ -3,6 +3,7 @@
 const pkg = require('../../package.json'),
   amphoraHtml = require('amphora-html'),
   amphoraRss = require('amphora-rss'),
+  amphoraAppleNews = require('amphora-apple-news'),
   helpers = require('../universal/helpers'),
   resolveMediaService = require('../server/resolve-media'),
   { prepare } = require('../universal/spaLink'),
@@ -37,5 +38,6 @@ amphoraHtml.addPlugins([{ postRender: transformHtml }]);
 module.exports = {
   default: 'html',
   html: amphoraHtml,
-  rss: amphoraRss
+  rss: amphoraRss,
+  anf: amphoraAppleNews
 };
