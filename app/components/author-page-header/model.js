d51455c65c2db642b9a2742e2ecc43fce22d364b
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-09-05 14:47:21 -0500

    ON-807 merge with styling

--- app/components/author-page-header/model.js
+++ app/components/author-page-header/model.js
@@ -1,8 +1,19 @@
 'use strict';
 
 module.exports.render = (ref, data, locals) => {
+  const {email, facebookHandle, instagramHandle, twitterHandle, youtubeHandle} = data;
+
   if (locals && locals.params && locals.params.dynamicAuthor) {
     data.author = locals.params.dynamicAuthor.replace(/-/g, ' ').replace(/\//g,'');
+  }
+
+  if (email || facebookHandle || instagramHandle || twitterHandle || youtubeHandle) {
+    data.showSocial = true;
+
+    data.facebookUrl = facebookHandle && `https://www.facebook.com/${facebookHandle}`;
+    data.instagramUrl = instagramHandle && `https://www.instagram.com/${instagramHandle}`;
+    data.twitterUrl = twitterHandle && `https://www.twitter.com/${twitterHandle}`;
+    data.youtubeUrl = youtubeHandle && `https://www.youtube.com/${youtubeHandle}`;
   }
 
   return data;
