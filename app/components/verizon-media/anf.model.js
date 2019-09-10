982bfaf08a08ca07688fb24b6bdead5276ef2a0d
Tiffany Chow <tiffachow@gmail.com>
2019-09-10 18:03:15 -0400

    ON-967 Verizon Media anf WIP need company key

--- app/components/verizon-media/anf.model.js
+++ app/components/verizon-media/anf.model.js
@@ -3,13 +3,30 @@
 /** https://developer.apple.com/documentation/apple_news/apple_news_format/components
  *  https://developer.apple.com/documentation/apple_news/video
 */
+const rest = require('../../services/universal/rest'),
+  _get = require('lodash/get'),
+  log = require('../universal/log').setup({ file: __filename }),
+  vidibleApi = `http://api.vidible.tv/${ process.env.VIDIBLE_KEY }/`,
+  getVideoRendition = async videoID => {
+    let srcURL = '';
 
-module.exports = function (ref, data, locals) {
+    try {
+      const video = await rest.get(`${ vidibleApi }video/${ videoID }?show_metadata=false&show_transcript=false&show_thumbnails=false`, {});
+
+      srcURL = _get(video, originalVideoUrl);
+    } catch (e) {
+      log('error', `Error fetching data from verizon api for video ID ${ videoID }: ${ e }`);
+    };
+
+    return srcURL;
+  };
+
+module.exports = function async (ref, data) {
   return {
-    role: 'video', // test
-    URL: '', // .M3U8
-    layout: 'verizonMediaLayout',
-    style: 'verizonMediaStyle',
+    role: 'video',
+    URL: await getVideoRendition(data.videoId) || '',
+    layout: 'vidibleLayout',
+    style: 'vidibleStyle',
     format: 'html'
   };
 };
