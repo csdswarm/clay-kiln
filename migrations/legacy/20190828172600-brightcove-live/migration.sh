ade3bd54f08e610420e0320cee7718b689bc65cd
Micah Allen <micah.allen@swarmsolutions.com>
2019-09-11 16:02:55 -0400

    ON-1021 | micah | add migration prologue

--- migrations/legacy/20190828172600-brightcove-live/migration.sh
+++ migrations/legacy/20190828172600-brightcove-live/migration.sh
@@ -1,8 +1,22 @@
 #! /bin/bash
 
+if [ "$1" != "" ]; then
+  if [ "$1" == "clay.radio.com" ]; then
+    http="http";
+  elif [ "$1" == "dev-clay.radio.com" ]; then
+    http="https";
+  elif [ "$1" == "stg-clay.radio.com" ]; then
+    http="https";
+  elif [ "$1" == "www.radio.com" ]; then
+    http="https";
+  fi
+  printf "Updating environment $http://$1\n"
+else
+  set "clay.radio.com" && http="http";
+  printf "No environment specified. Updating environment $http://$1\n"
+fi
+
 printf "\n\nCreating Brightcove Live Component...\n\n\n"
 clay import -k demo -y -p "$1" < ./_components.yml
 
 printf "\n\n\n\n"
-
-
