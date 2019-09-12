234c2c657880aa9b861430fd86a9afd67cc0b93c
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-09-12 14:03:16 -0500

    ON-664 add meta-title to stations directory

---
+++ migrations/legacy/20190912102700-station-directory-meta-title/migration.js
@@ -0,0 +1,20 @@
+'use strict';
+const host = process.argv[2],
+  { clayExport, clayImport } = require('../migration-utils').v1;
+
+const STATIONS_DIRECTORY_PAGE = '/_pages/stations-directory',
+    META_TITLE_INSTANCE = '/_components/meta-title/instances/stations-directory',
+    META_DESCRIPTION_INSTANCE = '/_components/meta-description/instances/stations-directory';
+
+const checkArrayForString = (arr, checkStr) => arr.some(str => str.includes(checkStr));
+
+clayExport({componentUrl: `${host}${STATIONS_DIRECTORY_PAGE}`})
+    .then(({data}) => {
+        [META_TITLE_INSTANCE, META_DESCRIPTION_INSTANCE].forEach(instance => {
+            if (!checkArrayForString(data._pages['stations-directory'].head, instance)) {
+                data._pages['stations-directory'].head.push(`${instance}`)
+            }
+        });
+        return clayImport({hostUrl: host, payload: data, publish: true});
+    });
+
