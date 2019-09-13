bbb12cd61005f18513c9bd025ebdb17e2cc5dd65
jzentercom <jeremy.zevin@entercom.com>
2019-09-12 23:59:22 -0400

    ON-1030: dynamic model to inject params of numItems to test

--- app/components/latest-top-recirc-slider/model.js
+++ app/components/latest-top-recirc-slider/model.js
@@ -1,37 +1,22 @@
 'use strict';
-const items = [{
-  img: 'https://placeimg.com/300/300/animals/1',
-  title: 'Diam donec adipiscing tristique risus nec feugiat. Etiam erat velit scelerisque in dictum non.',
-  category: 'music'
-},
-{
-  img: 'https://placeimg.com/300/300/animals/2',
-  title: 'Aliquet lectus proin nibh nisl condimentum id.',
-  category: 'sports'
-},
-{
-  img: 'https://placeimg.com/300/300/animals/7',
-  title: 'Eget aliquet nibh praesent tristique.',
-  category: 'news'
-},
-{
-  img: 'https://placeimg.com/300/300/animals/4',
-  title: 'Mauris a diam maecenas sed enim ut sem viverra aliquet.',
-  category: 'music'
-},
-{
-  img: 'https://placeimg.com/300/300/animals/8',
-  title: 'Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras.',
-  category: 'sports'
-},
-{
-  img: 'https://placeimg.com/300/300/animals/11',
-  title: 'Faucibus a pellentesque sit amet porttitor eget dolorssss.',
-  category: 'music'
-}
-];
 
-module.exports.render = (ref, data) => {
-  data.items = items;
+function getItems(num = 6) {
+  const items = [];
+  
+  for (let i = 0; i < num; i++) {
+    let item;
+    
+    item = {
+      img: `https://placeimg.com/300/300/animals/${i + 1}`,
+      title: 'Faucibus a pellentesque sit amet porttitor eget dolorssss.',
+      category: 'music'
+    };
+    items.push(item);
+  }
+  return items;
+}
+
+module.exports.render = (ref, data, locals) => {
+  data.items = getItems(locals.query.numItems);
   return data;
 };
