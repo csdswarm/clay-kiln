6ffc493d87fa4cda7ec26e5b1cf3d153e1d76436
Chris Strolia-Davis <chris.strolia-davis@swarmsolutions.com>
2019-09-13 15:58:34 -0400

    ON-837 | csd | SEO: Create a Google News site map
    Some refactoring and cleanup, plus order by published date desc and
    add 1k record limit.
    
    Added ability to add schema check section for validation

--- app/routes/sitemap-google-news.js
+++ app/routes/sitemap-google-news.js
@@ -1,15 +1,31 @@
 'use strict';
 
-const db = require('../services/server/db');
+const _get = require('lodash/get'),
+  db = require('../services/server/db');
+/**
+ * Returns a google news style site-map for all articles published recently
+ * @param {object} req
+ * @param {object} res
+ * @returns {Promise<object>}
+ */
 
 module.exports = async function (req, res) {
   const NUMBER_OF_DAYS_TO_RETRIEVE = 2,
-    urlsList = `
-    SELECT id, xmlelement(name url, 
+    PUBLICATION_NAME = 'RADIO.COM',
+    schemaLocationInfo = req.query.schemaCheck
+      ? ', \'http://www.w3.org/2001/XMLSchema-instance\' as "xmlns:xsi", ' +
+        "'http://www.sitemaps.org/schemas/sitemap/0.9 " +
+        'https://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd ' +
+        'http://www.google.com/schemas/sitemap-news/0.9 ' +
+        "https://www.google.com/schemas/sitemap-news/0.9/sitemap-news.xsd'" +
+      ' as "xsi:schemaLocation"'
+      : '',
+    urlsInGoogleNewsFormatSQL = `
+    SELECT (meta->>'publishTime')::timestamp as published, xmlelement(name url, 
       xmlelement(name loc, replace(meta->>'url', 'http://', 'https://')),
       xmlelement(name "news:news", 
         xmlelement(name "news:publication", 
-          xmlelement(name "news:name", 'RADIO.COM'),
+          xmlelement(name "news:name", '${PUBLICATION_NAME}'),
           xmlelement(name "news:language", 'en')
         ),
         xmlelement(name "news:publication_date", (meta->>'publishTime')::timestamptz),
@@ -20,30 +36,33 @@ module.exports = async function (req, res) {
     WHERE 
       meta @> '{"published": true}'
       AND (meta->>'publishTime')::date > current_date - ${NUMBER_OF_DAYS_TO_RETRIEVE}
-      AND data#>>'{main, 0}' ~ '_components/article/'`,
-    aggregateUrls = `
+      AND data#>>'{main, 0}' ~ '_components/article/'
+    ORDER BY 
+      published DESC
+    LIMIT 1000`,
+
+    urlsetAggregationSQL = `
     SELECT 
       xmlroot(
         xmlelement(name urlset, 
           xmlattributes(
             'http://www.sitemaps.org/schemas/sitemap/0.9' as xmlns,
-            'http://www.google.com/schemas/sitemap-news/0.9' as "xmlns:news"
+            'http://www.google.com/schemas/sitemap-news/0.9' as "xmlns:news"${schemaLocationInfo}
           ),
           xmlagg(xml_data)
         ), version '1.0" encoding="UTF-8' -- postgres does not have a direct way to add encoding
       )::text as data
     FROM _urls`,
-    data = await db.raw(`WITH _urls as (${urlsList}) ${aggregateUrls};`),
-    gotData = data && data.rows && data.rows.length;
 
-  res.set('Content-Type', 'text/xml');
-  if (gotData) {
-    return res.send(data.rows[0].data);
-  }  else {
-    return res.send(`<?xml version="1.0" encoding="UTF-8"?>
-      <urlset 
-        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
-        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
-      </urlset>`);
-  }
+    googleNewsSiteMapSQL = `WITH _urls as (${urlsInGoogleNewsFormatSQL}) ${urlsetAggregationSQL};`,
+    emptySiteMap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset 
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+      xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+    </urlset>`,
+
+    data = await db.raw(googleNewsSiteMapSQL);
+
+  res.type('application/xml');
+  return res.send(_get(data, 'rows[0].data', emptySiteMap));
 };
