4c95738352e22c279e89686a2fe5a698de89f6e9
jzentercom <jeremy.zevin@entercom.com>
2019-09-13 00:35:59 -0400

    ON-1030: item link integration and adjustments

--- app/components/latest-top-recirc-slider/model.js
+++ app/components/latest-top-recirc-slider/model.js
@@ -1,15 +1,22 @@
 'use strict';
 
+function rndInt(min = 0, max = 1) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function rndPick(array) {
+  return array[rndInt(0,array.length - 1)];
+}
 function getItems(num = 6) {
   const items = [];
   
   for (let i = 0; i < num; i++) {
     let item;
-    
+
     item = {
-      img: `https://placeimg.com/300/300/animals/${i + 1}`,
+      img: `https://placeimg.com/300/300/animals/${rndInt(1,10)}`,
       title: 'Faucibus a pellentesque sit amet porttitor eget dolorssss.',
-      category: 'music'
+      category: rndPick(['music', 'news', 'sports']),
+      link: '/foo'
     };
     items.push(item);
   }
