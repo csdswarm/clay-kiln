8ed623dec74e53b84ee84a61b1b04fdccdf57dc0
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-09-17 12:51:44 -0500

    ON-664 add meta-image to stations as well

--- migrations/legacy/20190912102700-station-directory-meta-title/migration.js
+++ migrations/legacy/20190912102700-station-directory-meta-title/migration.js
@@ -4,13 +4,14 @@ const host = process.argv[2],
 
 const STATIONS_DIRECTORY_PAGE = '/_pages/stations-directory',
     META_TITLE_INSTANCE = '/_components/meta-title/instances/stations-directory',
-    META_DESCRIPTION_INSTANCE = '/_components/meta-description/instances/stations-directory';
+    META_DESCRIPTION_INSTANCE = '/_components/meta-description/instances/stations-directory',
+    META_IMAGE_INSTANCE = '/_components/meta-image/instances/og-logo';
 
 const checkArrayForString = (arr, checkStr) => arr.some(str => str.includes(checkStr));
 
 clayExport({componentUrl: `${host}${STATIONS_DIRECTORY_PAGE}`})
     .then(({data}) => {
-        [META_TITLE_INSTANCE, META_DESCRIPTION_INSTANCE].forEach(instance => {
+        [META_TITLE_INSTANCE, META_DESCRIPTION_INSTANCE, META_IMAGE_INSTANCE].forEach(instance => {
             if (!checkArrayForString(data._pages['stations-directory'].head, instance)) {
                 data._pages['stations-directory'].head.push(`${instance}`)
             }
