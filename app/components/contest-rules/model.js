f2712743a3f3db855189c796bc250f4be0cc97cb
jzentercom <jeremy.zevin@entercom.com>
2019-09-27 13:54:57 -0400

    ON-1054: removed mock and add styling

--- app/components/contest-rules/model.js
+++ app/components/contest-rules/model.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports.render = (ref, data) => {
-  data.rulesBodyText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit ea repellat libero ex dignissimos, repellendus neque nisi maiores deleniti, ut suscipit fugit nihil blanditiis eius! A dolorem, repudiandae nisi obcaecati similique praesentium perspiciatis libero nihil dolorum aut eius vero temporibus, totam adipisci delectus ducimus nemo, mollitia recusandae! Cupiditate rem quam corporis facere reiciendis, laborum quisquam laboriosam sunt quis alias ex deleniti culpa sed perspiciatis modi ducimus eos nemo placeat vel veniam numquam et. Repudiandae vel molestiae corporis repellendus illo aliquam.';
-
+  
   return data;
 };
