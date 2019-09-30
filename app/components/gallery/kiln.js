ba26c8fc9560752498dde9889c18bfe93917556e
Tiffany Chow <tiffachow@gmail.com>
2019-09-30 11:17:14 -0400

    ON-965 Add service to gallery

--- app/components/gallery/kiln.js
+++ app/components/gallery/kiln.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { syncFields } = require('../../services/client/kiln-utils');
+const { syncFields, syncHeadlines } = require('../../services/client/kiln-utils');
 
 module.exports = (schema) => {
   return syncFields(schema, syncHeadlines('article'));
