ec2f5accf1a60df8ff1ba389d22f6c2d451545a1
Leland Kwong <Leland.Kwong@entercom.com>
2019-10-02 11:10:48 -0400

    ON-1042 | Apple news frontend
    * add image asset urls
    * update footer color to match design

--- app/services/universal/contentAppleNews/component-footer.js
+++ app/services/universal/contentAppleNews/component-footer.js
@@ -1,9 +1,72 @@
 'use strict';
 
+const URL_LOGO = 'https://images.radio.com/aiu-media/radiocomlogowhite-47112475-0fac-4811-b34b-49f88dc2af49.png',
+  URL_MAIL_ICON = 'https://images.radio.com/aiu-media/mail-7d850a71-cd77-46e5-ae88-76633a92bb57.png',
+  COLOR_PRIMARY = '#1F055E',
+  logoComponent = {
+    role: 'image',
+    URL: URL_LOGO,
+    layout: {
+      maximumContentWidth: 140,
+      maximumWidth: 140,
+      margin: {
+        bottom: 11
+      }
+    }
+  },
+  ctaLink = {
+    role: 'container',
+    contentDisplay: {
+      type: 'collection',
+      alignment: 'center',
+      distribution: 'narrow',
+      maximumWidth: 190
+    },
+    components: [
+      {
+        role: 'container',
+        contentDisplay: {
+          type: 'horizontal_stack'
+        },
+        style: {
+          backgroundColor: 'white'
+        },
+        layout: {
+          padding: {
+            top: 16,
+            bottom: 16,
+            right: 30,
+            left: 30
+          }
+        },
+        components: [
+          {
+            role: 'image',
+            URL: URL_MAIL_ICON,
+            layout: {
+              maximumContentWidth: 26,
+              maximumWidth: 26
+            }
+          },
+          {
+            role: 'body',
+            text: '<a href="https://app.radio.com/apple-news-download"><span data-anf-textstyle="footerCTAStyle">SIGN UP NOW</span></a>',
+            format: 'html',
+            layout: {
+              padding: {
+                left: 10
+              }
+            }
+          }
+        ]
+      }
+    ]
+  };
+
 module.exports = {
   role: 'container',
   style: {
-    backgroundColor: 'purple'
+    backgroundColor: COLOR_PRIMARY
   },
   layout: {
     ignoreDocumentMargin: true,
@@ -13,17 +76,7 @@ module.exports = {
     }
   },
   components: [
-    {
-      role: 'image',
-      URL: 'https://via.placeholder.com/140x17',
-      layout: {
-        maximumContentWidth: 140,
-        maximumWidth: 140,
-        margin: {
-          bottom: 11
-        }
-      }
-    },
+    logoComponent,
     {
       role: 'body',
       text: 'Get the latest news and alerts delivered right to your inbox',
@@ -39,53 +92,6 @@ module.exports = {
         }
       }
     },
-    {
-      role: 'container',
-      contentDisplay: {
-        type: 'collection',
-        alignment: 'center',
-        distribution: 'narrow',
-        maximumWidth: 190
-      },
-      components: [
-        {
-          role: 'container',
-          contentDisplay: {
-            type: 'horizontal_stack'
-          },
-          style: {
-            backgroundColor: 'white'
-          },
-          layout: {
-            padding: {
-              top: 16,
-              bottom: 16,
-              right: 30,
-              left: 30
-            }
-          },
-          components: [
-            {
-              role: 'image',
-              URL: 'https://via.placeholder.com/26x17',
-              layout: {
-                maximumContentWidth: 26,
-                maximumWidth: 26
-              }
-            },
-            {
-              role: 'body',
-              text: '<a href="https://app.radio.com/apple-news-download"><span data-anf-textstyle="footerCTAStyle">SIGN UP NOW</span></a>',
-              format: 'html',
-              layout: {
-                padding: {
-                  left: 10
-                }
-              }
-            }
-          ]
-        }
-      ]
-    }
+    ctaLink
   ]
 };
