15146eff4e18cfe826b222048adc50f3133e5928
Tiffany Chow <tiffachow@gmail.com>
2019-10-03 16:39:47 -0400

    ON-1055 Put layouts directly

--- migrations/legacy/20190925190000-contest-pg/migration.sh
+++ migrations/legacy/20190925190000-contest-pg/migration.sh
@@ -18,16 +18,24 @@ fi
 
 printf "\n\nCreating Tags 'contests-new' instance and Contest 'new' instance...\n\n\n"
 cat ./_components.yml | clay import -k demo -y -p $1
-printf "\n\nCreating two column layout 'station-contest' and 'national-contest' instances...\n\n\n"
-cat ./_layouts.yml | clay import -k demo -y -p $1
+
+# cat ./_layouts.yml | clay import -k demo -y $1
+layout="_layouts/two-column-layout/instances/national-contest"
+printf "\n\Creating $layout...\n\n"
+curl -X PUT $http://$1/$layout -H 'Authorization: token accesskey' -H 'Content-Type: application/json' -d @./contest-layout-national.json -o /dev/null -s
+curl -X PUT $http://$1/$layout@published -H 'Authorization: token accesskey' -o /dev/null -s
+
+layout="_layouts/two-column-layout/instances/station-contest"
+printf "\n\Creating $layout...\n\n"
+curl -X PUT $http://$1/$layout -H 'Authorization: token accesskey' -H 'Content-Type: application/json' -d @./contest-layout-station.json -o /dev/null -s
+curl -X PUT $http://$1/$layout@published -H 'Authorization: token accesskey' -o /dev/null -s
+
 printf "\n\nCreating 'station-contest' and 'national-contest' pages...\n\n\n"
 cat ./_pages.yml | clay import -k demo -y $1
 
-printf "\nUpdating new-pages _lists to add 'station-contest' and 'national-contest' pages...\n\n"
-
 # _lists/new-pages
 listType="new-pages"
-printf "\n\nUpdating _lists instance $listType...\n\n"
+printf "\nUpdating _lists instance $listType to add 'station-contest' and 'national-contest' pages...\n\n"
 curl -X GET -H "Accept: application/json" $http://$1/_lists/$listType > ./lists-$listType.json
 node ./new-pages-update.js "$1" "$listType";
 cat ./lists-$listType.yml | clay import -k demo -y $1
