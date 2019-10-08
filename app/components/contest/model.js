a45bd6cbff2c9b9233e044f164e9b1c01d595bb2
Tiffany Chow <tiffachow@gmail.com>
2019-10-08 16:18:55 -0400

    ON-1055 Move date range formatting to model

--- app/components/contest/model.js
+++ app/components/contest/model.js
@@ -1,8 +1,32 @@
 'use strict';
 
-const createContent = require('../../services/universal/create-content');
+const createContent = require('../../services/universal/create-content'),
+  moment = require('moment'),
+  dateFormat = require('date-fns/format'),
+  dateParse = require('date-fns/parse'),
+  formatContestDate = data => {
+    let timezone = 'ET';
+    const formatWithTimezone = `MMMM D, YYYY [at] h:mma [${ timezone }]`;
+
+    data.startDateTime = dateFormat(dateParse(data.startDate + ' ' +
+      data.startTime));
+    data.endDateTime = dateFormat(dateParse(data.endDate + ' ' +
+      data.endTime));
+
+    if (data.station) {
+      timezone = data.station.timezone;
+    }
+
+    data.contestDateRange = `${
+      moment(data.startDateTime).format(formatWithTimezone)
+    } through - ${
+      moment(data.endDateTime).format(formatWithTimezone)
+    }`;
+  };
 
 module.exports.render = function (ref, data, locals) {
+  formatContestDate(data);
+
   return createContent.render(ref, data, locals);
 };
 
