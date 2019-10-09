dd7dce9534bf974ca926c35294df55cc4e326f93
Tiffany Chow <tiffachow@gmail.com>
2019-10-09 18:02:50 -0400

    ON-1055 Add styling, es migration

--- app/components/contest/model.js
+++ app/components/contest/model.js
@@ -4,6 +4,11 @@ const createContent = require('../../services/universal/create-content'),
   moment = require('moment'),
   dateFormat = require('date-fns/format'),
   dateParse = require('date-fns/parse'),
+  /**
+   * Format contest dates to include start date and time and end date and time
+   * 
+   * @param {Object} data
+   */
   formatContestDate = data => {
     let timezone = 'ET';
 
@@ -12,15 +17,17 @@ const createContent = require('../../services/universal/create-content'),
     }
     const formatWithTimezone = `MMMM D, YYYY [at] h:mma [${ timezone }]`;
 
-    data.startDateTime = dateFormat(dateParse(data.startDate + ' ' +
-      data.startTime));
-    data.endDateTime = dateFormat(dateParse(data.endDate + ' ' +
-      data.endTime));
-    data.contestDateRange = `${
-      moment(data.startDateTime).format(formatWithTimezone)
-    } through ${
-      moment(data.endDateTime).format(formatWithTimezone)
-    }`;
+    if (data.startDate && data.startTime && data.endDate && data.endTime) {
+      data.startDateTime = dateFormat(dateParse(data.startDate + ' ' +
+        data.startTime));
+      data.endDateTime = dateFormat(dateParse(data.endDate + ' ' +
+        data.endTime));
+      data.contestDateRange = `${
+        moment(data.startDateTime).format(formatWithTimezone)
+      } through ${
+        moment(data.endDateTime).format(formatWithTimezone)
+      }`;
+    }
   };
 
 module.exports.render = function (ref, data, locals) {
