66c48e985a0b4a3cebff1c41318b87fa6ed169f9
Benjamin Leffler <ben.leffler@swarmsolutions.com>
2019-10-17 11:43:56 -0400

    ON-896 | Add default station to constants

--- app/services/universal/constants.js
+++ app/services/universal/constants.js
@@ -21,10 +21,31 @@ const SECOND = 1000,
     ARTICLE: 'article',
     GALLERY: 'gallery',
     SECTIONFRONT: 'section-front'
+  },
+
+  // Default 'station' NATL-RC
+  DEFAULT_STATION = {
+    id: 0,
+    name: 'Radio.com',
+    callsign: 'NATL-RC',
+    website: 'https://www.radio.com',
+    slug: 'www',
+    square_logo_small: 'https://images.radio.com/aiu-media/og_775x515_0.jpg',
+    square_logo_large: 'https://images.radio.com/aiu-media/og_775x515_0.jpg',
+    city: 'New York',
+    state: 'NY',
+    country: 'US',
+    gmt_offset: -5,
+    market: {
+      id: 15,
+      name: 'New York, NY'
+    },
+    category: ''
   };
 
 
 module.exports = {
+  DEFAULT_STATION,
   SECOND,
   MINUTE,
   HOUR,
