3b37ace8fcf8062b8ad8719edb776c3d01919d63
Micah Allen <micah.allen@swarmsolutions.com>
2019-10-28 16:15:45 -0400

    ON-1127 | micah | only use single request for each list

--- app/services/kiln/plugins/brightcove/brightcoveUtils.js
+++ app/services/kiln/plugins/brightcove/brightcoveUtils.js
@@ -1,6 +1,8 @@
 'use strict';
 require('isomorphic-fetch');
 
+let categoriesCache = null;
+
 const { getFetchResponse } = require('../utils/fetch'),
   AD_SUPPORTED = 'AD_SUPPORTED',
   FREE = 'FREE',
@@ -15,26 +17,15 @@ const { getFetchResponse } = require('../utils/fetch'),
     SPORTS,
     NEWS_LIFESTYLE
   ],
-  categoriesCache = {
-    musicEntertainmentSecondary: [],
-    sportsSecondary: [],
-    newsLifestyleSecondary: [],
-    allTertiary: []
-  },
   /**
    * Gets secondary categories from _lists
    * or provide fallback categories
    *
    * @param {string} endpoint
    * @param {string[]} fallbackCategories
-   * @param {string} cacheKey
    * @returns {string[]}
    */
-  getCategoriesFromList = async (endpoint, fallbackCategories, cacheKey) => {
-    if (categoriesCache[cacheKey].length) {
-      return categoriesCache[cacheKey];
-    }
-
+  getCategoriesFromList = async (endpoint, fallbackCategories) => {
     const { status, data } = await getFetchResponse('GET', `${ window.location.origin }/_lists/${ endpoint }`);
 
     let categories = [];
@@ -45,37 +36,44 @@ const { getFetchResponse } = require('../utils/fetch'),
       categories = fallbackCategories;
     }
 
-    categoriesCache[cacheKey] = categories;
-
     return categories;
   },
   /**
-   * Gets all secondary & tertiary categories from _lists
-   *
+   * Gets all secondary & tertiary categories from _lists, if not already retrieved.
    */
   getAllCategoryOptions = async () => {
-    await Promise.all([
-      getCategoriesFromList('brightcove_music_entertainment_secondary_categories', [
-        'awards', 'performance', 'tv', 'streaming', 'digitalvideo', 'film', 'unrelatedentertainment',
-        'pop', 'rock', 'alternative', 'hiphop-r&b', 'country', 'classicrock', 'latino'
-      ], 'musicEntertainmentSecondary'),
-      getCategoriesFromList('brightcove_sports_secondary_categories', [
-        'nfl', 'nhl', 'mlb', 'nba', 'ncaafootball', 'ncaabasketball', 'mma-wwe', 'tennis', 'golf',
-        'soccer', 'unrelatedsports'
-      ], 'sportsSecondary'),
-      getCategoriesFromList('brightcove_news_lifestyle_secondary_categories', [
-        'national', 'lasvegas', 'international', 'losangeles', 'austin', 'madison', 'baltimore', 'memphis', 'bosto.n',
-        'miami', 'buffalo', 'milwaukee', 'charlotte', 'minneapolis', 'chattanooga', 'neworleans', 'chicago', 'newyork',
-        'cleveland', 'norfolk', 'dfw', 'orlando', 'denver', 'phoenix', 'detroit', 'philadelphia', 'gainesville',
-        'pittsburgh', 'greensboro', 'portland', 'greenville', 'providence', 'hartford', 'richmond', 'houston',
-        'riverside', 'indianapolis', 'rochester', 'kansascity', 'sacramento', 'lasvegas', 'sandiego', 'losangeles',
-        'sanfrancisco', 'madison', 'seattle', 'memphis', 'springfield', 'miami', 'stlouis', 'milwaukee',
-        'washingtondc', 'minneapolis', 'wichita', 'neworleans', 'wilkesbarre'
-      ], 'newsLifestyleSecondary'),
-      getCategoriesFromList('brightcove_tertiary_categories', [
-        'food', 'drink', 'travel', 'home', 'health', 'environment'
-      ], 'allTertiary')
-    ]);
+    if (!categoriesCache) {
+      // start loading categories from server
+      categoriesCache = Promise.all([
+        getCategoriesFromList('brightcove_music_entertainment_secondary_categories', [
+          'awards', 'performance', 'tv', 'streaming', 'digitalvideo', 'film', 'unrelatedentertainment',
+          'pop', 'rock', 'alternative', 'hiphop-r&b', 'country', 'classicrock', 'latino'
+        ]),
+        getCategoriesFromList('brightcove_sports_secondary_categories', [
+          'nfl', 'nhl', 'mlb', 'nba', 'ncaafootball', 'ncaabasketball', 'mma-wwe', 'tennis', 'golf',
+          'soccer', 'unrelatedsports'
+        ]),
+        getCategoriesFromList('brightcove_news_lifestyle_secondary_categories', [
+          'national', 'lasvegas', 'international', 'losangeles', 'austin', 'madison', 'baltimore', 'memphis', 'bosto.n',
+          'miami', 'buffalo', 'milwaukee', 'charlotte', 'minneapolis', 'chattanooga', 'neworleans', 'chicago', 'newyork',
+          'cleveland', 'norfolk', 'dfw', 'orlando', 'denver', 'phoenix', 'detroit', 'philadelphia', 'gainesville',
+          'pittsburgh', 'greensboro', 'portland', 'greenville', 'providence', 'hartford', 'richmond', 'houston',
+          'riverside', 'indianapolis', 'rochester', 'kansascity', 'sacramento', 'lasvegas', 'sandiego', 'losangeles',
+          'sanfrancisco', 'madison', 'seattle', 'memphis', 'springfield', 'miami', 'stlouis', 'milwaukee',
+          'washingtondc', 'minneapolis', 'wichita', 'neworleans', 'wilkesbarre'
+        ]),
+        getCategoriesFromList('brightcove_tertiary_categories', [
+          'food', 'drink', 'travel', 'home', 'health', 'environment'
+        ])
+      ]).then(([musicEntertainmentSecondary, sportsSecondary, newsLifestyleSecondary, allTertiary]) => ({
+        musicEntertainmentSecondary,
+        sportsSecondary,
+        newsLifestyleSecondary,
+        allTertiary
+      }));
+    }
+
+    return categoriesCache;
   },
   /**
    * Return secondary category options based on selected
@@ -85,11 +83,12 @@ const { getFetchResponse } = require('../utils/fetch'),
    * @returns {string[]}
    */
   secondaryCategoryOptions = async highLevelCategory => {
-    const categoryMap = {
-      MUSIC_ENTERTAINMENT: categoriesCache.musicEntertainmentSecondary,
-      SPORTS: categoriesCache.sportsSecondary,
-      NEWS_LIFESTYLE: categoriesCache.newsLifestyleSecondary
-    };
+    const categories = await getAllCategoryOptions(),
+      categoryMap = {
+        MUSIC_ENTERTAINMENT: categories.musicEntertainmentSecondary,
+        SPORTS: categories.sportsSecondary,
+        NEWS_LIFESTYLE: categories.newsLifestyleSecondary
+      };
 
     return categoryMap[highLevelCategory] || [];
   },
@@ -98,8 +97,10 @@ const { getFetchResponse } = require('../utils/fetch'),
    *
    * @returns {string[]}
    */
-  tertiaryCategoryOptions = () => {
-    return categoriesCache.allTertiary;
+  tertiaryCategoryOptions = async () => {
+    const categories = await getAllCategoryOptions();
+
+    return categories.allTertiary;
   };
 
 module.exports.AD_SUPPORTED = AD_SUPPORTED;
@@ -108,7 +109,6 @@ module.exports.INFO = INFO;
 module.exports.ERROR = ERROR;
 module.exports.SUCCESS = SUCCESS;
 module.exports.NEWS_LIFESTYLE = NEWS_LIFESTYLE;
-module.exports.getAllCategoryOptions = getAllCategoryOptions;
 module.exports.highLevelCategoryOptions = highLevelCategoryOptions;
 module.exports.secondaryCategoryOptions = secondaryCategoryOptions;
 module.exports.tertiaryCategoryOptions = tertiaryCategoryOptions;
