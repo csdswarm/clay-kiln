bc917bb06562ea97ebd121ea6774223684cf0a52
jeremy <jeremy.zevin@entercom.com>
2019-11-14 01:37:09 -0500

    ON-1141: component settings creation integration
    ground work for global component settings partial or component to be used on article/gallery etc.

--- app/components/event/model.js
+++ app/components/event/model.js
@@ -2,18 +2,56 @@
 
 const
   { unityComponent } = require('../../services/universal/amphora'),
-  createContent = require('../../services/universal/create-content'),
+  // createContent = require('../../services/universal/create-content'),
+  moment = require('moment'),
   { autoLink } = require('../breadcrumbs');
 
+function getFormattedDate(date, time) {
+  return date && time ? moment(`${date} ${time}`).format('LLLL') : '';
+}
+
+
+function getSettingsConfigData() {
+  const emptyHtml = '<span class="circulation--empty">None</span>';
+
+  return {
+    settingsTitle: 'Event Circulation!!!!!',
+    classModifier: '--event-settings',
+    sections: [
+      {
+        dataEditable: 'fooData',
+        sectionTitle: 'Section 1',
+        settings: [
+          {
+            title: 'Setting 1',
+            isRequired: true,
+            templateString: 'Straight Text!'
+          },
+          {
+            title: 'Setting 2',
+            isRequired: false,
+            templateString: `{{{ default startDate '${emptyHtml}' }}}`
+          }
+        ]
+      }
+    ]
+  };
+}
+
 module.exports = unityComponent({
+
   save: (ref, data) => {
     // NOTE: may need to return createContent.render(uri, data, locals);
     return data;
   },
+
   render: (ref, data, locals) => {
+    data._computed.dateTime = getFormattedDate(data.startDate, data.startTime);
+    data._computed.eventCirculationSettings = getSettingsConfigData();
     // NOTE: figure out what is needed here
     autoLink(data, ['stationSlug', '{events}'], locals.site.host);
     // NOTE: may need to return createContent.render(uri, data, locals);
-    return createContent.render(ref, data, locals);;
+    return data;
   }
+
 });
