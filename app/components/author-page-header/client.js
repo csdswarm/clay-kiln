595078d773bbcef73458ab2a86b2c67346c4d1bd
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-11-20 15:27:54 -0600

    ON-1139 update more-content-feed on topic page

--- app/components/author-page-header/client.js
+++ app/components/author-page-header/client.js
@@ -2,12 +2,14 @@
 
 const getRightRail = (el) => {
   const layoutPageHeader = document.querySelector('.layout__page-header'),
-    moreContentRightRail = document.querySelector('.more-content-feed__right-rail'),
+    moreContentRightRail = document.querySelector('.component--more-content-feed-right-rail'),
     authorPageHeaderRightRail = el.querySelector('.author-page-header__right-content');
 
-  layoutPageHeader.style['z-index'] = 4;
+  if (moreContentRightRail) {
+    layoutPageHeader.style['z-index'] = 4;
 
-  authorPageHeaderRightRail.appendChild(moreContentRightRail);
+    authorPageHeaderRightRail.appendChild(moreContentRightRail);
+  }
 };
 
 module.exports = getRightRail;
