b09a531c67e89ba85eab25a06bc1561bbd6acff2
jeremy <jeremy.zevin@entercom.com>
2019-11-22 14:21:45 -0500

    added in Tiffany's changes to allow for event

--- app/components/breadcrumbs/index.js
+++ app/components/breadcrumbs/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const nonWhitespaceBtwnBrackets = /^\{(\S*)\}$/;
+
 /**
  * Takes a text value and converts it to a navigable slug
  *
@@ -47,7 +49,7 @@ function concatValues(arr, prop, separator = '/') {
 function toLinkSegments(data) {
 
   return prop => {
-    const text = data[prop];
+    const text = data[prop] || prop.match(nonWhitespaceBtwnBrackets)[1];
 
     return { segment: slugify(text), text };
   };
@@ -91,10 +93,12 @@ module.exports = {
    * @param {string} host the hostname of the site
    */
   autoLink(data, props, host) {
-    const onlyExistingItems = prop => data[prop];
+    const onlyExistingItemsOrLiterals = prop => {
+      return data[prop] || nonWhitespaceBtwnBrackets.test(prop);
+    };
 
     data.breadcrumbs = props
-      .filter(onlyExistingItems)
+      .filter(onlyExistingItemsOrLiterals)
       .map(toLinkSegments(data))
       .map(toFullLinks(host));
   },
