c7fe78b2606a1656ac063c565df4890183f99819
jeremy <jeremy.zevin@entercom.com>
2019-11-22 16:56:28 -0500

    ON-1142: removal of addStationNoteToCustomUrl

--- app/components/event/kiln.js
+++ app/components/event/kiln.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const addStationNoteToCustomUrl = require('../../services/kiln/add-station-note-to-custom-url'),
-  KilnInput = window.kiln.kilnInput,
+const
   { syncFields, syncHeadlines } = require('../../services/client/kiln-utils');
 
 module.exports = (schema) => {
-  addStationNoteToCustomUrl(new KilnInput(schema));
-
   return syncFields(schema, syncHeadlines('event'));
 };
