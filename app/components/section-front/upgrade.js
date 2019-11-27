409ca82c657de340a38983aedd6ff71d0f54d051
Ricky Harkins <ricky.harkins@swarmsolutions.com>
2019-11-27 09:17:19 -0600

    ON-1139 limit max number of items

--- app/components/section-front/upgrade.js
+++ app/components/section-front/upgrade.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const { getComponentInstance, putComponentInstance } = require('../../services/server/publish-utils'),
+  { getComponentName } = require('clayutils'),
   publishedUri = uri => `${uri}@published`;
 
 module.exports['1.0'] = function (uri, data) {
-  if (!uri.includes('instances/new')) {
+  if (getComponentName(uri) !== 'new') {
     return { ...data, titleLocked: !!data.title, primary: !!data.title };
   }
 
