399aa7794f396a71de85c5fb1e5c0e1e32986739
jchase <jchase@desktop.lan>
2019-12-03 11:32:53 -0500

    DEVOPS-212 eslint fixes

--- app/local.config.js
+++ app/local.config.js
@@ -1,26 +1,28 @@
+'use strict';
+
 module.exports = {
-    apps : [{
-      name: "app",
-      script: "./app.js",
-      watch: [
-        "app.js",
-        "components",
-        "global/kiln-js",
-        "search",
-        "services",
-        "sites",
-        "public",
-        "routes",
-        "layouts"
-      ],
-      ignore_watch: [
-        "**/*.test.js",
-        "components/**/client.js"
-      ],
-      watch_delay: 3000,
-      env: {
-        NODE_ENV: "local",
-      },
-      max_memory_restart: "256M"
-    }]
-  }
+  apps : [{
+    name: 'app',
+    script: './app.js',
+    watch: [
+      'app.js',
+      'components',
+      'global/kiln-js',
+      'search',
+      'services',
+      'sites',
+      'public',
+      'routes',
+      'layouts'
+    ],
+    ignore_watch: [
+      '**/*.test.js',
+      'components/**/client.js'
+    ],
+    watch_delay: 3000,
+    env: {
+      NODE_ENV: 'local'
+    },
+    max_memory_restart: '256M'
+  }]
+};
