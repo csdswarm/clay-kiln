8869fba911d8b19fa3ddd080aa1916abd0ff6dc3
Tiffany Chow <tiffachow@gmail.com>
2019-12-04 17:27:20 -0500

    ON-1232 Create generic page header

---
+++ app/components/page-header/model.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports.render = (ref, data, locals) => {
+  // If we're publishing for a dynamic page, switch out -'s with spaces
+  let title = '';
+
+  if (locals && locals.params) {
+    title = locals.params.tag || locals.params.dynamicTag;
+  }
+  data.title = !data.title && locals && title ? title.replace(/-/g, ' ').replace(/\//g,'').replace(/%26/g, '&') : data.title;
+  return data;
+};
+
+module.exports.save = function (ref, data) {
+  // make sure all of the numbers we need to save aren't strings
+  if (data.size) {
+    data.size = parseInt(data.size, 10);
+  }
+
+  return data;
+};
