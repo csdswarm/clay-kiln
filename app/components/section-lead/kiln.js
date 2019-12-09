ef26e5cb8b7f01942a9f4843b82db37ea45a4433
jeremy <jeremy.zevin@entercom.com>
2019-12-09 15:11:19 -0500

    ON-1177: | jz | lint fix

--- app/components/section-lead/kiln.js
+++ app/components/section-lead/kiln.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const KilnInput = window.kiln.kilnInput;
-
 module.exports = schema => {
   // TODO: handle setting max length of items to 2 when in multi-column mode and hide content label when it's not
   return schema;
