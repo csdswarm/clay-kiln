4a0c32028ff676bcb531a650696787c33c46c679
Benjamin Leffler <ben.leffler@swarmsolutions.com>
2020-01-21 11:23:30 -0500

    ON-1352 | Simplify isDesktop  - Client services should only be used client side  - Don't bother with a SERVER_SIDE check

--- app/services/client/isDesktop.js
+++ app/services/client/isDesktop.js
@@ -1,14 +1,8 @@
 'use strict';
 
-const { SERVER_SIDE } = require('../universal/constants'),
-  MEDIUM_SCREEN_WIDTH = 1023,
+const MEDIUM_SCREEN_WIDTH = 1023,
   isDesktop = () => {
-    if (!SERVER_SIDE) {
-      return !window.matchMedia(`(max-width: ${MEDIUM_SCREEN_WIDTH}px)`).matches;
-    } else {
-      // Node context, not desktop
-      return false;
-    }
+    return !window.matchMedia(`(max-width: ${MEDIUM_SCREEN_WIDTH}px)`).matches;
   };
 
 module.exports = isDesktop;
