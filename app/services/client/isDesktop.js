7e2e6a873771e4a179357768eb1e8fa8a96d2a75
Benjamin Leffler <ben.leffler@swarmsolutions.com>
2020-01-21 10:35:07 -0500

    ON-1352 | Fix isDesktop  - Only check window if it's NOT server side

--- app/services/client/isDesktop.js
+++ app/services/client/isDesktop.js
@@ -3,7 +3,7 @@
 const { SERVER_SIDE } = require('../universal/constants'),
   MEDIUM_SCREEN_WIDTH = 1023,
   isDesktop = () => {
-    if (SERVER_SIDE) {
+    if (!SERVER_SIDE) {
       return !window.matchMedia(`(max-width: ${MEDIUM_SCREEN_WIDTH}px)`).matches;
     } else {
       // Node context, not desktop
