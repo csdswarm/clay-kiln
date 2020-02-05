0aa15c9622d868905a4c724a86ca4a6ee275bc24
Philip Olson <phil.olson@swarmsolutions.com>
2020-02-05 12:08:36 -0600

    ON-892: add static-page to list

--- app/services/kiln/apply-content-logic.js
+++ app/services/kiln/apply-content-logic.js
@@ -13,7 +13,11 @@ const addStationNoteToCustomUrl = require('./add-station-note-to-custom-url'),
   KilnInput = window.kiln.kilnInput,
   // these components require specific permissions to publish.  The other
   //   content components restrict publishing based off station access.
-  componentsToCheckPublishPermission = new Set(['homepage', 'section-front']);
+  componentsToCheckPublishPermission = new Set([
+    'homepage',
+    'section-front',
+    'static-page'
+  ]);
 
 module.exports = schema => {
   addStationNoteToCustomUrl(new KilnInput(schema));
