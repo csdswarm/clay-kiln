81c4bcebc371033f022cf64f4016053ee8218046
Tiffany Chow <tiffachow@gmail.com>
2020-02-11 12:38:37 -0500

    ON-1385 Rm episode and trending data from pg

--- app/components/podcast-show-page/model.js
+++ app/components/podcast-show-page/model.js
@@ -2,9 +2,7 @@
 
 const { unityComponent } = require('../../services/universal/amphora'),
   radioApiService = require('../../services/server/radioApi'),
-  _get = require('lodash/get'),
-  url = require('url'),
-  qs = require('qs');
+  _get = require('lodash/get');
   // { autoLink } = require('../breadcrumbs');
 
 /**
@@ -20,52 +18,6 @@ function getPodcastShow(locals) {
   });
 }
 
-/**
- * fetch episodes from a podcast show
- * @param {object} locals
- * @returns {Promise<array>}
- */
-function getEpisodesInShow(locals) {
-  const route = 'episodes',
-    querystring = url.parse(locals.url).query,
-    { sort = 'newest', page = 1 } = qs.parse(querystring),
-    params = {
-      'filter[podcast_id]': locals.podcast.id,
-      'page[size]': 20,
-      'page[number]': page
-    };
-
-  switch (sort) {
-    case 'oldest':
-      params.sort = 'published_date';
-      break;
-    case 'newest':
-    default:
-      params.sort = '-published_date';
-  };
-
-  return radioApiService.get(route, params, null, {}, locals).then(response => {
-    return response.data || [];
-  });
-}
-
-/**
- * fetch episodes from a podcast show
- * @param {object} locals
- * @returns {Promise<array>}
- */
-function getPopularPodcasts(locals) {
-  const route = 'podcasts',
-    params = {
-      'page[size]': 6,
-      sort: '-popularity'
-    };
-
-  return radioApiService.get(route, params, null, {}, locals).then(response => {
-    return response.data || [];
-  });
-}
-
 module.exports = unityComponent({
   render: async (uri, data, locals) => {
     if (!locals || !locals.params) {
@@ -75,20 +27,7 @@ module.exports = unityComponent({
     // Stored in data for breadcrumbs component
     data.stationSlug = _get(locals, 'params.stationSlug');
 
-    [locals.podcast, locals.trendingPodcasts] = await Promise.all( [
-      getPodcastShow(locals),
-      getPopularPodcasts(locals)
-    ]);
-
-    locals.episodes = await getEpisodesInShow(locals);
-
-    data = {
-      ...data,
-      podcast: {
-        ...locals.podcast,
-        episodes: locals.episodes
-      }
-    };
+    data.podcast = locals.podcast = await getPodcastShow(locals);
 
     // @TODO breadcrumbs to be done in ON-446
     // autoLink(data, ['stationSlug', '{podcasts}', 'podcast.slug'], locals.site.host);
