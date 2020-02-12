57005a09c854abdac850a547aa17b36811a47571
Chris Strolia-Davis <chris.strolia-davis@swarmsolutions.com>
2020-02-12 14:26:29 -0500

    ON-1415 | csd | refactor to be more understandable and flexible
        * Split into two files, one that converts and one that restores, so already modified files need not be modified again
        * Handle cleaning DB directly, so backup does not need the -c option
        * Add query that handles updating URIs directly to the backup so that it need not be a separate thing added during import.
        * handle specifying any location that the backup is, rather than requiring the user to save to the utility/backups folder first
        * Add ability to handle bzip and gzipped backups
        * Add ability to specify a different target db

--- utility/import_db.sh
+++ utility/import_db.sh
@@ -1,62 +1,42 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
-BACKUPS_DIR="$SCRIPT_DIR/backups"
-MODIFIED_DIR="$BACKUPS_DIR/modified"
-
-# create backups and modified directories if they don't already exist
-# although the assumption is that the first argument is the name of the file in backups, so that dir should probably
-# already exist
-mkdir -p "$MODIFIED_DIR"
-
-BACKUP="$BACKUPS_DIR/$1"
-MODIFIED="$MODIFIED_DIR/$1"
-
-SEARCH="$2"
-REPLACE=${3:-clay.radio.com}
-REPLACE_HTTPS=${4:-n}
-
-echo Creating Modified Backup
-cp -f "$BACKUP" "$MODIFIED"
-
-# Delete data rows that include clay.radio.com as the id (e.g. start with clay.radio.com)
-sed -i '' '/^clay\.radio\.com/d' "$MODIFIED"
-
-if [ "$(echo "$REPLACE_HTTPS" | tr '[A-Z]' '[a-z]')" = "y" ]; then
-  sed -i '' "s/https\:\/\/$SEARCH/http:\/\/$REPLACE/g" "$MODIFIED"
-fi
-
-# replace all data in backup with the replace value
-sed -i '' "s/$SEARCH/$REPLACE/g" "$MODIFIED"
-
-
-echo "Modified backup created: $MODIFIED"
-
-# The following assumes we are doing this in the root of a local dev environment
-# maybe it should be put into a separate script so that different scripts can handle this per env
-# besides, the backup may have already been fixed, so why fix it again if not needed.
-
 PG_TEMP="$SCRIPT_DIR/../postgres/data/temp.sql"
 
+# get absolute path of target
+MODIFIED="$( cd "${1%/*}"; pwd -P )/$( basename "$1" )"
+
+DB=${2:-clay}
+
 # verify that the postgres container is running
-if [ "$(docker inspect -f '{{.State.Running}}' clay-radio_postgres_1)" = "false" ]; then
+POSTGRES_ID="$(docker ps -aq --filter name=^$(basename $(pwd)_postgres))"
+
+if [ "$(docker inspect -f '{{.State.Running}}' "$POSTGRES_ID")" = "false" ]; then
+  # send an error message to stderr and exit with a non-0 exit #
   echo postgres container is not running. Please start it and try again. 1>&2
   exit 1;
 fi;
 
-echo Restoring backup
+echo Beginning Restore
 
 # Copy the backup to the postgres/data
+echo Copied backup to docker data folder
 cp -f "$MODIFIED" "$PG_TEMP"
 
-docker-compose exec postgres psql -q -U postgres -d clay -f "/var/lib/postgresql/data/temp.sql"
+echo Disconnecting all users
+# make sure to disconnect all users or we won't be able to drop and recreate our db (make sure to set the db to postgres so we aren't connected to the target when we try to disconnect everyone)
+docker-compose exec postgres psql -U postgres -c "UPDATE pg_database SET datallowconn='false' WHERE datname='$DB'; SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$DB' AND pid <> pg_backend_pid();" -d postgres > /dev/null 2>&1
 
-echo "Modifying uris table (post clean operation to handle base 64 encoded ids)"
-docker-compose exec postgres psql -q -U postgres -d clay -c "UPDATE public.uris SET id = '$REPLACE/_uris/'||ENCODE(CONVERT_TO(url, 'UTF-8'), 'BASE64');"
+echo Cleaning up and/or creating new dbs as needed.
+# drop and recreate the existing DB first to clear it. May error out if db does not exist, but this does not matter
+docker-compose exec postgres dropdb -U postgres "$DB" > /dev/null 2>&1
+docker-compose exec postgres createdb -U postgres "$DB" > /dev/null 2>&1
 
+echo Restoring the data
+docker-compose exec postgres psql -q -U postgres -f "/var/lib/postgresql/data/temp.sql" "$DB" > /dev/null 2>&1
+
+echo cleaning up
 # cleanup after ourselves
-unlink "$PG_TEMP"
+rm -f "$PG_TEMP"
 
 echo Restore complete!
-
-#docker-compose exec postgres pg_dump -c --if-exists -O -h "cmsdb-write.radio-stg.com" -w -U "dbadmin" cms -f "/var/lib/postgresql/data/backup.sql"
