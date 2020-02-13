3338c87fd1f83989fe20654c85e26954b4383eda
Benjamin Leffler <ben.leffler@swarmsolutions.com>
2020-02-13 11:12:39 -0500

    ON-1174 | PR Feedback  - Remove addStationNoteToCustomUrl from author-page kiln.js  - Add data-station-slug property to author-page template

--- app/components/author-page/kiln.js
+++ app/components/author-page/kiln.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const addStationNoteToCustomUrl = require('../../services/kiln/add-station-note-to-custom-url'),
-  handleEditModePlaceholders = require('../../services/kiln/handle-edit-mode-placeholders'),
+const handleEditModePlaceholders = require('../../services/kiln/handle-edit-mode-placeholders'),
   KilnInput = window.kiln.kilnInput;
 
 module.exports = (schema) => {
-  addStationNoteToCustomUrl(new KilnInput(schema));
   handleEditModePlaceholders(new KilnInput(schema));
 
   return schema;
