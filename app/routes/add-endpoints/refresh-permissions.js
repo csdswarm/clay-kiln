3669b4676e1d8db16f50dc90e1af4fe2b6bd81f3
Philip Olson <phil.olson@swarmsolutions.com>
2020-02-14 00:56:57 -0600

    ON-1483: update refresh-permissions
    - the refresh-permissions endpoint now calls a specific method inside
      server/urps which only makes the calls necessary

--- app/routes/add-endpoints/refresh-permissions.js
+++ app/routes/add-endpoints/refresh-permissions.js
@@ -1,21 +1,24 @@
 'use strict';
 
-const { wrapInTryCatch } = require('../../services/startup/middleware-utils'),
-  urps = require('../../services/server/urps');
+const _get = require('lodash/get'),
+  { wrapInTryCatch } = require('../../services/startup/middleware-utils'),
+  urps = require('../../services/server/urps'),
+  refreshPath = '/rdc/refresh-permissions';
 
-module.exports = (router, checkAuth) => {
-  router.get('/rdc/refresh-permissions', checkAuth, wrapInTryCatch(async (req, res) => {
-    const { provider } = res.locals.user;
+module.exports = router => {
+  router.get(refreshPath, wrapInTryCatch(async (req, res) => {
+    const { provider } = _get(res, 'locals.user', {});
 
     if (provider !== 'cognito') {
-      res.status(400);
+      res.status(403);
       res.send('you must be logged into cognito for this endpoint to work');
       return;
     }
 
-    const auth = req.session.auth;
+    await urps.refreshPermissions(req.session.auth);
 
-    auth.permissions = await urps.getAllPermissions(auth.token);
     res.send('success');
   }));
 };
+
+Object.assign(module.exports, { refreshPath });
