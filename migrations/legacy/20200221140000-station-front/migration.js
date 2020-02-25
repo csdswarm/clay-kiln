7449fdfb692fa90e85f5817f073fd0bbf8b52d52
Micah Allen <micah.allen@swarmsolutions.com>
2020-02-25 10:16:14 -0500

    ON-1450 | micah | create elasticsearch index

--- migrations/legacy/20200221140000-station-front/migration.js
+++ migrations/legacy/20200221140000-station-front/migration.js
@@ -3,11 +3,13 @@
 const createNewComponentInstance = require('./component-new-instance');
 const createPage = require('./create-page');
 const updateNewPages = require('./update-new-pages');
+const createESIndex = require('./create-elasticsearch-index');
 
 const host = process.argv[2] || 'clay.radio.com';
 
 Promise.all([
   createPage(host),
   updateNewPages(host),
-  createNewComponentInstance(host)
+  createNewComponentInstance(host),
+  createESIndex(host)
 ]);
