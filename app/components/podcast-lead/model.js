4c05f12ca98298a92457c47091d697f0c5f65736
jeremy <jeremy.zevin@entercom.com>
2020-03-02 15:37:15 -0500

    ON-1383: jz | improved model/view for missing data at this point it isn't clear how we will get the share and subscription info necessary to create the view required by design therefore, to show the design and code for the future I am creating two props _computed.shares and _computed.subscriptions that the model will have to compute once integrated

--- app/components/podcast-lead/model.js
+++ app/components/podcast-lead/model.js
@@ -3,7 +3,25 @@
 const
   { unityComponent } = require('../../services/universal/amphora'),
   rest = require('../../services/universal/rest'),
-  _get = require('lodash/get');
+  _get = require('lodash/get'),
+  qs = require('querystring'),
+  { DEFAULT_RADIOCOM_LOGO } = require('../../services/universal/constants');
+
+/**
+ * Returns a query string with all that you provide or some defaults
+ *
+ * @param {object} params - object of parmas
+ *
+ * @returns {string}
+ */
+function getUtmTracking(params = {}) {
+  return `?${qs.stringify({
+    utm_campaign: params.utm_campaign || 'sharebutton',
+    utm_medium: params.utm_medium || 'social',
+    utm_term: params.utm_term || 'NATL-RC',
+    ...params
+  })}`;
+}
 
 module.exports = unityComponent({
   /**
@@ -16,31 +34,48 @@ module.exports = unityComponent({
    * @returns {object}
    */
   render: (uri, data, locals) => {
-    // NOTE: this is just for testing purposes and should be removed upon integration with page
-    // in fact there shouldn't even be a model.js as this component will be a partial
     const
-      id = _get(locals, 'query.id', [8,11,13,14,160,162,165].sort(() => Math.random() < 0.5 ? 1 : -1)[0]);
+      id = _get(locals, 'query.id', [8,11,13,14,160,162,165,246].sort(() => Math.random() < 0.5 ? 1 : -1)[0]), // temporary for FE dev/testing will come from page once integrated
+      domain = _get(locals, 'site.host'),
+      stationCallSign = _get(locals, 'station.callsign');
 
-    console.log('locals.query.id'. id);
     return rest.get(`https://api.radio.com/v1/podcasts/${id}`)
       .then(response => {
         data._computed.podcast = response.data;
-        data._computed.podcast.attributes.shares = ['facebook', 'twitter', 'email'];
+        // at this point it isn't clear how we will get the share and subscription info necessary to create the view required by design therefore, to show the design and code for the future I am creating two props _computed.shares and _computed.subscriptions that the model will have to compute like so:
+        data._computed.shares = [
+          { name: 'email',
+            link: `mailto:${getUtmTracking({
+              subject: data._computed.podcast.attributes.title,
+              body: `${locals.url}${getUtmTracking({
+                utm_source: domain,
+                utm_medium: 'email',
+                utm_term: stationCallSign
+              })}`
+            })}`
+          },
+          { name: 'facebook',
+            link: `http://www.facebook.com/sharer/sharer.php${getUtmTracking({
+              u: locals.url,
+              utm_source: 'facebook.com'
+            })}`
+          },
+          { name: 'twitter',
+            link: `https://twitter.com/share${getUtmTracking({
+              text: data._computed.podcast.attributes.title,
+              via: data._computed.podcast.attributes.twitterHandle | undefined,
+              utm_source: 'twitter.com'
+            })}`
+          }
+        ];
+        data._computed.subscriptions = {
+          google: 'https://google.com',
+          apple: 'https://google.com',
+          rss: data._computed.podcast.attributes.rss_feed
+        };
+        data._computed.DEFAULT_RADIOCOM_LOGO = DEFAULT_RADIOCOM_LOGO;
         return data;
       })
       .catch( () => data);
-  },
-
-  /**
-   * Makes any necessary modifications to data just prior to persisting it
-   *
-   * @param {string} uri - The uri of the component instance
-   * @param {object} data - persisted or bootstrapped data for this instance
-   * @param {object} locals - data that has been attached to express locals for the current page request
-   *
-   * @returns {object}
-   */
-  save: (uri, data) => {
-    return data;
   }
 });
