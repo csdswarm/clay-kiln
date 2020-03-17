1cde0576b77faaebf6e3eb8da340f30c307053bb
Philip Olson <phil.olson@swarmsolutions.com>
2020-03-17 14:25:24 -0500

    ON-1599: pr feedback
    remove the trigger and default timestamp in order to keep the origin
    of the data on the server rather than a combination of the server
    and db

--- migrations/legacy/20200314195137-national-subscription-table/migration.js
+++ migrations/legacy/20200314195137-national-subscription-table/migration.js
@@ -10,32 +10,10 @@ usingDb(async db => {
       station_slug varchar,
       -- timestamptz(0) <-- 0 here is the precision that means no fractions of
       -- a second
-      last_updated_utc timestamptz(0) default (now() at time zone 'utc'),
+      last_updated_utc timestamptz(0),
       filter jsonb
     )
   `)
 
-  // the following two queries were mostly grabbed from SO
-  // https://stackoverflow.com/a/9556527
-
-  await db.query(`
-    create or replace function update_last_updated_utc()
-    returns trigger as $func$
-    begin
-      new.last_updated_utc = now() at time zone 'utc';
-      return new;
-    end
-    $func$ language plpgsql;
-  `)
-
-  await db.query(`
-    create trigger update_last_updated_utc
-    before update
-    on national_subscriptions
-    for each row execute procedure update_last_updated_utc();
-  `)
-
-  console.log(
-    'done creating national_subscriptions and the auto-update trigger'
-  );
+  console.log('done creating national_subscriptions');
 })
