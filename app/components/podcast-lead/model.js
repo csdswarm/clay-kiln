0a9019e8fbeaeae593b7d9ee4ea3c9d4d7399da8
jeremy <jeremy.zevin@entercom.com>
2020-03-23 12:59:14 -0400

    ON-1376: jz | address PR comments

--- app/components/podcast-lead/model.js
+++ app/components/podcast-lead/model.js
@@ -38,11 +38,15 @@ module.exports = unityComponent({
     }
 
     data._computed.podcast = locals.podcast;
-    // at this point it isn't clear how we will get the share and subscription info necessary to create the view required by design therefore, to show the design and code for the future I am creating two props _computed.shares and _computed.subscriptions that the model will have to compute like so:
+    // at this point it isn't clear how we will get the share and subscription info necessary to
+    // create the view required by design therefore, to show the design and code for the future I am
+    // creating two props _computed.shares and _computed.subscriptions that the model will have to compute like so:
+    const computedPodcast = data._computed.podcast;
+
     data._computed.shares = [
       { name: 'email',
         link: `mailto:${getUtmTracking({
-          subject: data._computed.podcast.attributes.title,
+          subject: computedPodcast.attributes.title,
           body: `${locals.url}${getUtmTracking({
             utm_source: locals.site.host,
             utm_medium: 'email',
@@ -58,16 +62,16 @@ module.exports = unityComponent({
       },
       { name: 'twitter',
         link: `https://twitter.com/share${getUtmTracking({
-          text: data._computed.podcast.attributes.title,
-          via: data._computed.podcast.attributes.twitterHandle | undefined,
+          text: computedPodcast.attributes.title,
+          via: computedPodcast.attributes.twitterHandle | undefined,
           utm_source: 'twitter.com'
         })}`
       }
     ];
     data._computed.subscriptions = {
       google: 'https://google.com',
-      apple: 'https://google.com',
-      rss: data._computed.podcast.attributes.rss_feed
+      apple: 'https://apple.com',
+      rss: computedPodcast.attributes.rss_feed
     };
     data._computed.DEFAULT_RADIOCOM_LOGO = DEFAULT_RADIOCOM_LOGO;
     return data;
