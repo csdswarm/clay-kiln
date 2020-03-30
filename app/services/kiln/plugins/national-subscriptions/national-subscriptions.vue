6b76d23e7491c07b8fae92c432f9fedc796880fa
Benjamin Leffler <ben.leffler@swarmsolutions.com>
2020-03-30 15:19:07 -0400

    ON-1602 | Clean up national-subscriptions drawer  - Shorten lines  - Put items in an ordered list proper  - Rename date filter to be less misleading

--- app/services/kiln/plugins/national-subscriptions/national-subscriptions.vue
+++ app/services/kiln/plugins/national-subscriptions/national-subscriptions.vue
@@ -2,27 +2,36 @@
 <template>
   <div class="national-subscriptions">
     <div class="page-list-headers national-subscriptions__header">
-        <span class="national-subscriptions__page-list-headers page-list-header page-list-headers__description">Description</span>
-        <span class="national-subscriptions__page-list-headers page-list-header page-list-headers__updated">Last Updated</span>
+        <span class="national-subscriptions__page-list-headers
+          page-list-header page-list-headers__description"
+        >Description</span>
+        <span class="national-subscriptions__page-list-headers
+          page-list-header page-list-headers__updated"
+        >Last Updated</span>
     </div>
     <div class="page-list-readout">
-      <div :key="index"
-          v-for="(subscription, index) in subscriptions"
-      >
-        <div class="page-list-item">
-          <h4 class="national-subscriptions__page-list-item__description">{{subscription.short_desc}}</h4>
-          <span class="national-subscriptions__page-list-item__updated"><time>{{ subscription.last_updated_utc | localDate }}</time></span>
-        </div>
-        <div :key="propIndex"
-          class="page-list-item"
-          v-for="(prop, propIndex) in subscription.filter"
-        >
-          <dl class="national-subscriptions__page-list-item__filter">
-            <dt>{{ propIndex | startCase }}</dt>
-            <dd>{{ prop | valList }}</dd>
-          </dl>
-        </div>
-      </div>
+      <ol>
+        <li :key="index"
+          v-for="(subscription, index) in subscriptions">
+          <div class="page-list-item">
+            <h4 class="national-subscriptions__page-list-item__description"
+            >{{subscription.short_desc}}</h4>
+            <span class="national-subscriptions__page-list-item__updated"
+            >
+              <time>{{ subscription.last_updated_utc | formatDate }}</time>
+            </span>
+          </div>
+          <div :key="propIndex"
+            class="page-list-item"
+            v-for="(prop, propIndex) in subscription.filter"
+          >
+            <dl class="national-subscriptions__page-list-item__filter">
+              <dt>{{ propIndex | startCase }}</dt>
+              <dd>{{ prop | valList }}</dd>
+            </dl>
+          </div>
+        </li>
+      </ol>
     </div>
   </div>
 </template>
@@ -47,7 +56,7 @@
     methods: {},
     components: {},
     filters: {
-      localDate: utcDateStr => moment(utcDateStr).format('MMM Do, YYYY, h:mm:ss a'),
+      formatDate: utcDateStr => moment(utcDateStr).format('MMM Do, YYYY, h:mm:ss a'),
       startCase,
       valList: val => Array.isArray(val) ? val.join(', ') : val
     }
