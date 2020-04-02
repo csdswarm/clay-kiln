48c29e990fb55881149c7b1421857972a54ed9ab
Philip Olson <phil.olson@swarmsolutions.com>
2020-04-01 23:39:02 -0500

    Revert "Revert "ON-1377: fix metaLocalsKey upgrade""
    This reverts commit c848b6f326dc13891e763965c29408bacccfb64c.

--- app/components/dynamic-meta-title/upgrade.js
+++ app/components/dynamic-meta-title/upgrade.js
@@ -22,9 +22,10 @@ module.exports['2.0'] = (uri, data) => {
 // key is the wrong term because 'path' is the term lodash employs
 module.exports['3.0'] = (uri, data) => {
   data.localsPath = data.localsPath || data.localsKey;
-  data.metaLocalsPath = data.metaLocalsPath || data.metaLocalsPath;
+  data.metaLocalsPath = data.metaLocalsPath || data.metaLocalskey;
 
   delete data.localsKey;
+  delete data.metaLocalsKey;
 
   return data;
 };
