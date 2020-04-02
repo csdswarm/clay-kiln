d5fb1519360edefe78b42eeda42db2eeead4e341
Philip Olson <phil.olson@swarmsolutions.com>
2020-04-01 22:13:30 -0500

    Revert "ON-1377: remove unnecessary async"
    This reverts commit 1ff068da1c9e5a28160f513b2dc3be011f570b75.

--- app/components/dynamic-meta-image/upgrade.js
+++ app/components/dynamic-meta-image/upgrade.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // key is the wrong term because 'path' is the term lodash employs
-module.exports['1.0'] = (uri, data) => {
+module.exports['1.0'] = async (uri, data) => {
   data.localsPath = data.localsPath || data.localsKey;
 
   delete data.localsKey;
