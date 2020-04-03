450937e7e9c954fb5b6af85847544fb4a6070aaf
Nick Renford <nick.renford@entercom.com>
2020-04-03 14:16:23 -0600

    Remove unused code and files Add definitions in schema files

--- app/components/podcast-set/model.js
+++ app/components/podcast-set/model.js
@@ -48,18 +48,5 @@ module.exports = unityComponent({
         return data;
       });
 
-  },
-
-  /**
-   * Makes any necessary modifications to data just prior to persisting it
-   *
-   * @param {string} uri - The uri of the component instance
-   * @param {object} data - persisted or bootstrapped data for this instance
-   * @param {object} locals - data that has been attached to express locals for the current page request
-   *
-   * @returns {object}
-   */
-  save: (uri, data) => {
-    return data;
   }
 });
