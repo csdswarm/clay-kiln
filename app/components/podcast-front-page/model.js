36777ebdb66aa992e6d3e55989b4d1d56359dfdf
Tiffany Chow <tiffachow@gmail.com>
2020-04-07 18:39:36 -0400

    ON-1561 Rm unused render func

--- app/components/podcast-front-page/model.js
+++ app/components/podcast-front-page/model.js
@@ -3,16 +3,4 @@
 const { unityComponent } = require('../../services/universal/amphora');
 
 module.exports = unityComponent({
-  /**
-   * Updates the data for the template prior to render
-   *
-   * @param {string} uri - The uri of the component instance
-   * @param {object} data - persisted or bootstrapped data for this instance
-   * @param {object} locals - data that has been attached to express locals for the current page request
-   *
-   * @returns {object}
-   */
-  render: (uri, data) => {
-    return data;
-  }
 });
