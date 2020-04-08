1f3446823a11b70c50f59330e6016e23319f10c0
Cristhiam David Henao Moreno <cristhiam.henao@Cristhiam-Henao-MacBook-Pro.local>
2020-04-08 12:02:42 -0500

    ON-1691-load more button logic fixed

--- app/services/universal/constants.js
+++ app/services/universal/constants.js
@@ -65,7 +65,9 @@ const _get = require('lodash/get'),
     category: ''
   },
 
-  DEFAULT_RADIOCOM_LOGO = 'https://images.radio.com/aiu-media/og_775x515_0.jpg';
+  DEFAULT_RADIOCOM_LOGO = 'https://images.radio.com/aiu-media/og_775x515_0.jpg',
+
+  LOAD_MORE_LIMIT = 10;
 
 
 module.exports = {
@@ -80,5 +82,6 @@ module.exports = {
   SERVER_SIDE,
   PAGE_TYPES,
   DEFAULT_RADIOCOM_LOGO,
-  DEFAULT_STATION
+  DEFAULT_STATION,
+  LOAD_MORE_LIMIT
 };
