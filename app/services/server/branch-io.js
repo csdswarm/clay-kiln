d50ae40dd2924bd70809d20a6cc4d79fbc5cdb83
Tiffany Chow <tiffachow@gmail.com>
2020-04-10 15:36:34 -0400

    ON-1497 Update national pages to only add station_name metatag

--- app/services/server/branch-io.js
+++ app/services/server/branch-io.js
@@ -53,23 +53,16 @@ const db = require('./db'),
       addTag('station_logo', _get(locals, 'station.square_logo_small'));
       addTag('page', 'station-detail');
     } else {
-      if (isStation) {
+      // article, gallery, section front pages
+      if (isStation) { // under a station
         addTag('market', _get(locals, 'station.market.display_name'));
         addTag('category', _get(locals, 'station.category'));
         _get(locals, 'station.genre', []).forEach(genre => addTag('genre', genre.name));
         addTag('station_id', _get(locals, 'station.id'));
         addTag('station_name', _get(locals, 'station.name'));
         addTag('station_logo', _get(locals, 'station.square_logo_small'));
-      } else {
-        /* placeholder code -
-        *  waiting for product to confirm values for national pages
-        */
-        addTag('market', _get(locals, 'station.market.name'));
-        addTag('category', 'national');
-        addTag('genre', 'national');
-        addTag('station_id', _get(locals, 'station.id'));
+      } else { // national page
         addTag('station_name', _get(locals, 'station.name'));
-        addTag('station_logo', _get(locals, 'station.square_logo_small'));
       }
     }
 
