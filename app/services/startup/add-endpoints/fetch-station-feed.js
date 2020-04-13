0f22e2134553af52edd0370a2f147e68f9a9fdd1
Luis Felipe Bertel Mercado <luis.bertel@gorillalogic.com>
2020-04-13 07:05:01 -0500

    ON-1678 added schema validations.

--- app/services/startup/add-endpoints/fetch-station-feed.js
+++ app/services/startup/add-endpoints/fetch-station-feed.js
@@ -3,12 +3,19 @@
 const { wrapInTryCatch } = require('../middleware-utils'),
   axios = require('axios');
 
+/**
+ * Exposes an endpoint '/rdc/fetch-station-feeds' to actually fetch the information from the Frequency RSS Feed.
+ *
+ * 'rdc' here is a namespace convention defined previously to avoid collision with the domain of other teams.
+ *
+ * @param {object} router
+ */
+
 module.exports = router => {
-  router.get('/rdc/fetch-station-feeds', wrapInTryCatch(async (req, res) => {
+  router.get('/rdc/etch-station-feeds', wrapInTryCatch(async (req, res) => {
     const { feedURL } = req.query,
       resp = await axios.get(`${feedURL}`);
-      
-    console.log('DEBUG: API CALL______________PARAMS', feedURL);
+
     res.send(resp.data);
   }));
 };
