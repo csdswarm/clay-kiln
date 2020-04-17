5d7fb70a231e027fc666cb4e6fbb9d443640df25
Juan David Restrepo Toro <juan.restrepo@gorillalogic.com>
2020-04-17 10:25:23 -0500

    ON-1047 Fixed domain

--- app/components/share/model.js
+++ app/components/share/model.js
@@ -12,15 +12,17 @@ const _get = require('lodash/get'),
 
 module.exports.render = (ref, data, locals) => {
   const title = encodeURIComponent(striptags(data.shortTitle || data.title)),
-    domain = _get(locals, 'site'),
+    domain = _get(locals, 'site.host'),
+    stationSlug = _get(locals, 'station.site_slug', ''),
+    emailDomain = stationSlug ? `${domain}/${stationSlug}` : domain,
     stationTwitter = _get(locals, 'station.twitter'),
     stationCallSign = _get(locals, 'station.callsign');
 
   data.twitterHandle = stationTwitter || data.twitterHandle;
- 
+
   return {
     ...data,
-    emailUrl: `mailto:?subject=${title}&body=${buildUTMUrl(locals.url, { utm_source: domain, utm_medium: 'email', utm_term: stationCallSign })}`,
+    emailUrl: `mailto:?subject=${title}&body=${buildUTMUrl(locals.url, { utm_source: emailDomain, utm_medium: 'email', utm_term: stationCallSign })}`,
     facebookUrl: `http://www.facebook.com/sharer/sharer.php?u=${buildUTMUrl(locals.url, { utm_source: 'facebook.com', utm_term: stationCallSign })}`,
     twitterUrl: `https://twitter.com/share?text=${title}&via=${data.twitterHandle}&url=${buildUTMUrl(locals.url, { utm_source: 'twitter.com', utm_term: stationCallSign })}`,
     domain,
