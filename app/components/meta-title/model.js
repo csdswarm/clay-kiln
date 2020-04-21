09a42a733c7298b748a96d188b80c122229e58a2
Philip Olson <phil.olson@swarmsolutions.com>
2020-04-21 11:49:39 -0500

    Revert "Merged in release-permissions-prod-able_revert-meta-tags (pull request #1916)"
    This reverts commit 60130c33c31e863daeff00b27a3bbd9ce0bde7df, reversing
    changes made to f9456bd87332de7175eaeeeb7882061c1cbb6238.

--- app/components/meta-title/model.js
+++ app/components/meta-title/model.js
@@ -1,5 +1,23 @@
 'use strict';
 
-const sanitize = require('../../services/universal/sanitize');
+const _pick = require('lodash/pick'),
+  sanitize = require('../../services/universal/sanitize'),
+  { handleDefault } = require('../../services/kiln/plugins/default-text-with-override/on-model-save');
 
-module.exports.save = (ref, data) => sanitize.recursivelyStripSeperators(data);
+module.exports.save = (ref, data) => {
+  const defaultProps = _pick(data, [
+    'defaultTitle',
+    'defaultOgTitle',
+    'defaultTwitterTitle',
+    'defaultKilnTitle'
+  ]);
+
+  Object.assign(data, sanitize.recursivelyStripSeperators(defaultProps));
+
+  handleDefault('title', 'defaultTitle', data);
+  handleDefault('ogTitle', 'defaultOgTitle', data);
+  handleDefault('twitterTitle', 'defaultTwitterTitle', data);
+  handleDefault('kilnTitle', 'defaultKilnTitle', data);
+
+  return data;
+};
