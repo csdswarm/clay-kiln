c096767cb513ae4a30fc68969a6d1a22fa4b7c96
Markham F Rollins IV <markham@swarmsolutions.com>
2020-04-24 09:47:07 -0400

    ON-1788: Set static dirs onto _state and cleanup checks

--- app/services/server/static-assets.js
+++ app/services/server/static-assets.js
@@ -1,15 +1,19 @@
 'use strict';
 
-const fs = require('fs'),
+const _isEmpty = require('lodash/isEmpty'),
+  _state = {
+    staticDirs: []
+  },
+  { readdirSync } = require('fs'),
   path = require('path'),
-  staticDirs = fs.readdirSync(path.resolve(__dirname , '../../public'), function (err, filesPath) {
-    if (err) throw err;
-    const result = filesPath.map(function (filePath) {
-      return filePath;
-    });
-
-    return result;
-  });
+  /**
+   * Set the list of static dirs onto state so all subsequent calls skip the load logic
+   */
+  setStaticDirs = () => {
+    _state.staticDirs = readdirSync(path.resolve(__dirname , '../../public'), { withFileTypes: true })
+      .filter(entry => entry.isDirectory())
+      .map(entry => entry.name);
+  };
 
 /**
  * Check the path for static dirs
@@ -17,9 +21,13 @@ const fs = require('fs'),
  * @param {string} path The path to check
  * @returns {boolean}
  */
-function isStaticAsset(path) {
+function isStaticAsset(filepath) {
+  if (_isEmpty(_state.staticDirs)) {
+    setStaticDirs();
+  }
+
   // If this is a static asset always return false
-  return staticDirs.some(dirName => path.startsWith('/' + dirName));
+  return _state.staticDirs.some(dirName => filepath.startsWith('/' + dirName));
 }
 
 module.exports = {
