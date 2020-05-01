8895b81bbe9fbdeea6430c5d82cfb1825aef43f7
julian.figueroa@gorillalogic.com <julian.figueroa@gorillalogic.com>
2020-05-01 12:45:58 -0500

    ON-1596 | jf | Add getAllStations function in migration/utils and improve migration code based on previous feedback/comments

--- migrations/legacy/20200428124500-add-editorial-group-for-ON-1596/migration.js
+++ migrations/legacy/20200428124500-add-editorial-group-for-ON-1596/migration.js
@@ -5,14 +5,15 @@ const fs = require("fs");
 const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const appendFileAsync = util.promisify(fs.appendFile);
-const axios = require("../../../app/node_modules/axios");
 const csv = require("../../../app/node_modules/csvtojson");
 const _ = require("../../../app/node_modules/lodash");
-const url = "https://api.radio.com/v1/stations?filter[callsign]=";
 const csvFile =
   "./create-editorial-group/Editorial Group - Editorial Group.csv";
 const dmlFile = "./create-editorial-group/dml_editorial_group.sql";
 const ddlFile = "./create-editorial-group/editorial_group.sql";
+const stations = require("../../utils/get-all-stations");
+
+let allTheStations;
 
 run();
 
@@ -21,19 +22,40 @@ async function run() {
     console.log(
       "Editorial Group - Create File with rows and then creating table and inserting records"
     );
-
     await createTable();
-    console.log("After createTable");
 
     await generateInserts();
-    console.log("After generateInserts");
 
-    setTimeout(insertData, 10000);
-    console.log("After insertData");
+    await insertData();
 
     console.log("Sucessfully !!!");
   } catch (err) {
     console.error(err.stack);
+  }
+}
+
+/**
+ * Get all the Station from RDC API and extract only id, callsign, site_slug, website amd market
+ * this props will filtered with the CSV editorial groups data
+ *
+ * @returns {Promise}
+ */
+async function filterStations() {
+  try {
+    const allStations = await stations.getAllStations();
+    const filteredStations = _.map(allStations.data, (station) => {
+      return {
+        id: station.attributes.id,
+        callsign: station.attributes.callsign,
+        siteSlug: station.attributes.site_slug,
+        website: station.attributes.website,
+        market: station.attributes.market,
+      };
+    });
+
+    return filteredStations;
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -59,14 +81,10 @@ async function createTable() {
  * @returns {Promise}
  */
 async function insertData() {
-  try {
-    const dmlEditorialGroup = await readFileAsync(dmlFile, "utf8");
-    await usingDb((db) => db.query(dmlEditorialGroup));
+  const dmlEditorialGroup = await readFileAsync(dmlFile, "utf8");
+  await usingDb((db) => db.query(dmlEditorialGroup));
 
-    console.log("successfully inserted rows in editorial_group table");
-  } catch (err) {
-    console.error(err);
-  }
+  console.log("successfully inserted rows in editorial_group table");
 }
 
 /**
@@ -75,14 +93,12 @@ async function insertData() {
  * @returns {Promise}
  */
 async function generateInserts() {
-  try {
-    // Loads the CSV file and transform it into a JSON array
-    const jsonArray = await csv().fromFile(csvFile);
-    await createDMLFile(jsonArray);
-    console.log("successfully generated DML file");
-  } catch (err) {
-    console.error(err);
-  }
+  allTheStations = await filterStations();
+  // Loads the CSV file and transform it into a JSON array
+  const jsonArray = await csv().fromFile(csvFile);
+
+  await createDMLFile(jsonArray);
+  console.log("successfully generated DML file");
 }
 
 /**
@@ -95,82 +111,101 @@ async function createDMLFile(data) {
   /**
    * Remove special characters from Call Letters property to match with RDC API
    *
-   * @param {Array} data
+   * @param {String} callLetters
    * @returns {String}
    */
   const sanitizeCallLetters = (callLetters) =>
     callLetters.replace(/[^a-zA-Z]/g, "");
 
   /**
-   * Extract the site slug property
+   * Find by property in all RDC API Stations
    *
-   * @param {Array} record
+   * @param {string} findProperty
+   * @param {string} value
    * @returns {String}
    */
-  const getSiteSlug = (record) =>
-    !_.isEmpty(record) ? record[0].attributes.site_slug : null;
+  const findInStations = async (findProperty, value) => {
+    return _.find(allTheStations, (station) => {
+      if (station[findProperty] === value) {
+        return station.siteSlug;
+      }
+    });
+  };
 
   /**
-   * Determine if a property should return true/false
+   * Extract the site slug property
    *
-   * @param {Object} record
+   * @param {Object} record - from csv
    * @returns {String}
    */
-  const checkProperty = (record) => {
+  const getSiteSlug = async (record) => {
+    // Extract the Call Letters property
+    let callLetters = record["Call Letters"];
+    // Removes special characters to match with RDC API
+    callLetters = sanitizeCallLetters(callLetters);
+    const station = await findInStations("callsign", callLetters);
+    let siteSlug = "";
+
+    if (_.isEmpty(station)) {
+      console.error(
+        `CSV record haven't been found in RDC API with this Call Letter/callsign ${record["Call Letters"]} \ 
+        Please update this record`
+      );
+    } else {
+      siteSlug = station.siteSlug;
+    }
+
+    return siteSlug;
+  };
+
+  /**
+   * Builds an object which declares which editorial feeds the station subscribes to
+   *
+   * @param {Object} record - from csv
+   * @returns {String}
+   */
+  const buildFeeds = (record) => {
     let feeds = {};
-    // Get properties with the "x"
-    let filteredByFeed = _.keys(_.pickBy(record, (value, key) => value));
-    // Delete unneeded props
-    filteredByFeed.splice(0, 3);
-    // Asigns true value to the feeds
-    filteredByFeed.forEach((feed) => {
-      return (feeds[feed] = true);
-    });
+    for (const [key, value] of Object.entries(record)) {
+      if (value === "x") {
+        feeds[key] = true;
+      }
+    }
+
     return feeds;
   };
 
   /**
    * Generate a DML file with the records to be inserted in editorial_group table
    *
-   * @param {String} sql
-   * @returns {Promise}
+   * @param {String} sql - insert statement INSERT INTO ...
+   * @returns {void}
    */
-  const generateFile = async (sql) => {
-    await appendFileAsync(
+  const generateFile = (sql) =>
+    appendFileAsync(
       `${__dirname}/create-editorial-group/dml_editorial_group.sql`,
       sql,
-      "utf8",
-      (err) => {
-        if (err) throw err;
-      }
+      "utf8"
     );
-  };
 
-  await data.forEach(async (record) => {
-    // Extract the Call Letters property
-    let callLetters = record["Call Letters"];
-    // Removes special characters to match with RDC API
-    callLetters = sanitizeCallLetters(callLetters);
-    // Get the Site Slug from RDC API
-    const station = await axios(url + callLetters);
-    // GEt the data from the RDC API
-    const { data } = station.data;
+  for (const record of data) {
     // Extracts SIte Slug property
-    const siteSlug = getSiteSlug(data);
+    const siteSlug = await getSiteSlug(record);
     // Get the feeds with the "x" value
-    const feeds = checkProperty(record);
-    // Generates the insert statememt
+    const feeds = await buildFeeds(record);
+    // // Generates the insert statememt
     const sql = `INSERT INTO editorial_group(data)
     VALUES (
       '{
-        "Market": "${record.Market}",
-        "site-slug": "${siteSlug}",
+        "market": "${record.Market}",
+        "siteSlug": "${siteSlug}",
         "feeds": ${JSON.stringify(feeds)}
-        
+
       }'
       );
     \ `;
+
     // Send the insert DML to a file
-    await generateFile(sql);
-  });
+    generateFile(sql);
+  }
 }
