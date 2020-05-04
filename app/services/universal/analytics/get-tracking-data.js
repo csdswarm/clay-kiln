8b68e99b7dc44ed6a28217eecad0fcbaa72df6c6
Luis Felipe Bertel Mercado <luis.bertel@gorillalogic.com>
2020-05-04 08:57:26 -0500

    ON-1821 add nmc:market to content pages.

--- app/services/universal/analytics/get-tracking-data.js
+++ app/services/universal/analytics/get-tracking-data.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _get = require('lodash/get'),
+  _lowerCase = require('lodash/lowerCase'),
   getPageId = require('./get-page-id'),
   makeFromPathname = require('./make-from-pathname');
 
@@ -26,9 +27,7 @@ module.exports = function getTrackingData({ pathname, station, pageData, content
     genre: fromPathname.getGenre(station) || 'aaa',
     pid: getPageId({ pageData, pathname }),
     tag: fromPathname.getTags(pageData, contentTags),
-    market: fromPathname.isStationDetail()
-      ? _get(station, 'market_name')
-      : undefined,
-    station: _get(station, 'callsign', defaultCallsign)
+    market: _get(station, 'market_name', undefined),
+    station: _lowerCase(_get(station, 'callsign', defaultCallsign))
   };
 };
