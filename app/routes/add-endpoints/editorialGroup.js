2d7e0ef433ad37517f9f633816b69b682c19630a
julian.figueroa@gorillalogic.com <julian.figueroa@gorillalogic.com>
2020-05-05 10:10:26 -0500

    ON-1643 | fix conflict in db.js file

--- app/routes/add-endpoints/editorialGroup.js
+++ app/routes/add-endpoints/editorialGroup.js
@@ -4,7 +4,7 @@ const { wrapInTryCatch } = require('../../services/startup/middleware-utils'),
   db = require('../../services/server/db');
 
 module.exports = router => {
-  router.get('/editorial-group', wrapInTryCatch(async (req, res) => {
+  router.get('/rdc/editorial-group', wrapInTryCatch(async (req, res) => {
     const result = await db.raw(`
     SELECT id, data
     FROM editorial_group
@@ -12,11 +12,12 @@ module.exports = router => {
     
     if (!result.rows) {
       res.status(404).end();
+      return;
     }
     res.send(result.rows);
   }));
   
-  router.put('/editorial-group/:id', wrapInTryCatch(async (req, res) => {
+  router.put('/rdc/editorial-group/:id', wrapInTryCatch(async (req, res) => {
     const { body, params } = req;
     
     if (_isEmpty(body) && _isEmpty(params)) {
