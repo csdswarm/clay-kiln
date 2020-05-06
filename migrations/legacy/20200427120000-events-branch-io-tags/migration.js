0040b2668982f16d42678ff5837718fc7b08ca66
Tiffany Chow <tiffachow@gmail.com>
2020-05-06 14:46:00 -0400

    ON-1706 Update migration

--- migrations/legacy/20200427120000-events-branch-io-tags/migration.js
+++ migrations/legacy/20200427120000-events-branch-io-tags/migration.js
@@ -3,23 +3,21 @@ const hostUrl = process.argv[2] || 'clay.radio.com';
 
 // add branch-io-head script and meta-tag to events pages
 async function addBranchandMetaTagHeadToPages() {
-  await Promise.all([
-    addComponentToContainers(
-      hostUrl,
-      [ '_pages/event' ],
-      '_components/branch-io-head/instances/default',
-      'head'
-    ),
-    addComponentToContainers(
-      hostUrl,
-      [ '_pages/event' ],
-      '_components/meta-tags/instances/general',
-      'head'
-    )
-  ]);
+  await addComponentToContainers(
+    hostUrl,
+    [ '_pages/event' ],
+    '_components/branch-io-head/instances/default',
+    'head'
+  );
+  await addComponentToContainers(
+    hostUrl,
+    [ '_pages/event' ],
+    '_components/meta-tags/instances/general',
+    'head'
+  );
 };
 
 addBranchandMetaTagHeadToPages()
   .catch(function (error) {
-    console.log('error', error);
+    console.error(error);
   });
