4036bdf7e1e1da4eba7e064c8f167cdbfdb33508
Nick Renford <nick.renford@entercom.com>
2020-05-11 13:47:21 -0600

    ON-1613 get recirc data from unity
    Check if then station is migrated. If not - continue
    doing the same thing. If it is migrated, then get
    article data from unity instead of drupal.

--- app/components/latest-recirculation/model.js
+++ app/components/latest-recirculation/model.js
@@ -5,6 +5,8 @@ const db = require('../../services/server/db'),
   radioApiService = require('../../services/server/radioApi'),
   { uploadImage } = require('../../services/universal/s3'),
   { getSectionFrontName, retrieveList } = require('../../services/server/lists'),
+  queryService = require('../../services/server/query'),
+  log = require('../../services/universal/log').setup({ file: __filename }),
   elasticFields = [
     'date',
     'primaryHeadline',
@@ -69,7 +71,6 @@ const db = require('../../services/server/db'),
         nodes = feed.nodes ? feed.nodes.filter((item) => item.node).slice(0, 5) : [],
         defaultImage = 'https://images.radio.com/aiu-media/og_775x515_0.jpg';
 
-      data._computed.station = locals.station.name;
       data._computed.articles = await Promise.all(nodes.map(async (item) => {
         return {
           feedImgUrl: item.node['OG Image'] ? await uploadImage(item.node['OG Image'].src) : defaultImage,
@@ -78,8 +79,33 @@ const db = require('../../services/server/db'),
         };
       }));
     }
-
     return data;
+  },
+
+
+  /**
+   * Determines whether or not a station is migrated, given the station slug
+   * @param {string} stationSlug
+   * @param {object} locals
+   * @returns {Promise<boolean>}
+   */
+  isStationMigrated = async function (stationSlug, locals) {
+    if (!stationSlug) {
+      return false;
+    }
+
+    const query = queryService.newQueryWithCount('published-stations', 1, locals);
+
+    queryService.addMust(query, { match: { stationSlug } });
+
+    try {
+      const results = await queryService.searchByQuery(query, locals, { shouldDedupeContent: true });
+
+      return results.length > 0;
+    } catch (e) {
+      log('error', e);
+      return false;
+    }
   },
 
   /**
@@ -89,8 +115,14 @@ const db = require('../../services/server/db'),
    * @returns {Promise}
    */
   render = async function (ref, data, locals) {
+    const slug = locals.station && locals.station.site_slug,
+      isMigrated = await isStationMigrated(slug);
+
     if (data.populateFrom === 'station' && locals.params) {
-      return renderStation(data, locals);
+      data._computed.station = locals.station.name;
+      if (!isMigrated) {
+        return renderStation(data, locals);// gets the articles from drupal and displays those instead
+      }
     }
 
     const primarySectionFronts = await retrieveList('primary-section-fronts', { locals });
@@ -112,6 +144,5 @@ module.exports = recirculationData({
     curated: [...data.items, ...await getItemsFromTrendingRecirculation(locals)],
     maxItems: getMaxItems(data)
   }),
-  render,
-  skipRender: (data, locals) => data.populateFrom === 'station' && locals.params
+  render
 });
