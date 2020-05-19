135b6ad5de18ea4083d242806bf79236aff79fe4
Nick Renford <nick.renford@entercom.com>
2020-05-19 17:07:55 -0600

    Update section-front to use unityComponent()

--- app/components/section-front/model.js
+++ app/components/section-front/model.js
@@ -1,15 +1,19 @@
 'use strict';
+const { unityComponent } = require('../../services/universal/amphora');
 
-module.exports.render = (uri, data, locals) => {
-  if (data.title) {
 
-    if (data.primary) {
-      locals.sectionFront = data.title.toLowerCase();
-    } else {
-      locals.sectionFront = data.primarySectionFront;
-      locals.secondarySectionFront = data.title.toLowerCase();
+module.exports = unityComponent({
+  render: (uri, data, locals) => {
+    if (data.title) {
+
+      if (data.primary) {
+        locals.sectionFront = data.title.toLowerCase();
+      } else {
+        locals.sectionFront = data.primarySectionFront;
+        locals.secondarySectionFront = data.title.toLowerCase();
+      }
     }
-  }
 
-  return data;
-};
+    return data;
+  }
+});
