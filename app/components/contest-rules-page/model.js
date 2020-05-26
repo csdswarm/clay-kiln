ac09cd9bea27666b8327ac3739e0069c4ef5a469
Leland Kwong <Leland.Kwong@entercom.com>
2020-05-26 13:43:16 -0400

    ON-1945 | Refactor contest rules server-only module
    * We now use separate client/server modules for `getContestRules` since the model file is universal. This is to prevent any potential issues when building.

--- app/components/contest-rules-page/model.js
+++ app/components/contest-rules-page/model.js
@@ -9,54 +9,14 @@
 
 const { unityComponent } = require('../../services/universal/amphora');
 const rest = require('../../services/universal/rest.js');
-const db = require('amphora-storage-postgres');
+const { getContestRules } = require('../../services/server/contest-rules');
 const {
   PRIVACY_POLICY
 } = require('../../services/universal/constants');
 const moment = require('moment');
-const maxAgeInDays = 31;
 const url = require('url');
 const _get = require('lodash/get');
 const log = require('../../services/universal/log').setup({ file: __filename });
-
-/**
- * Creates a conditional station operator for contest sql query
- * @param {String} stationCallsign
- * @returns {String}
- */
-const stationQuery = stationCallsign => `AND data->>'stationCallsign' = '${stationCallsign}'`;
-
-/**
- * Queries the db for contest rules
- * @param {String} param.startTime
- * @param {String} param.stationCallsign
- */
-const getContestRules = async ({
-  startTime = '',
-  stationCallsign = ''
-}) => {
-  const contestRulesQuery = /* sql */ `
-    SELECT *
-    FROM components."contest"
-
-    -- make sure contest is active within current time
-    WHERE data->>'endDateTime' >= '${startTime}'
-
-    -- show contests that are active within 30 days from current time
-    AND DATE_PART(
-      'day',
-      (data ->> 'endDateTime')::timestamp - '${startTime}'::timestamp
-    ) <= ${maxAgeInDays}
-
-    AND id SIMILAR TO '%@published'
-    ${stationQuery(stationCallsign)}
-  `;
-
-  const { rows } = await db.raw(contestRulesQuery);
-  const pluckData = ({ data }) => data;
-
-  return rows.map(pluckData);
-};
 
 module.exports = unityComponent({
   render: async (ref, data, locals = {}) => {
