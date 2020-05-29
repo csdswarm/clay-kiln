575360fe7e5aa346e0812d8d8f2e03a53405bd1f
Laura Tejada <laura.tejada@gorillalogic.com>
2020-05-29 16:44:35 -0500

    ON-1576 | adding changes for unit tests

--- app/components/podcast-category-strip/model.js
+++ app/components/podcast-category-strip/model.js
@@ -7,48 +7,72 @@ const { unityComponent } = require('../../services/universal/amphora'),
   _uniqBy = require('lodash/uniqBy'),
   _slice = require('lodash/slice');
 
-async function populatePodcasts(data, locals) {
+/**
+ * Make http request for getting backfill podcasts
+ *
+ * @param {object} category
+ * @param {object} locals
+ * @returns {array}
+ */
+async function getBackfillPodcasts(category, locals) {
+  const response = await radioApiService.get(
+    'podcasts',
+    {
+      sort: '-popularity',
+      filter: { category_id: category.id },
+      page: { size: 7 }
+    },
+    null, {}, locals
+  );
+
+  return response.data;
+}
+
+/**
+ * Get category podcasts lists, counting curated items and backfilling up to 7
+ *
+ * @param {object} data
+ * @param {object} locals
+ * @returns {promise | array}
+ */
+function populatePodcasts(data, locals) {
   if (data.category) {
-    return radioApiService.get(
-      'podcasts',
-      {
-        sort: '-popularity',
-        filter: { category_id: data.category.id },
-        page: { size: 7 }
-      },
-      null, {}, locals
-    ).then(response => {
-      data.backfillPodcasts = response.data || [];
+    return __.getBackfillPodcasts(data.category, locals)
+      .then(responseData => {
+        const backfillPodcasts = (responseData || []).map(podcast => {
+            return {
+              id: podcast.id,
+              // creates url for small podcast image
+              imageUrl: utils.createImageUrl(_get(podcast, 'attributes.image')),
+              title: _get(podcast, 'attributes.title'),
+              url: `/podcasts/${ _get(podcast, 'attributes.site_slug') }`
+            };
+          }),
+          // curated podcasts should be pinned to front of list
+          curatedPodcasts = data.curatedPodcasts.map(podcast => podcast.podcast),
+          totalPodcasts = curatedPodcasts.concat(backfillPodcasts),
+          // filter out duplicates from backfill
+          uniquePodcasts = _uniqBy(totalPodcasts, 'id'),
+          // total deduped curated + backfill items should amount to exactly 7
+          podcasts = _slice(uniquePodcasts, 0, 7);
 
-      const backfillPodcasts = data.backfillPodcasts.map(podcast => {
-          return {
-            id: podcast.id,
-            // creates url for small podcast image
-            imageUrl: utils.createImageUrl(_get(podcast, 'attributes.image')),
-            title: _get(podcast, 'attributes.title'),
-            url: `/podcasts/${ _get(podcast, 'attributes.site_slug') }`
-          };
-        }),
-        // curated podcasts should be pinned to front of list
-        curatedPodcasts = data.curatedPodcasts.map(podcast => podcast.podcast),
-        totalPodcasts = curatedPodcasts.concat(backfillPodcasts),
-        // filter out duplicates from backfill
-        uniquePodcasts = _uniqBy(totalPodcasts, 'id'),
-        // total deduped curated + backfill items should amount to exactly 7
-        podcasts = _slice(uniquePodcasts, 0, 7);
-
-      data._computed = {
-        podcasts,
-        seeAllLink: `/podcasts/collection/${ _get(data.category, 'slug') }`
-      };
-      return data;
-    }).catch(e => {
-      console.error('error getting podcast data from api', e);
-      return data;
-    });
+        data._computed = {
+          podcasts,
+          seeAllLink: `/podcasts/collection/category/${ _get(data.category, 'slug') }`
+        };
+        return data;
+      }).catch(e => {
+        console.error('error getting podcast data from api', e);
+        return data;
+      });
   }
   return data;
 }
+
+// Exposing internal method for testing purposes
+const __ = {
+  getBackfillPodcasts
+};
 
 module.exports = unityComponent({
   /**
@@ -60,19 +84,9 @@ module.exports = unityComponent({
    *
    * @returns {object}
    */
-  render: async (uri, data, locals) => {
+  render: (uri, data, locals) => {
     return populatePodcasts(data, locals);
-  },
-  /**
-   * Makes any necessary modifications to data just prior to persisting it
-   *
-   * @param {string} uri - The uri of the component instance
-   * @param {object} data - persisted or bootstrapped data for this instance
-   * @param {object} locals - data that has been attached to express locals for the current page request
-   *
-   * @returns {object}
-   */
-  save: (uri, data) => {
-    return data;
   }
 });
+
+module.exports._internals = __;
