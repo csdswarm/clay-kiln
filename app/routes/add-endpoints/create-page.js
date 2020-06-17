2b6edb8f4a268657fa86d594fe9c0179f09302bd
Juan Camilo Giraldo <juan.giraldo@gorillalogic.com>
2020-06-16 22:33:16 -0500

    ON-1920 | JCG | Add /clone-content endpoint

--- app/routes/add-endpoints/create-page.js
+++ app/routes/add-endpoints/create-page.js
@@ -4,12 +4,24 @@ const amphora = require('amphora'),
   { elastic } = require('amphora-search'),
   { wrapInTryCatch } = require('../../services/startup/middleware-utils'),
   stationUtils = require('../../services/server/station-utils'),
+  db = require('../../services/server/db'),
+  _get = require('lodash/get'),
+  axios = require('axios'),
+  utils = require('../../services/universal/utils'),
   addStationSlug = (uri, stationSlug) => stationSlug && amphora.db.getMeta(uri)
     .then(meta => ({ ...meta, stationSlug }))
     .then(updatedMeta => amphora.db.putMeta(uri, updatedMeta))
-    .then(data => elastic.put('pages', uri, data));
+    .then(data => elastic.put('pages', uri, data)),
+  removeSyndication = (content) => ({
+    ...content,
+    corporateSyndication: null,
+    editorialFeeds: null,
+    genreSyndication: null,
+    stationSyndication: [],
+    syndicatedUrl: null,
+    syndicationStatus: 'original'
+  });
     
-
 module.exports = router => {
   router.post('/_pages', (req, res) => {
     res.status(404);
@@ -43,6 +55,51 @@ module.exports = router => {
     
     await addStationSlug(result._ref, stationSlug);
 
+    res.status(201);
+    res.send(result);
+  }));
+
+  router.post('/clone-content', wrapInTryCatch(async (req, res) => {
+    const { canonicalUrl, stationSlug } = req.body,
+      pagesUri = req.hostname + '/_pages/',
+      { locals } = res,
+      url = canonicalUrl.split('//')[1],
+      
+      QUERY = `SELECT 
+      u.data FROM uris u 
+      WHERE u.url = '${url}'`,
+
+      contentPageURL = await db.raw(QUERY).then(results => _get(results, 'rows[0].data'));
+      
+    let { data: pageBody } = await axios.get(utils.uriToUrl(`${contentPageURL}@published`, locals));
+      
+    if (!pageBody) {
+      res.status(400).send({ error: 'Page not found' });
+      return;
+    }
+
+    const contentUri = pageBody.main[0],
+      layoutUri = utils.replaceVersion(pageBody.layout),
+      content = await db.get(contentUri),
+      updatedContent = removeSyndication(content);
+    
+    await db.put(contentUri, updatedContent);
+
+    pageBody = {
+      ...pageBody,
+      layout: layoutUri
+    };
+
+    if (stationSlug) {
+      const allStations = await stationUtils.getAllStations({ locals });
+
+      res.locals.newPageStation = allStations.bySlug[stationSlug];
+    }
+    
+    const result = await amphora.pages.create(pagesUri, pageBody, res.locals);
+
+    await addStationSlug(result._ref, stationSlug);
+    
     res.status(201);
     res.send(result);
   }));
