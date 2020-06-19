a206cfb2256496ea68e462e1d6025ec72c51d720
Kurt Moffett <kurtis.moffett@entercom.com>
2020-06-19 13:53:53 -0700

    ON-1854 | Corrected the protocol and host for the national contest migration.

--- migrations/legacy/20200612122350-national-contest-ON-1854/migration.js
+++ migrations/legacy/20200612122350-national-contest-ON-1854/migration.js
@@ -1,39 +1,41 @@
 'use strict';
 
 const axios = require('../../../app/node_modules/axios'),
-  host = process.argv.slice(2)[0];
+  host = process.argv.slice(2)[0] || 'clay.radio.com',
+  protocol = host === 'clay.radio.com' ? 'http' : 'https';
 
 console.log('\n NATIONAL CONTEST PAGE \n');
 
 createNationalContestPage();
 
 async function createNationalContestPage () {
+
   const data = {
     "head": [
-      "clay.radio.com/_components/meta-title/instances/general",
-      "clay.radio.com/_components/meta-description/instances/general",
-      "clay.radio.com/_components/meta-image/instances/general",
-      "clay.radio.com/_components/meta-url/instances/general",
-      "clay.radio.com/_components/meta-tags/instances/general",
-      "clay.radio.com/_components/branch-io-head/instances/default"
+      `${host}/_components/meta-title/instances/general`,
+      `${host}/_components/meta-description/instances/general`,
+      `${host}/_components/meta-image/instances/general`,
+      `${host}/_components/meta-url/instances/general`,
+      `${host}/_components/meta-tags/instances/general`,
+      `${host}/_components/branch-io-head/instances/default`
     ],
     "main": [
-      "clay.radio.com/_components/contest/instances/new"
+      `${host}/_components/contest/instances/new`
     ],
-    "layout": "clay.radio.com/_layouts/two-column-layout/instances/contest",
+    "layout": `${host}/_layouts/two-column-layout/instances/contest`,
     "tertiary": [
-      "clay.radio.com/_components/google-ad-manager/instances/mediumRectangleTop",
-      "clay.radio.com/_components/latest-recirculation/instances/national-contest",
-      "clay.radio.com/_components/google-ad-manager/instances/halfPageBottom"
+      `${host}/_components/google-ad-manager/instances/mediumRectangleTop`,
+      `${host}/_components/latest-recirculation/instances/national-contest`,
+      `${host}/_components/google-ad-manager/instances/halfPageBottom`
     ],
     "pageHeader": [
-      "clay.radio.com/_components/google-ad-manager/instances/billboardTop"
+      `${host}/_components/google-ad-manager/instances/billboardTop`
     ]
   };
 
   console.log('Creating National Contest Page');
 
-  await axios.put(`http://${host}/_pages/national-contest`, data, { headers: { Authorization: 'token accesskey', 'Content-Type': 'application/json' } })
+  await axios.put(`${protocol}://${host}/_pages/national-contest`, data, { headers: { Authorization: 'token accesskey', 'Content-Type': 'application/json' } })
     .then((response) => {
       console.log('Successfully created National Contest Page. \n', response.data);
     })
