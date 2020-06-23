1fc7d278ebd5d7220f857dc33166e70db05b6640
Luis Felipe Bertel Mercado <luis.bertel@gorillalogic.com>
2020-06-23 17:07:36 -0500

    ON-2052 | added video_updated_by custom field to track updated videos.

--- app/services/startup/brightcove.js
+++ app/services/startup/brightcove.js
@@ -199,7 +199,8 @@ const brightcoveApi = require('../universal/brightcoveApi'),
       station,
       highLevelCategory: high_level_category,
       tags,
-      adSupported: economics
+      adSupported: economics,
+      video_updated_by
     } = req.body;
 
     try {
@@ -210,12 +211,13 @@ const brightcoveApi = require('../universal/brightcoveApi'),
         long_description,
         custom_fields: {
           station,
-          high_level_category
+          high_level_category,
+          video_updated_by
         },
         tags,
         economics
       });
-
+      
       if (status === 200 && updateResponse.id) {
         res.send(updateResponse);
       } else {
