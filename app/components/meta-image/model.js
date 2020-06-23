85854a8e426d1d57ff77e8648504bb14faa1f8c3
Philip Olson <phil.olson@swarmsolutions.com>
2020-06-23 00:15:29 -0500

    Revert "ON-1626: Revert "ON-894: restructure meta-tags permissions""
    This reverts commit d0ec69fd8314c8a578fd2ded0446ee5171dec15a.

--- app/components/meta-image/model.js
+++ app/components/meta-image/model.js
@@ -1,17 +1,24 @@
 'use strict';
 
-const _get = require('lodash/get');
+const _get = require('lodash/get'),
+  { handleDefault } = require('../../services/kiln/plugins/default-text-with-override/on-model-save');
 
 module.exports.render = (ref, data, locals) => {
   if (data.localsKey && locals) {
     const value = _get(locals, data.localsKey);
 
-    if (value && data.imageUrl.includes('${paramValue}')) {
-      data.imageUrl = data.imageUrl.replace('${paramValue}', value);
+    if (value && data.defaultImageUrl.includes('${paramValue}')) {
+      data.defaultImageUrl = data.defaultImageUrl.replace('${paramValue}', value);
     } else {
-      data.imageUrl = value;
+      data.defaultImageUrl = value;
     }
   }
+
+  return data;
+};
+
+module.exports.save = (ref, data) => {
+  handleDefault('imageUrl', 'defaultImageUrl', data);
 
   return data;
 };
