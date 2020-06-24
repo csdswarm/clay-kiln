fab53bd5a7e8de266ff8a0f02e28f0344e10087f
Kurt Moffett <kurtis.moffett@entercom.com>
2020-06-24 13:04:57 -0700

    ON-1715 | Fixed linting errors.

--- app/components/meta-description/model.js
+++ app/components/meta-description/model.js
@@ -2,6 +2,7 @@
 
 module.exports.save = (uri, data, locals) => {
   const populateDescription = (data.description === '' || data.description === undefined) ? true : false;
+  
   // If data.description is an empty string or undefined, and locals.newPageStation exists
   // we are creating a new station page and should set the values for the intial meta data
   if (populateDescription && locals.newPageStation) {
--- app/components/meta-title/model.js
+++ app/components/meta-title/model.js
@@ -4,6 +4,7 @@ const sanitize = require('../../services/universal/sanitize');
 
 module.exports.save = (uri, data, locals) => {
   const populateTitle = (data.title === '' || data.title === undefined) ? true : false;
+  
   // If data.title is an empty string or undefined, and locals.newPageStation exists
   // we are creating a new station page and should set the values for the intial meta data
   if (populateTitle && locals.newPageStation) {
