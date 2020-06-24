badbe865a2dd8f30011621d6790d2e0d7ef64b61
Kurt Moffett <kurtis.moffett@entercom.com>
2020-06-24 13:03:49 -0700

    ON-1715 | Added a check in the meta title and description for undefined values.

--- app/components/meta-description/model.js
+++ app/components/meta-description/model.js
@@ -1,9 +1,10 @@
 'use strict';
 
 module.exports.save = (uri, data, locals) => {
-  // If data.description is an empty string and locals.newPageStation exists
+  const populateDescription = (data.description === '' || data.description === undefined) ? true : false;
+  // If data.description is an empty string or undefined, and locals.newPageStation exists
   // we are creating a new station page and should set the values for the intial meta data
-  if (data.description === '' && locals.newPageStation) {
+  if (populateDescription && locals.newPageStation) {
     data.description       = locals.newPageStation.description;
   };
   return data;
--- app/components/meta-title/model.js
+++ app/components/meta-title/model.js
@@ -3,11 +3,12 @@
 const sanitize = require('../../services/universal/sanitize');
 
 module.exports.save = (uri, data, locals) => {
-  // If data.title is an empty string and locals.newPageStation exists
+  const populateTitle = (data.title === '' || data.title === undefined) ? true : false;
+  // If data.title is an empty string or undefined, and locals.newPageStation exists
   // we are creating a new station page and should set the values for the intial meta data
-  if (data.title === '' && locals.newPageStation) {
-    data.title       = locals.newPageStation.name;
-    data.kilnTitle   = locals.newPageStation.name;
+  if (populateTitle && locals.newPageStation) {
+    data.title       = `${locals.newPageStation.name} ${locals.newPageStation.slogan}`;
+    data.kilnTitle   = `${locals.newPageStation.name} ${locals.newPageStation.slogan}`;
   };
   return sanitize.recursivelyStripSeperators(data);
 };
