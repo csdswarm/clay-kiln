34042bf73e1b80768df93f7005e1d22a7f3dda00
Kurt Moffett <kurtis.moffett@entercom.com>
2020-06-24 14:05:51 -0700

    ON-1715 | Removed unneeded ternary, and improved the readability of changes to meta-title and met-description.

--- app/components/meta-description/model.js
+++ app/components/meta-description/model.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports.save = (uri, data, locals) => {
-  const populateDescription = (data.description === '' || data.description === undefined) ? true : false;
-  
   // If data.description is an empty string or undefined, and locals.newPageStation exists
   // we are creating a new station page and should set the values for the intial meta data
-  if (populateDescription && locals.newPageStation) {
-    data.description       = locals.newPageStation.description;
+  const populateDescription = (data.description === '' || data.description === undefined) && locals.newPageStation;
+
+  if (populateDescription) {
+    data.description = locals.newPageStation.description;
   };
   return data;
 };
--- app/components/meta-title/model.js
+++ app/components/meta-title/model.js
@@ -3,13 +3,13 @@
 const sanitize = require('../../services/universal/sanitize');
 
 module.exports.save = (uri, data, locals) => {
-  const populateTitle = (data.title === '' || data.title === undefined) ? true : false;
-  
   // If data.title is an empty string or undefined, and locals.newPageStation exists
   // we are creating a new station page and should set the values for the intial meta data
-  if (populateTitle && locals.newPageStation) {
-    data.title       = `${locals.newPageStation.name} ${locals.newPageStation.slogan}`;
-    data.kilnTitle   = `${locals.newPageStation.name} ${locals.newPageStation.slogan}`;
+  const populateTitle = (data.title === '' || data.title === undefined) && locals.newPageStation;
+
+  if (populateTitle) {
+    data.title     = `${locals.newPageStation.name} ${locals.newPageStation.slogan}`;
+    data.kilnTitle = data.title;
   };
   return sanitize.recursivelyStripSeperators(data);
 };
