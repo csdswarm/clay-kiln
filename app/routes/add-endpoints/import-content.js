e1ba866a33554c8d67e61e2306c79a9b2cfec728
Leland Kwong <Leland.Kwong@entercom.com>
2020-06-25 14:36:24 -0400

    ON-2012 | Fix page list for imported station content
    * Add missing metadata necessary for querying imported station content
    in the page-list component.
    * Added new service for adding metadata to be reusable between modules

--- app/routes/add-endpoints/import-content.js
+++ app/routes/add-endpoints/import-content.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const qs = require('qs'),
+  { updateMetaData } = require('../../services/server/update-metadata'),
   rest = require('../../services/universal/rest'),
   { wrapInTryCatch } = require('../../services/startup/middleware-utils'),
   importContentUrl = process.env.IMPORT_CONTENT_URL;
@@ -8,7 +9,21 @@ const qs = require('qs'),
 module.exports = router => {
   router.post('/import-content', wrapInTryCatch(async (req, res) => {
     const params = qs.stringify({ ...req.body, publish: false }),
-      { results } = await rest.get(`${importContentUrl}?${params}`);
+      { results } = await rest.get(`${importContentUrl}?${params}`),
+      { stationSlug } = req.body,
+      metaDataModificationRequests = results.map(({ url }) => {
+        const pagePathRaw = url.replace('.html', ''),
+          ref = `${req.host}${pagePathRaw}`,
+          fullUrl = `${req.protocol}://${ref}`;
+
+        return updateMetaData(ref, {
+          stationSlug,
+          url: fullUrl,
+          siteSlug: 'demo'
+        });
+      });
+      
+    await Promise.all(metaDataModificationRequests);
 
     res.send(results);
   }));
