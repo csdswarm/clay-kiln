ce6306e67b9f2efe7134a37d286239e42475109a
davirlein <davirlein.prada@gorillalogic.com>
2020-07-06 16:57:32 -0500

    ON-1998 Included Endpoints and migration for ap-subscriptions

---
+++ app/services/server/ap/ap-subscriptions.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const db = require('../db'),
+  uuidV4 = require('uuid/v4'),
+  CLAY_SITE_HOST = process.env.CLAY_SITE_HOST,
+  /**
+   * Get a record matching a given id
+   *
+   * @param {any} key
+   */
+  ensureRecordExists = async key => {
+    const { rows } = await db.raw(`
+    SELECT data FROM ap_subscriptions
+    WHERE id = ?
+  `,[key]);
+
+    return rows.length > 0;
+  },
+  /**
+   * Returns all records in ap_subscriptions
+   */
+  getAll = async () => {
+    const  { rows } = await db.raw(`
+      SELECT * FROM ap_subscriptions;
+    `);
+
+    return rows;
+  },
+  /**
+   * saves an individual subscription. if an id is provided,
+   * it will update it. if no id is given, it will generate one and return what was generated.
+   * @param {any} id
+   * @param {Object} subscription
+   */
+  save = async (id = '', subscription = {}) => {
+    if (id) {
+      return await db.put(id, subscription);
+    } else {
+      const id = uuidV4(),
+        key = `${CLAY_SITE_HOST}/_ap_subscriptions/${id}`;
+
+      await db.post(key, subscription);
+      return id;
+    };
+  };
+
+module.exports = {
+  ensureRecordExists,
+  getAll,
+  save
+};
