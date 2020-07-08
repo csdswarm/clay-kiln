4269d7a26b2301b386a62f0500554f2670921ef3
Alejandro Chaves Orozco <alejandro.chaves@gorillalogic.com>
2020-07-08 17:21:32 -0500

    fix lint

--- app/components/article/model.js
+++ app/components/article/model.js
@@ -6,7 +6,26 @@ const _get = require('lodash/get'),
   { autoLink } = require('../breadcrumbs'),
   defaultTextWithOverride = {
     onModelSave: require('../../services/kiln/plugins/default-text-with-override/on-model-save')
+  },
+  { ARTICLE_AD_INSERT_EVERY, CLAY_SITE_HOST: host } = process.env,
+  /**
+   * Inject ads every nth index to content array
+   * @param {object} dataWithContent
+   * @returns {Void}
+   */
+  injectAdsToArticleContent = async (dataWithContent) => {
+    const insertEvery = +ARTICLE_AD_INSERT_EVERY;
+
+    if (dataWithContent.content.length > 4) {
+      for (let i = insertEvery; i < dataWithContent.content.length; i += insertEvery + 1) {
+        dataWithContent.content.splice(i, 0, {
+          _ref: `${host}/_components/google-ad-manager/instances/mediumRectangleBottom`
+        });
+      }
+    }
   };
+
+
 
 module.exports = unityComponent({
   render: async (uri, data, locals) => {
@@ -15,7 +34,13 @@ module.exports = unityComponent({
       'sectionFront',
       'secondarySectionFront'
     ], locals);
-    return createContent.render(uri, data, locals);
+    const dataWithContent = await createContent.render(uri, data, locals);
+    
+    if (!locals.edit) {
+      injectAdsToArticleContent(dataWithContent);
+    }
+
+    return dataWithContent;
   },
   save: async (uri, data, locals) => {
     data.dateModified = (new Date()).toISOString();
