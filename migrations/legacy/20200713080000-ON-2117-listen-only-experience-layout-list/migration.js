a9a4ec8db15ae51e40e93e80df376a7d93bcf465
Juan David Restrepo Toro <juan.restrepo@gorillalogic.com>
2020-07-14 10:44:28 -0500

    ON-2112 | JDR | Changed list name to listen-only-station-style

--- migrations/legacy/20200713080000-ON-2117-listen-only-experience-layout-list/migration.js
+++ migrations/legacy/20200713080000-ON-2117-listen-only-experience-layout-list/migration.js
@@ -6,7 +6,7 @@
 
 const { clayImport } = require('../migration-utils').v1,
   hostUrl = process.argv[2] || 'clay.radio.com',
-  listenOnlyLayoutStyle = [];
+  listenOnlyStationStyle = [];
 
 run()
 
@@ -14,14 +14,14 @@ async function run() {
   // construct the payload.
   const payload = {
     _lists : {
-      'listen-only-layout-style' : listenOnlyLayoutStyle
+      'listen-only-station-style' : listenOnlyStationStyle
     }
   }
 
   try {
-    console.log('Creating the listen-only-layout-style list');
+    console.log('Creating the listen-only-station-style list');
     await clayImport({ payload, hostUrl });
-    console.log('List listen-only-layout-style created successfully!');
+    console.log('List listen-only-station-style created successfully!');
   } catch (error) {
     console.error(`An error occurred while trying to create the list`, error);
   }
