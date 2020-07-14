7740af18f24b000acc6875301cf289efd26d61ab
Markham F Rollins IV <markham@swarmsolutions.com>
2020-07-14 09:04:57 -0400

    Revert "Merged in ON-2127-main-station-nav-scrolling-issue (pull request #2944)"
    This reverts commit 596e5c1624e491fe19735aff47242fe2376a4b27, reversing
    changes made to eb2d03d033a5dacc3391f186e63257bb3f955652.

--- app/components/station-nav/client.js
+++ app/components/station-nav/client.js
@@ -13,19 +13,7 @@ let desktopNavItems,
   stationId,
   stationListenNavInstance,
   lastTarget,
-  isMobile = false,
-  thresholdNavBar;
-
-
-const stationNavBar = document.querySelector('.station-nav__fixed-container');
-
-if (stationNavBar) {
-  // Assures that the stationNavBar is present before attaching the event.
-  thresholdNavBar = stationNavBar.offsetTop;
-  // Needs to be added outside the station-nav-mount event.
-  window.onscroll = function () { toggleStickyNavBar(); };
-}
-
+  isMobile = false;
 
 const { getComponentInstance } = require('clayutils'),
   { isMobileNavWidth } = require('../../services/client/mobile'),
@@ -251,22 +239,6 @@ const { getComponentInstance } = require('clayutils'),
   },
 
   /**
-   * Toggle Sticky Station Nav Bar
-   */
-  toggleStickyNavBar = () => {
-    // Handles sticky nav bars.
-    const topNavBar = document.querySelector('.radiocom-nav'),
-      stationNavBar = document.querySelector('.station-nav__fixed-container');
-
-    topNavBar.classList.add('reset-radiocom-top-nav');
-    if (window.pageYOffset > thresholdNavBar) {
-      stationNavBar.classList.add('sticky-station-nav-bar');
-    } else {
-      stationNavBar.classList.remove('sticky-station-nav-bar');
-    }
-  },
-
-  /**
    * Add event listeners to nav elements to toggle drawers & carets.
    *
    */
@@ -311,7 +283,6 @@ const { getComponentInstance } = require('clayutils'),
         }
       }
     });
-
   };
 
 // mount listener for vue (optional)
@@ -331,7 +302,6 @@ document.addEventListener('station-nav-mount', function () {
   listenNavComponent = document.querySelector('.component--station-listen-nav');
   stationId = stationNav.dataset.stationId;
   stationListenNavInstance = getComponentInstance(listenNavComponent.dataset.uri);
-
 
   addEventListeners();
 });
