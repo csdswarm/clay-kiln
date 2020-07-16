3ab8ef74edd9311a52ea866aaadfd45577fa5d38
Tiffany Chow <tiffachow@gmail.com>
2020-07-15 20:02:32 -0400

    ON-2187 Fix bug where secondary sf was getting overwritten to '' during old upgrade script

--- app/components/article/upgrade.js
+++ app/components/article/upgrade.js
@@ -169,7 +169,9 @@ module.exports['7.0'] = async function (uri, data) {
 module.exports['8.0'] = function (uri, data) {
   const newData = Object.assign({}, data);
 
-  newData.secondarySectionFront = data.secondaryArticleType || '';
+  if (data.secondaryArticleType) {
+    newData.secondarySectionFront = data.secondaryArticleType || '';
+  }
 
   delete newData.secondaryArticleType;
 
