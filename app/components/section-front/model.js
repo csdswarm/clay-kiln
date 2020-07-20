9285e9cfe6fb8ca01d1a5f8492837d10667528b5
jeremy <jeremy.zevin@entercom.com>
2020-07-20 13:43:35 -0400

    ON-2175: jz | add primaryColor when prim natl SF

--- app/components/section-front/model.js
+++ app/components/section-front/model.js
@@ -1,6 +1,38 @@
 'use strict';
 const { unityComponent } = require('../../services/universal/amphora'),
-  { assignStationInfo } = require('../../services/universal/create-content');
+  { assignStationInfo } = require('../../services/universal/create-content'),
+  _get = require('lodash/get');
+
+/**
+ * adds the primaryColor to _computed if a primary section front
+ * which is used in the template to set a global css var and
+ * eliminates the need for complicated postCSS mixin
+ *
+ * @param {object} data
+ * @param {object} locals
+ * @returns {string}
+ */
+function addPrimaryColorToPrimarySectionFronts(data, locals) {
+  let primaryColor = '';
+
+  switch (_get(locals,'sectionFront', '').toLowerCase()) {
+    case 'music':
+      primaryColor = '#FF4940';
+      break;
+    case 'news':
+      primaryColor = '#FEC517';
+      break;
+    case 'sports':
+      primaryColor = '#0099FF';
+      break;
+    case 'audio':
+      primaryColor = '#00CCB7';
+      break;
+    default:
+      break;
+  }
+  return primaryColor;
+}
 
 module.exports = unityComponent({
   render: (uri, data, locals) => {
@@ -12,6 +44,8 @@ module.exports = unityComponent({
         locals.secondarySectionFront = data.title.toLowerCase();
       }
     }
+
+    data._computed.primaryColor = addPrimaryColorToPrimarySectionFronts(data, locals);
 
     return data;
   },
