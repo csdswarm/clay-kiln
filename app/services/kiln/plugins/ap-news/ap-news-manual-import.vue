dbebe925871fad1fdcf6de5082b805c6f01a5210
Luis Felipe Bertel Mercado <luis.bertel@gorillalogic.com>
2020-07-28 11:34:58 -0500

    ON-1980 | integrated with be services.

--- app/services/kiln/plugins/ap-news/ap-news-manual-import.vue
+++ app/services/kiln/plugins/ap-news/ap-news-manual-import.vue
@@ -18,21 +18,35 @@
       v-model="filter"
       @change="searchContent"
     ></ui-textbox>
-    <ui-progress-circular color="primary" v-show="isLoading" style="width: 100%; height: 44px;"></ui-progress-circular>
-    <div v-show="items.length > 0" class="page-list-readout ap-media-manual-import__list">
-        <div 
+    <ui-progress-circular
+      color="primary"
+      v-show="isLoading"
+      style="width: 100%; height: 44px;"
+    ></ui-progress-circular>
+    <div
+      v-show="items.length > 0"
+      class="page-list-readout ap-media-manual-import__list"
+    >
+      <div
         class="page-list-item ap-media-manual-import__list-item"
-        :class="{'ap-media-manual-import__list-item--selected': item.headline === article.headline}"
-        @click="selectArticle(item)" :key="idx" v-for="(item, idx) in items" 
-        >
-          <div class="page-list-item-title">{{ item.headline }}</div>
-          <div class="page-list-item-status">
-            <a :href="`https://apnews.com/${ item.altids.itemid }`" target="_blank">View</a>
-            <span>{{ formatStatusTime(item.versioncreated) }}</span>
-          </div>
+        :class="{
+          'ap-media-manual-import__list-item--selected':
+            item.headline === article.headline,
+        }"
+        @click="selectArticle(item)"
+        :key="idx"
+        v-for="(item, idx) in items"
+      >
+        <div class="page-list-item-title">{{ item.headline }}</div>
+        <div class="page-list-item-status">
+          <a :href="`https://apnews.com/${item.altids.itemid}`" target="_blank"
+            >View</a
+          >
+          <span>{{ formatStatusTime(item.versioncreated) }}</span>
         </div>
+      </div>
     </div>
-    
+
     <div v-if="article" class="ap-media-manual-import__mapping">
       <div class="ap-media-manual-import__mapping--options">
         <ui-select
@@ -60,7 +74,13 @@
       </div>
       <div class="ap-media-manual-import__mapping--buttons">
         <ui-button @click="clearSelections" color="default">Clear</ui-button>
-        <ui-button @click="importContent" :disabled="!enableImport" :loading="isSubmitting" color="primary">Import</ui-button>
+        <ui-button
+          @click="importContent"
+          :disabled="!enableImport"
+          :loading="isSubmitting"
+          color="primary"
+          >Import</ui-button
+        >
       </div>
     </div>
   </div>
@@ -74,30 +94,30 @@ const {
   UiProgressCircular,
   UiButton,
 } = window.kiln.utils.components;
-import axios from 'axios';
-import moment from 'moment';
+import axios from "axios";
+import moment from "moment";
 
 const AP_MEDIA_API_SEARCH = "/rdc/ap-subscriptions/search";
 const AP_MEDIA_API_IMPORT = "/rdc/ap-subscriptions/manual-import";
 
 export default {
-  name: 'ap-media-manual-import-import',
-  props: ['entitlements'],
+  name: "ap-media-manual-import-import",
+  props: ["entitlements"],
   data() {
     return {
       isLoading: false,
       isSubmitting: false,
-      filter: '',
+      filter: "",
       selectedEntitlements: [],
       items: [],
-      article: '',
-      station: '',
-      primarySectionFront: '',
-      secondarySectionFront: '',
+      article: "",
+      station: "",
+      primarySectionFront: "",
+      secondarySectionFront: "",
       sectionFronts: {
         primarySectionFronts: [],
-        secondarySectionFronts: []
-      }
+        secondarySectionFronts: [],
+      },
     };
   },
   components: {
@@ -113,7 +133,7 @@ export default {
         return {
           label: entitlement.name,
           value: entitlement.value,
-          class: 'ap-media-manual-import__entitlement--option',
+          class: "ap-media-manual-import__entitlement--option",
         };
       });
     },
@@ -127,14 +147,13 @@ export default {
         }
       );
     },
-    enableImport(){
-      return this.station && this.primarySectionFront  
-    }
+    enableImport() {
+      return this.station && this.primarySectionFront;
+    },
   },
   methods: {
     selectArticle(article) {
       this.article = article;
-      
     },
     clearSelections() {
       this.filter = "";
@@ -143,9 +162,9 @@ export default {
       this.$refs.primarySectionFrontSelect.reset();
       this.$refs.secondarySectionFrontSelect.reset();
       this.items = [];
-      this.station = ''
-      this.primarySectionFront = ''
-      this.secondarySectionFront = ''
+      this.station = "";
+      this.primarySectionFront = "";
+      this.secondarySectionFront = "";
     },
     stations() {
       return Object.values(window.kiln.locals.stationsIHaveAccessTo).map(
@@ -164,39 +183,51 @@ export default {
     mapToOptions(item) {
       return {
         label: item.name,
-        value: item.value
-      }
+        value: item.value,
+      };
     },
-    async getList(listName){
+    async getList(listName) {
       try {
-        const response = await axios.get(
-          `/_lists/${listName}`
-        );
-        
-        return response.data || [];
+        const response = await axios.get(`/_lists/${listName}`);
+
+        return response.data || [];
       } catch (err) {
         console.log(`An error ocurred while fetching /_lists/${listName}`, err);
         return [];
       }
     },
-    async fetchStationFronts(station){
-      const stationSlug = station.value ? `${station.value}-` : ''
-      
+    async fetchStationFronts(station) {
+      const stationSlug = station.value ? `${station.value}-` : "";
+
       // Reset selectors and state.
       this.sectionFronts.primarySectionFronts = [];
       this.sectionFronts.secondarySectionFronts = [];
-      this.$refs.primarySectionFrontSelect ? this.$refs.primarySectionFrontSelect.reset() : null;
-      this.$refs.secondarySectionFrontSelect ? this.$refs.secondarySectionFrontSelect.reset() : null;
+      this.$refs.primarySectionFrontSelect
+        ? this.$refs.primarySectionFrontSelect.reset()
+        : null;
+      this.$refs.secondarySectionFrontSelect
+        ? this.$refs.secondarySectionFrontSelect.reset()
+        : null;
 
       try {
-        const primarySectionFronts = await this.getList(`${stationSlug}primary-section-fronts`);
-        const secondarySectionFronts = await this.getList(`${stationSlug}secondary-section-fronts`);
+        const primarySectionFronts = await this.getList(
+          `${stationSlug}primary-section-fronts`
+        );
+        const secondarySectionFronts = await this.getList(
+          `${stationSlug}secondary-section-fronts`
+        );
 
-        this.sectionFronts.primarySectionFronts = primarySectionFronts.map(this.mapToOptions);
-        this.sectionFronts.secondarySectionFronts = secondarySectionFronts.map(this.mapToOptions)
-
+        this.sectionFronts.primarySectionFronts = primarySectionFronts.map(
+          this.mapToOptions
+        );
+        this.sectionFronts.secondarySectionFronts = secondarySectionFronts.map(
+          this.mapToOptions
+        );
       } catch (err) {
-        console.log(`An error ocurred while fetching section fronts for ${stationSlug}`, err);
+        console.log(
+          `An error ocurred while fetching section fronts for ${stationSlug}`,
+          err
+        );
       }
     },
     async searchContent() {
@@ -204,62 +235,73 @@ export default {
       this.isLoading = true;
       try {
         const filterConditions = [
-        `${
-          this.selectedEntitlements.length
-            ? `productid:(${this.selectedEntitlements.join(" OR ")})`
-            : ''
-        }`,
-        `${this.filter ? `headline:${this.filter}` : ""}`,
-      ].filter((item) => item).join(' AND ');
+          `${
+            this.selectedEntitlements.length
+              ? `productid:(${this.selectedEntitlements.join(" OR ")})`
+              : ""
+          }`,
+          `${this.filter ? `headline:${this.filter}` : ""}`,
+        ]
+          .filter((item) => item)
+          .join(" AND ");
 
-      const response = await axios.get(AP_MEDIA_API_SEARCH, {
-        params: {
-          q: filterConditions,
-        },
-      });
-      this.items = response.data;
+        const response = await axios.get(AP_MEDIA_API_SEARCH, {
+          params: {
+            q: filterConditions,
+          },
+        });
+        this.items = response.data;
       } catch (err) {
-        console.log('Something went wrong while fetching content', err);
+        console.log("Something went wrong while fetching content", err);
       } finally {
         this.isLoading = false;
       }
-      
     },
 
-    async importContent(){
+    async importContent() {
       const payload = {
         apMeta: this.article,
-        stationMappings : {
-          [this.station.value] : {
+        stationMappings: {
+          [this.station.value]: {
             sectionFront: this.primarySectionFront.value,
-            secondarySectionFront: this.secondarySectionFront.value
-          }
+            secondarySectionFront: this.secondarySectionFront.value,
+          },
         },
-        locals: window.kiln.locals
-      }
+        locals: window.kiln.locals,
+      };
       try {
         this.isSubmitting = true;
-        const response = await axios.post(
-          AP_MEDIA_API_IMPORT, {
-            apMeta: this.article,
-            stationMappings : {
-              [this.station.value] : {
-                sectionFront: this.primarySectionFront.value,
-                secondarySectionFront: this.secondarySectionFront.value
-              }
+        const response = await axios.post(AP_MEDIA_API_IMPORT, {
+          apMeta: this.article,
+          stationMappings: {
+            [this.station.value]: {
+              sectionFront: this.primarySectionFront.value,
+              secondarySectionFront: this.secondarySectionFront.value,
             },
-            locals: window.kiln.locals
-          }
-        );
-        console.log('DEBUG:::::::::::::::::::::: apMeta', this.article);
-        console.log('DEBUG:::::::::::::::::::::: response', response);
+          },
+          locals: window.kiln.locals,
+        });
+
+        this.$store.dispatch("showSnackbar", {
+          message: "Article Imported",
+          duration: 5000,
+          position: "left",
+          queueSnackbars: true,
+          transition: "fade",
+        });
       } catch (err) {
-        console.log(`Something happened while saving`, err);
+         this.$store.dispatch("showSnackbar", {
+          message: "Failed Importing Article",
+          duration: 5000,
+          position: "left",
+          queueSnackbars: true,
+          transition: "fade",
+        });
         return [];
       } finally {
-         this.isSubmitting = false;
+        this.isSubmitting = false;
       }
-    }
+    },
   },
 };
 </script>
