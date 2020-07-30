ddfdb25fd600295f60a34f0b3600f5cfcbb403a6
Kurt Moffett <kurtis.moffett@entercom.com>
2020-07-30 12:24:43 -0700

    ON-2099 | Moved logic from models into upgrades. Added original code back to recirculation.js and updated logic for correcting tags as arrays of strings.

--- app/components/latest-recirculation/upgrade.js
+++ app/components/latest-recirculation/upgrade.js
@@ -58,3 +58,16 @@ module.exports['6.0'] = (uri, data) => {
   return data;
 };
 
+module.exports['7.0'] = (uri, data) => {
+  // Account for inccorrect formatting of data.tag in the database for previously created content.
+  // Instances of data.tag that do not return an empty array, or properly formatted tag array will break the kiln UI.
+  // e.g. tag array: data.tag: [] || data.tag: [{ text: 'tag' }].
+  if (data.tag.length === 0) {
+    data.tag = [];
+    return data;
+  } else if (Array.isArray(data.tag)) {
+    data.tag = data.tag.map((tag) => typeof tag === 'string' ? { text: tag } : tag);
+    return data;
+  }
+  return data;
+};
