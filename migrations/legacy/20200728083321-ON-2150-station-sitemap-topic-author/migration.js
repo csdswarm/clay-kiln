992fcbde0f3481ae5396891b2d8db22e6f8dc492
Kurt Moffett <kurtis.moffett@entercom.com>
2020-07-31 13:00:42 -0700

    ON-2150 | Added logic to fix baseUrl in sitemaps.

--- migrations/legacy/20200728083321-ON-2150-station-sitemap-topic-author/migration.js
+++ migrations/legacy/20200728083321-ON-2150-station-sitemap-topic-author/migration.js
@@ -4,15 +4,24 @@ const usingDb = require('../using-db').v1,
   fs = require('fs'),
   util = require('util'),
   readFileAsync = util.promisify(fs.readFile);
+const {
+  parseHost,
+} = require('../migration-utils').v1;
+const host = process.argv[2];
+const { url } = parseHost(host); // should happen after migration-utils is imported
 
 run()
+
+function transformBaseUrl(query) {
+  return query.replace(/{{baseUrl}}/g, url);
+}
 
 async function run() {
   try {
     const [stationAuthor, stationTopic] = await Promise.all([
       readFileAsync('./create-sitemap/station-author.sql', 'utf8'),
       readFileAsync('./create-sitemap/station-topic.sql', 'utf8')
-    ]);
+    ]).map(transformBaseUrl);
 
     await usingDb(db => Promise.all([
       db.query(stationAuthor),
