01ac608be720355d00f1ee82b29204f170ef5df5
Philip Olson <phil.olson@swarmsolutions.com>
2020-08-11 16:15:30 -0500

    ON-2282: add tests
    - add a success test for a mock article and gallery each.  I tried to encompass
      all the data properties referenced in the transformer.
    
    note in code regarding mock locals
    - I chose to use an example snapshot of locals since the testing was written way
      after all the components which require various properties on locals - so
      figuring out which properties needed to be mocked would have been very
      time consuming.

---
+++ app/components/feeds/transforms/freq-content.test/expected/article.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = [
+  { title: { _cdata: 'test headline' } },
+  { link: 'https://clay.radio.com/articles/abc-music-news/leslie-odom-jr-reveals-that-he-almost-walked-out-of-the-hamilton-movie-over' },
+  { pubDate: 'Tue, 11 Aug 2020 03:30:00 -0500' },
+  {
+    guid: [
+      {
+        _attr: {
+          isPermaLink: false
+        }
+      },
+      '664887'
+    ]
+  },
+  { syndicatedUrl: 'https://www.radio.com/news/delta-to-resume-flights-between-united-states-and-china' },
+  { description: { _cdata: 'test seoDescription' } },
+  { 'content:encoded': { _cdata: '' } },
+  { stationUrl: 'http://www.610sports.com/' },
+  { stationTitle: undefined },
+  { subHeadline: 'test subHeadline' },
+  { seoHeadline: { _cdata: 'test seoHeadline' } },
+  { coverImage: 'https://stg-images.radio.com/aiu-media/e-leslieodomjr-081120.jpg' },
+  { featured: false },
+  { featured_sports: false },
+  { featured_news: false },
+  { lead: { _cdata: '' } },
+  { category: 'test tag 1' },
+  { category: 'test tag 2' },
+  { 'dc:creator': { slug: 'bob-diehl', text: 'Bob Diehl' } },
+  { editorialFeeds: 'Trending' }
+];
---
+++ app/components/feeds/transforms/freq-content.test/expected/gallery.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = [
+  { title: { _cdata: 'test headline' } },
+  { link: 'https://clay.radio.com/music/gallery/test-seo-headline' },
+  { pubDate: 'Tue, 11 Aug 2020 15:53:26 -0500' },
+  {
+    guid: [
+      {
+        _attr: {
+          isPermaLink: false
+        }
+      },
+      'ckdqf7jdz000ke3o4vtvyry1f'
+    ]
+  },
+  { syndicatedUrl: undefined },
+  {
+    description: {
+      _cdata: 'test seo description test seo description test seo description test seo description test seo description test seo description test seo description test seo description '
+    }
+  },
+  { 'content:encoded': { _cdata: '' } },
+  { stationUrl: undefined },
+  { stationTitle: undefined },
+  { subHeadline: 'test subheadline' },
+  { seoHeadline: { _cdata: 'test seo headline' } },
+  { coverImage: 'https://stg-images.radio.com/aiu-media/cat1-e0e6c162-3f21-4acd-b4dc-1248794eefef.jpg' },
+  { featured: true },
+  { featured_sports: true },
+  { featured_news: true },
+  { slides: { _cdata: '' } },
+  { lead: { _cdata: '' } },
+  { category: 'test tag 1' },
+  { category: 'test tag 2' },
+  { 'dc:creator': { slug: 'bob-diehl', text: 'Bob Diehl' } }
+];
