f0501b80927026123ecf504ca9e617a23f79d66b
Nick Renford <nick.renford@entercom.com>
2020-08-25 09:14:26 -0600

    ON-2356 Hide expired contests from contest list

--- app/components/contest-rules-page/model.js
+++ app/components/contest-rules-page/model.js
@@ -5,17 +5,18 @@
  */
 
 'use strict';
-/* eslint-disable one-var */
+const
+  _get = require('lodash/get'),
+  log  = require('../../services/universal/log').setup({ file: __filename }),
+  moment = require('moment'),
+  rest = require('../../services/universal/rest.js'),
+  { getContestRules } = require('../../services/server/contest-rules'),
+  { isPresentationMode } = require('../../services/universal/contest-rules-page'),
+  {
+    PRIVACY_POLICY
+  } = require('../../services/universal/constants'),
+  { unityComponent } = require('../../services/universal/amphora');
 
-const { unityComponent } = require('../../services/universal/amphora');
-const rest = require('../../services/universal/rest.js');
-const { getContestRules } = require('../../services/server/contest-rules');
-const {
-  PRIVACY_POLICY
-} = require('../../services/universal/constants');
-const { isPresentationMode } = require('../../services/universal/contest-rules-page');
-const _get = require('lodash/get');
-const log = require('../../services/universal/log').setup({ file: __filename });
 
 module.exports = unityComponent({
   render: async (ref, data, locals) => {
@@ -26,25 +27,27 @@ module.exports = unityComponent({
       return data;
     }
 
-    const { station, defaultStation, site } = locals;
-    const { callsign: defaultCallsign } = defaultStation;
-    const { protocol } = site;
-    const callsign = _get(locals, 'stationForPermissions.callsign', defaultCallsign);
-    const showPresentation = isPresentationMode(locals.url);
+    const { station, defaultStation, site } = locals,
+      { callsign: defaultCallsign } = defaultStation,
+      { protocol } = site,
+      callsign = _get(locals, 'stationForPermissions.callsign', defaultCallsign),
+      showPresentation = isPresentationMode(locals.url);
 
     try {
-      const stationSlugContext = station.site_slug ? `${station.site_slug}/` : '';
-      const stationPath = `${process.env.CLAY_SITE_PROTOCOL}://${process.env.CLAY_SITE_HOST}/${stationSlugContext}`;
-      const contestRules = await Promise.all((await getContestRules({
-        stationCallsign: callsign
-      })).map(async (ruleData) => ({
-        ...ruleData,
-        stationTimeZone: locals.station.timezone,
-        showHeader: true,
-        showPresentation,
-        description: (await rest.get(`${protocol}://${ruleData.description[0]._ref}`)).text,
-        contestSlug: `${stationPath}contests/${ruleData.slug}`
-      })));
+      const stationSlugContext = station.site_slug ? `${station.site_slug}/` : '',
+        stationPath = `${process.env.CLAY_SITE_PROTOCOL}://${process.env.CLAY_SITE_HOST}/${stationSlugContext}`,
+        contestRules = await Promise.all((await getContestRules({
+          stationCallsign: callsign
+        })).map(await (async (ruleData) => ({
+          ...ruleData,
+          stationTimeZone: locals.station.timezone,
+          showHeader: true,
+          showPresentation,
+          description: (await rest.get(`${protocol}://${ruleData.description[0]._ref}`)).text,
+          contestSlug: `${stationPath}contests/${ruleData.slug}`
+        }))).filter((rulesBlock)=>{
+          return moment().isAfter(rulesBlock.endDateTime,'day');
+        }));
 
       data._computed = {
         showPrivacyPolicy: !showPresentation,
