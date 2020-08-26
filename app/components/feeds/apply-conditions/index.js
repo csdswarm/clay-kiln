93955ede268999ac796c893e48eb53ba11bab426
Philip Olson <phil.olson@swarmsolutions.com>
2020-08-26 15:17:09 -0500

    ON-2398: implement andFilter and orFilter
    also refactor feeds/model.js to make the code more manage'able
    
    - extract queryFilters into its own file now called 'conditions.js'
      : only one change was made in this code, importer.excludeConditionType was
        removed since we weren't passing any overrides for that condition type
      : the term 'condition' is intended to encompass both 'filter' and 'exclude'
    - extract and refactor code which applied the conditions.  The new file
      is ./apply-conditions/index.js
      : this needed to be refactored in order to support andFilter and orFilter.
        Additionally the new code allows us to handle multiple of the same filter
        i.e. ?andFilter[tag]=sports&andFilter[tag]=eagles works like you'd expect.
        The old filter was left alone in this regard for backward compatibility.
      : addCondition was left alone.  It was too hairy for me to want to touch (ha).
    - extract the code which generates the ES query for rendering the component.
      from model.js -> get-render-query.js
      : the only changes I made here were to separate server/query.js and
        universal/query.js to make testing easier
    - what's left of model.js, refactor slightly to make the code more succinct and
      read better
    - update rendererPipeline to ensure the station and station feed filters were
      handled correctly from both filter and andFilter.  Also add validation to make
      sure these params sent are appropriate, throw an error otherwise.
    - update all references to utils.js -> renderer-pipeline.js.  Utils was too big
      to support additional functions.
    - add tests for get-render-query.js, renderer-pipeline.js and
      apply-conditions/index.js.  More tests could have been added but none of the
      other files had logic modified so we can write tests for those files in
      future tickets.

---
+++ app/components/feeds/apply-conditions/index.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const conditions = require('./conditions'),
+  queryService = require('../../../services/universal/query');
+
+/**
+ * Applies the filter to the query per the passed in condition type
+ *
+ * Note: conditionType 'default' should be considered deprecated and remain
+ *   as-is for backward compatibility with the frequency feeds.  'Why' is
+ *   explained in the commit message.
+ *
+ * @param {object} query - this is mutated
+ * @param {object} filter - the parsed query value from express
+ * @param {string} [conditionType] - 'default', 'addShould' or 'addMust'
+ */
+function applyFilterByType(query, filter, conditionType = 'default') {
+  const isDeprecatedFilter = conditionType === 'default';
+
+  for (const [filterKey, filterVal] of Object.entries(filter)) {
+    const aCondition = conditions[filterKey],
+      appliedConditionType = isDeprecatedFilter
+        ? aCondition.filterConditionType || 'addShould'
+        : conditionType;
+
+    // filterVal will be an array in the case where multiple filters are
+    //   provided e.g. ?andFilter[tag]=sports&andFilter[tag]=eagles
+    if (isDeprecatedFilter || !Array.isArray(filterVal)) {
+      addCondition(query, filterVal, aCondition, appliedConditionType);
+    } else {
+      for (const val of filterVal) {
+        addCondition(query, val, aCondition, appliedConditionType);
+      }
+    }
+  }
+}
+
+/**
+ * Applies the exclude to the query
+ *
+ * @param {object} query - this is mutated
+ * @param {object} exclude - the parsed query value from express
+ */
+function applyExclude(query, exclude) {
+  for (const [excludeKey, excludeVal] of Object.entries(exclude)) {
+    const aCondition = conditions[excludeKey];
+
+    addCondition(query, excludeVal, aCondition, 'addMustNot');
+  }
+}
+
+/**
+ * add a condition to the query
+ *
+ * @param {object} query - the ES query to update
+ * @param {*} queryParamVal - this is passed to the condition
+ * @param {object} aCondition
+ * @param {string} conditionType - possible values addShould/addMust/addMustNot
+ */
+function addCondition(query, queryParamVal, aCondition, conditionType) {
+  const { nested, multiQuery, createObj } = aCondition,
+    localQuery = nested
+      ? queryService.newNestedQuery(nested)
+      : query,
+    items = typeof item === 'string'
+      ? queryParamVal.split(',')
+      : [queryParamVal];
+
+  items.forEach(instance => {
+    if (multiQuery) {
+      createObj(instance).forEach(cond => {
+        if (cond.nested) {
+          let nestedQuery = queryService.newNestedQuery(cond.nested);
+
+          nestedQuery = queryService[conditionType](nestedQuery, cond);
+
+          const queryOp = conditionType === 'addMustNot'
+            ? 'addMust'
+            : conditionType;
+
+          queryService[queryOp](localQuery, nestedQuery);
+        } else {
+          queryService[conditionType](localQuery, cond);
+        }
+      });
+    } else {
+      queryService[conditionType](localQuery, createObj(instance));
+    }
+
+    if (conditionType === 'addShould') {
+      queryService.addMinimumShould(localQuery, 1);
+    }
+  });
+
+  // add nested queries back into the main query
+  if (nested) {
+    queryService.addShould(query, localQuery);
+    queryService.addMinimumShould(query, 1);
+  }
+}
+
+/**
+ * Applies the filters and excludes passed in the url query
+ *
+ * @param {object} query - the ES query - this is mutated
+ * @param {object} locals
+ */
+module.exports = (query, locals) => {
+  const {
+    andFilter = {},
+    exclude = {},
+    filter = {},
+    orFilter = {}
+  } = locals;
+
+  applyFilterByType(query, andFilter, 'addMust');
+  applyFilterByType(query, filter);
+  applyFilterByType(query, orFilter, 'addShould');
+
+  applyExclude(query, exclude);
+};
