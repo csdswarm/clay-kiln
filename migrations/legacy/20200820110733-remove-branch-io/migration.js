93ada93d0e76e3633e417594516108105582417d
Cristhiam Henao <cristhiam.henao@gorillalogic.com>
2020-09-01 15:54:10 -0500

    ON-2378 | Remove branch-io-head instance from pages

--- migrations/legacy/20200820110733-remove-branch-io/migration.js
+++ migrations/legacy/20200820110733-remove-branch-io/migration.js
@@ -1,73 +1,42 @@
 'use strict';
 
-const Promise = require('../../../app/node_modules/bluebird');
-
-const {
-  removeComponentsFromContainers,
-  usingDb
-} = require('../migration-utils').v1;
-
-const { parseHost } = require('../migration-utils').v2;
-
-const host = process.argv[2] || 'clay.radio.com',
-  envInfo = parseHost(host);
-  
+const { usingDb } = require('../migration-utils').v1,
+  host = process.argv[2] || 'clay.radio.com',
+  Promise = require('../../../app/node_modules/bluebird');
 
 getPagesUsingBranchIO();
 
 // helper functions
-function removeBranchIoComponents(page, branchIOInstance) {
-  return removeComponentsFromContainers({
-    envInfo,
-    remove: {
-      [page]: {
-        head: branchIOInstance
-      }
-    }
-  })
-}
-
-async function getAllArchivedPages(db, host) {
-  const archivedQuery = `SELECT p.id, p.data
-      FROM pages p
-      WHERE (meta->>'published')::boolean IS false
-      AND (meta->>'archived')::boolean IS true`,
-    archivedResult = await db.query(archivedQuery);
-
-  return archivedResult.rows
-    .filter(({ id }) => (
-      id.startsWith(host)
-    )).map(({id}) => id.slice((host).length))
+async function removeBranchIOComponents(db, id, data) {
+  await db.query(
+    `update pages
+    set data = $2
+    where id = $1`,
+    [id, data]
+  )
 }
 
 async function getAllPagesWithBranchIOHead(db, host) {
   const query = `SELECT p.id, p.data
       FROM pages p
-      WHERE id ~ '@published$' 
-      AND data->>'head' ~ 'branch-io-head'`,
+      WHERE data->>'head' ~ 'branch-io-head'`,
     result = await db.query(query);
 
-  return result.rows
-    .filter(({ id }) => (id.startsWith(host)))
-    .map((row) => Object.assign({}, row, { id: row.id.replace('@published', '') }))
-    .map((row) => Object.assign({}, row, { id: row.id.slice((host).length) }))
+  return result.rows.filter(({ id }) => id.startsWith(host))
 }
 
 async function getPagesUsingBranchIO() {
   try {
     await usingDb(async db => {
-      const archivedPages = await getAllArchivedPages(db, host),
-        pages = await getAllPagesWithBranchIOHead(db, host);
+      const pages = await getAllPagesWithBranchIOHead(db, host);
 
-      return Promise.map(pages, async ({ id, data: { head } }) => {
-        if (!archivedPages.includes(id)) {
-          const branchIOInstance = head
-            .filter(instance => instance.includes('branch-io-head'))
-            .map((row) => row.replace('@published', ''))
-            .map((row) => row.slice((host).length))
+      return Promise.map(pages, async ({ id, data }) => {
+        const { head } = data,
+          [ branchIOInstance ] = head.filter(instance => instance.includes('branch-io-head')),
+          index = head.indexOf(branchIOInstance);
 
-          return await removeBranchIoComponents(id, branchIOInstance);
-        }
+        head.splice(index, 1);
+        return await removeBranchIOComponents(db, id, data);
       }, { concurrency: 5 }
     )
     })
