7cdefea91b3a25ec68256320913ed215b236c6f4
Alejandro Chaves Orozco <alejandro.chaves@gorillalogic.com>
2020-09-01 16:15:41 -0500

    [ON-2360] Use css theming, bring its own header styles

--- app/components/podcast-front-page/model.js
+++ app/components/podcast-front-page/model.js
@@ -1,48 +1,9 @@
 'use strict';
 
 const { unityComponent } = require('../../services/universal/amphora'),
-  stationThemingApi = require('../../services/server/stationThemingApi'),
-  { COLORS, DEFAULT_STATION } = require('../../services/universal/constants'),
-  { defaultTheme } = require('../theme/model'),
   { assignStationInfo } = require('../../services/universal/create-content/index');
 
-const sectionFrontToPrimaryColor = {
-  audio: COLORS.robinsEggBlue,
-  music: COLORS.sunsetOrange,
-  news: COLORS.lightningYellow,
-  sports: COLORS.azureRadience
-};
-
-/**
- * returns the primary color based first off station then section front
- *
- * @param {object} locals
- * @returns {string}
- */
-async function getPrimaryColor(locals) {
-  const { sectionFront = '', station = {} } = locals;
-
-  if (station.id !== DEFAULT_STATION.id) {
-    try {
-      const { primaryColor } = await stationThemingApi.get(station.site_slug, defaultTheme);
-
-      return primaryColor;
-    } catch (err) {
-      console.error(err);
-
-      return defaultTheme.primaryColor;
-    }
-  }
-
-  return sectionFrontToPrimaryColor[sectionFront.toLowerCase()]
-    || defaultTheme.primaryColor;
-}
-
 module.exports = unityComponent({
-  render: async (uri, data, locals) => {
-    data._computed.primaryColor = await getPrimaryColor(locals);
-    return data;
-  },
   /**
    * Makes any necessary modifications to data just prior to persisting it
    *
