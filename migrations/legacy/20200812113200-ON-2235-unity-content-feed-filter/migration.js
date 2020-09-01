344974cecdccbeeb558b081fa36c386a1b014412
davirlein <davirlein.prada@gorillalogic.com>
2020-09-01 10:12:28 -0500

    ON-2235 Update feed for postup and reversechron

--- migrations/legacy/20200812113200-ON-2235-unity-content-feed-filter/migration.js
+++ migrations/legacy/20200812113200-ON-2235-unity-content-feed-filter/migration.js
@@ -1,20 +1,117 @@
 'use strict';
 
-const fs = require('fs'),
-  migrationUtils = require('../../utils/migration-utils'),
+const migrationUtils = require('../../utils/migration-utils'),
   { clayImport } = migrationUtils.v1,
   { parseHost } = migrationUtils.v2;
 
-const { host } = parseHost(process.argv.slice(2)[0]);
+const { host = 'clay.radio.com', http, hostname } = parseHost(process.argv.slice(2)[0]);
 
 run()
 
 async function run() {
   try {
 
-    const payload = fs.readFileSync('./_components.yml', 'utf8');
+    const payloadPostup = 
+      {
+        _components: {
+          feeds: {
+            instances: {
+              postup: {
+                meta: {
+                  link: `${http}://${hostname}`,
+                  title: 'Radio.com PostUp Feed',
+                  renderer: 'rss',
+                  contentType: 'application/rss+xml',
+                  description: 'Most recent content from Radio.com',
+                  fileExtension: 'rss'
+                },
+                attr: {
+                  'xmlns:radio': 'https://www.radio.com'
+                },
+                index: 'published-content',
+                  query: {
+                    size: 20,
+                      sort: {
+                      date: 'desc'
+                    },
+                    bool: {
+                      filter: [
+                        {
+                          match: {
+                            'contentType': 'article'
+                          }
+                        },
+                        {
+                          match: {
+                            'contentType': 'gallery'
+                          }
+                        },
+                        {
+                          match: {
+                            'contentType': 'contest'
+                          }
+                        },
+                        {
+                          match: {
+                            'contentType': 'event'
+                          }
+                        },
 
-    await clayImport({ hostUrl: 'https://' + host, payload });
+                      ]
+                    }
+                  }
+                ,
+                results: [],
+                transform: 'article'
+              }
+            }
+          }
+        }
+      }
+    const payloadReversechron = 
+      {
+        _components: {
+          feeds: {
+            instances: {
+              reversechron: {
+                meta: {
+                  link: `${http}://${hostname}`,
+                  title: 'Radio.com Reverse Chron Feed',
+                  renderer: 'rss',
+                  contentType: 'application/rss+xml',
+                  description: 'Most recent content from Radio.com',
+                  fileExtension: 'rss'
+                },
+                attr: {
+                  'xmlns:radio': 'https://www.radio.com'
+                },
+                index: 'published-content',
+                query: {
+                  size: 20,
+                  sort: {
+                    date: 'desc'
+                  },
+                  bool: {
+                    filter: [
+                      {
+                        match: { contentType: 'article' }
+                      },
+                      {
+                        match: { contentType: 'gallery' }
+                      },
+                    ]
+                  }  
+                },
+                results: []
+              }
+            }
+          }
+        }
+      }
+
+    await clayImport({ hostUrl: 'https://' + host, payload: payloadPostup });
+    await clayImport({ hostUrl: 'https://' + host, payload: payloadReversechron });
+
 
     console.log('feeds updated successfully');
 
