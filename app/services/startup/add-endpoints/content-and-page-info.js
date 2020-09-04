481b502632a10585df74232eaa50e278b3c9a48b
Tiffany Chow <tiffachow@gmail.com>
2020-09-04 11:10:04 -0400

    ON-2369 Add generic endpoint to check if page exists in db

--- app/services/startup/add-endpoints/content-and-page-info.js
+++ app/services/startup/add-endpoints/content-and-page-info.js
@@ -47,4 +47,19 @@ module.exports = router => {
       res.send(result.rows[0]);
     }
   }));
+  router.get('/rdc/station-migration/generic-page-info', wrapInTryCatch(async (req, res) => {
+    preventFastlyCache(res);
+
+    const result = await db.raw(`
+      SELECT id, data, meta
+      FROM pages 
+      WHERE meta->>'url' = ?
+    `, [req.query.url]);
+
+    if (!result.rows.length) {
+      res.status(404).end();
+    } else {
+      res.send(result.rows[0]);
+    }
+  }));
 };
