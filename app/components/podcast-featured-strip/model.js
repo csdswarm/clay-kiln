4b6af1792d099d4beb2d6fcdd5911ec6208e1d66
Juan Camilo Giraldo <juan.giraldo@gorillalogic.com>
2020-09-10 11:42:18 -0500

    ON-2486 | JCG | filter unselected podcasts

--- app/components/podcast-featured-strip/model.js
+++ app/components/podcast-featured-strip/model.js
@@ -10,17 +10,29 @@ const utils = require('../../services/universal/podcast'),
  */
 module.exports = unityComponent({
   async render(ref, data) {
-    data._computed.items = data.items.map(({ podcast }) => {
-      const { imageUrl, url, title, category, description, customDescription, shouldOverrideDescription, customCategoryLabel, shouldOverrideCategoryLabel } = podcast || {};
-      
-      return {
-        title,
-        url,
-        imageUrl: imageUrl ? utils.createImageUrl(imageUrl) : '',
-        description: shouldOverrideDescription ? customDescription : description,
-        label: shouldOverrideCategoryLabel ? customCategoryLabel : category.name
-      };
-    });
+    data._computed.items = data.items
+      .filter(({ podcast }) => !!podcast)
+      .map(({ podcast }) => {
+        const {
+          imageUrl,
+          url,
+          title,
+          category,
+          description,
+          customDescription,
+          shouldOverrideDescription,
+          customCategoryLabel,
+          shouldOverrideCategoryLabel
+        } = podcast;
+        
+        return {
+          title,
+          url,
+          imageUrl: imageUrl ? utils.createImageUrl(imageUrl) : '',
+          description: shouldOverrideDescription ? customDescription : description,
+          label: shouldOverrideCategoryLabel ? customCategoryLabel : category.name
+        };
+      });
     return data;
   }
 });
