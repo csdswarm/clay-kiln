7142cc1982c1179dadcf2b74c619386c3f271669
Philip Olson <phil.olson@swarmsolutions.com>
2020-09-11 09:49:27 -0500

    Revert "Merge branch 'ON-2006-add-station-syndication-to-page-index' into release-CAR-196"
    This reverts commit 729ae712c2fd317cc084e9764bb45172148f94ca, reversing
    changes made to 5cb35e5c57dac0779934edc811824a78730a497a.

--- migrations/utils/format-axios-error.js
+++ migrations/utils/format-axios-error.js
@@ -6,52 +6,33 @@ const { _, prettyJSON } = require('./base')
  * If the error is from axios with a response then format it as a string.
  *   Otherwise return the error's message.  This is meant to make errors
  *   informative yet concise because the raw error from axios is enormous.
- *
- * @param {object} err
- * @param {object} opts
- * @param {bool} opts.includeStack - if it's an axios error, determines whether
- *   the stacktrace should be displayed.  Normally this isn't useful for axios
- *   errors since axios swallows the stack trace.
- * @param {bool} opts.shouldTruncate - whether the request and response bodies
- *   should be truncated in the error output.  Sometimes these are so big that
- *   they are unhelpful/clutter.
  */
-function formatAxiosError_v1(err, opts = {}) {
-  const { response: res } = err,
-    { includeStack, shouldTruncate = true } = opts,
-    isAxiosError = !!res;
+function formatAxiosError_v1(err, { includeStack } = {}) {
+  const { response: res } = err;
 
-  let result
+  let result = includeStack
+    ? err.stack
+    : err.message;
 
-  if (!isAxiosError) {
-    result = err.stack;
-  } else {
+  if (res) {
     const req = res.config;
 
-    let resData = prettyJSON(res.data),
-      reqData = prettyJSON(JSON.parse(req.data));
-
-    if (shouldTruncate) {
-      resData = _.truncate(resData, { length: 120 });
-      reqData = _.truncate(reqData, { length: 120 });
-    }
-
-    result = includeStack
-      ? err.stack
-      : err.message;
-
-    result += '\n\n'
+    result = ''
       + 'response'
       + '\n--------'
       + '\nstatus: ' + res.status
       + '\nheaders: ' + prettyJSON(res.headers)
-      + '\ndata: ' + resData
+      + '\ndata: ' + _.truncate(prettyJSON(res.data), { length: 120 })
       + '\n\nrequest'
       + '\n-------'
       + '\nmethod: ' + req.method
       + '\nurl: ' + req.url
-      + '\ndata: ' + reqData
+      + '\ndata: ' + _.truncate(req.data, { length: 120 })
       + '\nheaders: ' + prettyJSON(req.headers)
+
+    if (includeStack) {
+      result += '\n' + err.stack;
+    }
   }
 
   return result;
