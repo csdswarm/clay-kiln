f341c4837d8f622eed79d6d5864899f7d47e58f6
Markham F Rollins IV <markham@swarmsolutions.com>
2020-09-11 09:12:43 -0400

    ON-2493: Added indices to help with migration queries and with loading the station themes. SQL used for migrations was updated and modified to no longer use a union.

--- app/services/startup/add-endpoints/content-and-page-info.js
+++ app/services/startup/add-endpoints/content-and-page-info.js
@@ -13,7 +13,8 @@ module.exports = router => {
   router.get('/rdc/station-migration/content-and-page-info', wrapInTryCatch(async (req, res) => {
     preventFastlyCache(res);
 
-    const result = await db.raw(`
+    // A Union was removed from here as it's too taxing on the DB, splitting into multiple queries reduces overall timing
+    const query = `
       SELECT
         p.id AS page_id,
         content.id AS content_uri,
@@ -21,25 +22,19 @@ module.exports = router => {
       FROM (
         SELECT id,
           data
-        FROM components.article
+        FROM ??
       ) AS content
       JOIN pages p ON p.data->'main'->>0 = content.id
       JOIN uris u ON u.data = p.id
       WHERE u.url = ?
-      UNION
-      SELECT
-        p.id AS page_id,
-        content.id AS content_uri,
-        content.data AS content_data
-      FROM (
-        SELECT id,
-          data
-        FROM components.gallery
-      ) AS content
-      JOIN pages p ON p.data->'main'->>0 = content.id
-      JOIN uris u ON u.data = p.id
-      WHERE u.url = ?
-    `, [req.query.url, req.query.url]);
+    `;
+
+    let result = await db.raw(query, ['components.article', req.query.url]);
+
+    // If there were no articles then check galleries
+    if (!result.rows.length) {
+      result = await db.raw(query, ['components.gallery', req.query.url]);
+    }
 
     if (!result.rows.length) {
       res.status(404).end();
