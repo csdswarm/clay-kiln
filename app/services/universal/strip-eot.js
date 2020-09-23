766faf2f4c31a73b5b083f318d1373c10050cf05
leland-kwong <leland.kwong@gmail.com>
2020-09-23 10:37:59 -0400

    ON-2536
    * Add jsdocs

--- app/services/universal/strip-eot.js
+++ app/services/universal/strip-eot.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Remove eot characters from the input text
+ *
+ * @param {string} text
+ * @returns {string}
+ */
 module.exports = function stripEot(text = '') {
   return text.replace('\u0004', '');
 };
