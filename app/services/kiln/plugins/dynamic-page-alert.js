3a798d48ac7bef38cd7d103be3d6e36df1a83c6e
Kurt Moffett <kurtis.moffett@entercom.com>
2020-09-25 14:33:34 -0700

    ON-1893 | Updated the page alert plugin to use lodash's set function and reduced the length of the warning string.

--- app/services/kiln/plugins/dynamic-page-alert.js
+++ app/services/kiln/plugins/dynamic-page-alert.js
@@ -1,13 +1,18 @@
 'use strict';
+const _ = require('lodash');
 
 module.exports = () => {
   const { edit, isDynamicPage } = window.kiln.locals;
 
   window.kiln = window.kiln || {};
   window.kiln.plugins = window.kiln.plugins || {};
-  window.kiln.plugins['dynamic-page-alert'] = function (store) {
+  _.set(window, ['kiln', 'plugins', 'dynamic-page-alert'], (store) => {
     if (edit && isDynamicPage) {
-      store.dispatch('addAlert', { type: 'warning', text: 'WARNING: You are currently editing a Dynamic Page.  Any changes saved to this page will be reflected on ALL pages that use this template - and across all stations on the Unity platform.  PLEASE NOTE: changes are applied immediately once saved and do not require the page to be republished.' });
+      store.dispatch('addAlert', { type: 'warning', text: `
+        WARNING: You are currently editing a Dynamic Page. Any changes saved to this page will be 
+        reflected on ALL pages that use this template - and across all stations on the Unity platform.  
+        PLEASE NOTE: changes are applied immediately once saved and do not require the page to be republished.
+      ` });
     };
-  };
+  });
 };
