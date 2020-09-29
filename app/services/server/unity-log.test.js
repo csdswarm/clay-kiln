35f37798a1dfb85787a24828f52421fe8b83ed22
Markham F Rollins IV <markham@swarmsolutions.com>
2020-09-29 11:39:29 -0400

    ON-2577: Updated to have custom error and brought back forbid-only

--- app/services/server/unity-log.test.js
+++ app/services/server/unity-log.test.js
@@ -40,6 +40,67 @@ describe('unity-log', () => {
     expect(data).to.have.property('error');
     expect(data.error).to.have.property('stack');
     expect(data.error).to.have.property('kind');
-    expect(data.error.kind).to.equal('Error');
+    expect(data.error).to.deep.include({ kind: 'Error' });
+    expect(data).to.deep.include({
+      params: {
+        foo: 'bar'
+      }
+    });
+  });
+
+  it('data contains error: CustomError', () => {
+    class CustomError extends Error {
+      constructor(message) {
+        super(message);
+        this.name = 'CustomError';
+      }
+    }
+
+    const data = {
+        error: new CustomError('data Error'),
+        params: {
+          foo: 'bar'
+        }
+      },
+      msg = 'Data will have error object';
+
+    proxyquire('./unity-log', {
+      'clay-log/plugins/_utils': {
+        wrap: (func) => func(data, msg)
+      }
+    });
+
+    expect(msg).to.equal('Data will have error object');
+    expect(data).to.have.property('error');
+    expect(data.error).to.have.property('stack');
+    expect(data.error).to.deep.include({ kind: 'CustomError' });
+    expect(data).to.deep.include({
+      params: {
+        foo: 'bar'
+      }
+    });
+  });
+
+  it('data does not contain error', () => {
+    const data = {
+        params: {
+          foo: 'bar'
+        }
+      },
+      msg = 'Data will have error object';
+
+    proxyquire('./unity-log', {
+      'clay-log/plugins/_utils': {
+        wrap: (func) => func(data, msg)
+      }
+    });
+
+    expect(msg).to.equal('Data will have error object');
+    expect(data).to.not.have.property('error');
+    expect(data).to.deep.equal({
+      params: {
+        foo: 'bar'
+      }
+    });
   });
 });
