bc955f3a2f48789d23192df18d92b5c39eaf7958
Markham F Rollins IV <markham@swarmsolutions.com>
2020-09-30 11:19:09 -0400

    ON-2577: Update context/descrtibe/it

--- app/services/server/unity-log.test.js
+++ app/services/server/unity-log.test.js
@@ -6,7 +6,7 @@ const { assert, expect } = require('chai'),
 
 describe('unity-log', () => {
   context('msg is instanceOf Error', () => {
-    it('msg unchanged, data to contain error info', () => {
+    it('msg unchanged, data to contain error info (stack/kind)', () => {
       const data = {},
         msg = new Error('msg Error');
 
@@ -16,7 +16,7 @@ describe('unity-log', () => {
         }
       });
 
-      assert.instanceOf(msg, Error, 'msg is an instance of Error');
+      assert.instanceOf(msg, Error, 'msg is no longer instance of Error');
       expect(data).to.have.property('error');
       expect(data.error).to.have.property('stack');
       expect(data.error).to.have.property('kind');
@@ -25,94 +25,98 @@ describe('unity-log', () => {
   });
 
   context('data contains error key', () => {
-    it('error is instanceOf Error', () => {
-      const data = {
-          error: new Error('data Error'),
+    describe('error', () => {
+      it('has a stack trace and kind set to Error, msg is string', () => {
+        const data = {
+            error: new Error('data Error'),
+            params: {
+              foo: 'bar'
+            }
+          },
+          msg = 'Data will have error object';
+
+        proxyquire('./unity-log', {
+          'clay-log/plugins/_utils': {
+            wrap: callWrapperImmediately(data, msg)
+          }
+        });
+
+        expect(msg).to.equal('Data will have error object');
+        expect(data).to.have.property('error');
+        expect(data.error).to.have.property('stack');
+        expect(data.error).to.have.property('kind');
+        expect(data.error).to.deep.include({ kind: 'Error' });
+        expect(data).to.deep.include({
           params: {
             foo: 'bar'
           }
-        },
-        msg = 'Data will have error object';
-
-      proxyquire('./unity-log', {
-        'clay-log/plugins/_utils': {
-          wrap: callWrapperImmediately(data, msg)
-        }
+        });
       });
 
-      expect(msg).to.equal('Data will have error object');
-      expect(data).to.have.property('error');
-      expect(data.error).to.have.property('stack');
-      expect(data.error).to.have.property('kind');
-      expect(data.error).to.deep.include({ kind: 'Error' });
-      expect(data).to.deep.include({
-        params: {
-          foo: 'bar'
+      it('has a stack trace and kind set to CustomError, msg is string', () => {
+        class CustomError extends Error {
+          constructor(message) {
+            super(message);
+            this.name = 'CustomError';
+          }
         }
-      });
-    });
 
-    it('error is instanceOf CustomError', () => {
-      class CustomError extends Error {
-        constructor(message) {
-          super(message);
-          this.name = 'CustomError';
-        }
-      }
+        const data = {
+            error: new CustomError('data Error'),
+            params: {
+              foo: 'bar'
+            }
+          },
+          msg = 'Data will have error object';
 
-      const data = {
-          error: new CustomError('data Error'),
+        proxyquire('./unity-log', {
+          'clay-log/plugins/_utils': {
+            wrap: callWrapperImmediately(data, msg)
+          }
+        });
+
+        expect(msg).to.equal('Data will have error object');
+        expect(data).to.have.property('error');
+        expect(data.error).to.have.property('stack');
+        expect(data.error).to.deep.include({ kind: 'CustomError' });
+        expect(data).to.deep.include({
           params: {
             foo: 'bar'
           }
-        },
-        msg = 'Data will have error object';
-
-      proxyquire('./unity-log', {
-        'clay-log/plugins/_utils': {
-          wrap: callWrapperImmediately(data, msg)
-        }
+        });
       });
 
-      expect(msg).to.equal('Data will have error object');
-      expect(data).to.have.property('error');
-      expect(data.error).to.have.property('stack');
-      expect(data.error).to.deep.include({ kind: 'CustomError' });
-      expect(data).to.deep.include({
-        params: {
-          foo: 'bar'
-        }
-      });
-    });
+      it('is plain object without and instance of Error', () => {
+        const data = {
+            error: {
+              foo: 'bar'
+            },
+            params: {
+              foo: 'bar'
+            }
+          },
+          msg = 'Data will have error object';
 
-    it('error is plain object', () => {
-      const data = {
+        proxyquire('./unity-log', {
+          'clay-log/plugins/_utils': {
+            wrap: callWrapperImmediately(data, msg)
+          }
+        });
+
+        expect(msg).to.equal('Data will have error object');
+        expect(data).to.deep.equal({
           error: {
             foo: 'bar'
           },
           params: {
             foo: 'bar'
           }
-        },
-        msg = 'Data will have error object';
-
-      proxyquire('./unity-log', {
-        'clay-log/plugins/_utils': {
-          wrap: callWrapperImmediately(data, msg)
-        }
-      });
-
-      expect(msg).to.equal('Data will have error object');
-      expect(data).to.deep.equal({
-        error: {
-          foo: 'bar'
-        },
-        params: {
-          foo: 'bar'
-        }
+        });
       });
     });
+
   });
+
 
   context('data does not contain error key', () => {
     it('error key undefined', () => {
