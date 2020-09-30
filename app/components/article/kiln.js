c0642a39d0647166d1ee3d5fe4b6d6c765c8cb43
Ferney Peña Rivillas <ferney.pena@gorillalogic.com>
2020-09-30 11:14:02 -0500

    ON-2407 Added validation to prevent same author and host name

--- app/components/article/kiln.js
+++ app/components/article/kiln.js
@@ -2,11 +2,28 @@
 
 const { syncFields, syncHeadlines } = require('../../services/client/kiln-utils'),
   applyContentLogic = require('../../services/kiln/apply-content-logic'),
-  autoFillRecircImg = require('../../services/kiln/shared/content-components/autofill-recirc-img-to-lead-img');
+  autoFillRecircImg = require('../../services/kiln/shared/content-components/autofill-recirc-img-to-lead-img'),
+  differenceBy = require('lodash/differenceBy'),
+  KilnInput = window.kiln.kilnInput;
 
 module.exports = (schema) => {
   applyContentLogic(schema);
   autoFillRecircImg(schema);
+
+  schema.byline = new KilnInput(schema, 'byline');
+
+  schema.formValidation = () => {
+    const authors = schema.byline.value()[0].names,
+      hosts = schema.byline.value()[0].hosts,
+      filtered = differenceBy(authors, hosts, 'text');
+
+    if (authors.length > filtered.length) {
+      schema.byline.showSnackBar({ message: 'Authors and Hosts should be different', position: 'center' });
+      return false;
+    }
+
+    return true;
+  };
 
   return syncFields(schema, syncHeadlines('article'));
 };
