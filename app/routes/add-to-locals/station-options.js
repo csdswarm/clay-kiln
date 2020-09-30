77913fd7cf21dd3b1db932ac354a850f9466a151
Cristhiam Henao <cristhiam.henao@gorillalogic.com>
2020-09-30 11:00:30 -0500

    ON-2503 | Backend for station options

--- app/routes/add-to-locals/station-options.js
+++ app/routes/add-to-locals/station-options.js
@@ -1,22 +1,24 @@
 'use strict';
 
-const db = require('../../services/server/db'),
+const { wrapInTryCatch } = require('../../services/startup/middleware-utils'),
+  db = require('../../services/server/db'),
   redis = require('../../services/server/redis');
   
 
 module.exports = app => {
-  app.use(async (req, res, next) => {
+  app.use(wrapInTryCatch(async (req, res, next) => {
     const { station } = res.locals;
 
     let stationOptions = await redis.get(`station_options:${station.id}`);
 
     if (!stationOptions) {
-      stationOptions = await db.get(`${process.env.CLAY_SITE_HOST}/_station_options/${station.id}`, res.locals, '{}');
-      redis.set(`station_options:${station.id}`, stationOptions);
+      stationOptions = await db.get(`${process.env.CLAY_SITE_HOST}/_station_options/${station.id}`, res.locals, {});
+      redis.set(`station_options:${station.id}`, JSON.stringify(stationOptions));
+      res.locals.stationOptions = stationOptions;
+      next();
     }
 
     res.locals.stationOptions = JSON.parse(stationOptions);
-
     next();
-  });
+  }));
 };
