c346f8c3c3a70aee10b4df29b4f1de52a17a06da
Kurt Moffett <kurtis.moffett@entercom.com>
2020-09-30 10:03:51 -0700

    ON-1893 | Corrected where statement to properly match meta urls.

--- app/routes/add-to-locals/is-dynamic-page.js
+++ app/routes/add-to-locals/is-dynamic-page.js
@@ -13,21 +13,21 @@ module.exports = router => {
 
     try {
       if (edit) {
-        const where = _.includes(req.uri, '_pages') ? 'WHERE id =' : "WHERE meta->>'url' ~",
-          isDynamic = await db.raw(`
+        const where = _.includes(req.uri, '_pages') ? `WHERE id = ${req.uri}` : `WHERE meta->>'url' = http://${req.uri}`,
+          results = await db.raw(`
             WITH _page_main (main) AS (
               SELECT data->'main'->>0, position('/instances/' in data->'main'->>0)
               FROM pages
-              ${where} '${req.uri}'
+              ${where}
             )
         
-            SELECT _p.data->>'_dynamic' AS dynamic
+            SELECT _pm.main, _p.data->>'_dynamic' AS dynamic
             FROM _page_main _pm, pages _p
             WHERE LEFT(_pm.main, _pm.position) || 'instances/new' = _p.data->'main'->>0
             AND _p.data->>'_dynamic' IS NOT NULL
           `);
 
-        if (_.get(isDynamic, 'rows[0].dynamic')) {
+        if (_.get(results, 'rows[0].dynamic')) {
           res.locals.isDynamicPage = true;
         };
 
