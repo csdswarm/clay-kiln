0f8f4ddd625ceada3781746622c8905021766b72
Ferney Peña Rivillas <ferney.pena@gorillalogic.com>
2020-10-02 10:09:31 -0500

    ON-2407 Refactor implementation

--- app/components/article/kiln.js
+++ app/components/article/kiln.js
@@ -3,27 +3,12 @@
 const { syncFields, syncHeadlines } = require('../../services/client/kiln-utils'),
   applyContentLogic = require('../../services/kiln/apply-content-logic'),
   autoFillRecircImg = require('../../services/kiln/shared/content-components/autofill-recirc-img-to-lead-img'),
-  differenceBy = require('lodash/differenceBy'),
-  KilnInput = window.kiln.kilnInput;
+  bylineValidator = require('../../services/kiln/byline-validator');
 
 module.exports = (schema) => {
   applyContentLogic(schema);
   autoFillRecircImg(schema);
-
-  schema.byline = new KilnInput(schema, 'byline');
-
-  schema.formValidation = () => {
-    const authors = schema.byline.value()[0].names,
-      hosts = schema.byline.value()[0].hosts,
-      filtered = differenceBy(authors, hosts, 'text');
-
-    if (authors.length > filtered.length) {
-      schema.byline.showSnackBar({ message: 'Authors and Hosts should be different' });
-      return false;
-    }
-
-    return true;
-  };
+  bylineValidator(schema);
 
   return syncFields(schema, syncHeadlines('article'));
 };
