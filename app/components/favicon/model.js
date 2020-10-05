f8975a2be059390fe3feac7adbeb29188fc26eca
jeremy <jeremy.zevin@entercom.com>
2020-10-05 12:28:56 -0400

    ON-2517: jz | add jsdocs and path changes

--- app/components/favicon/model.js
+++ app/components/favicon/model.js
@@ -1,13 +1,25 @@
 'use strict';
 
 const { unityComponent } = require('../../services/universal/amphora'),
+  _get = require('lodash/get'),
   iconConfig = {
     faviconSizes: [16, 32, 96],
     appleTouchIconSizes: [0, 152, 167, 180],
     defaultIconPath: '/media/components/favicon/favicon.png'
   };
 
-function buildIconLinkObject(size, linkTarget, path) {
+/**
+ * returns formatted data for the view to consume to build the icon head links
+ *
+ * @param {string} size
+ * @param {string} linkTarget
+ * @param {object} locals
+ * @returns {object}
+ */
+function buildIconLinkObject(size, linkTarget, locals) {
+  const faviconPath = _get(locals,'stationOptions.favicon'),
+    path = faviconPath || iconConfig.defaultIconPath;
+
   return {
     rel: linkTarget === 'favicon' ? 'icon' : 'apple-touch-icon-precomposed',
     sizes: size ? `${size}x${size}` : null,
@@ -16,6 +28,12 @@ function buildIconLinkObject(size, linkTarget, path) {
   };
 }
 
+/**
+ * returns the extension of a file path
+ *
+ * @param {string} path
+ * @returns {string}
+ */
 function getFileTypeFromPath(path) {
   return path.slice(path.lastIndexOf('.') + 1);
 }
@@ -28,8 +46,12 @@ module.exports = unityComponent({
     }
 
     data._computed.icons = [
-      ...iconConfig.faviconSizes.map(size => buildIconLinkObject(size, 'favicon', iconConfig.defaultIconPath)),
-      ...iconConfig.appleTouchIconSizes.map(size => buildIconLinkObject(size, 'appleTouchIcon', iconConfig.defaultIconPath))
+      ...iconConfig.faviconSizes.map(size => buildIconLinkObject(
+        size, 'favicon', locals)
+      ),
+      ...iconConfig.appleTouchIconSizes.map(size => buildIconLinkObject(
+        size, 'appleTouchIcon', locals)
+      )
     ];
     data._computed.stationId = locals.station.id;
     data._computed.stationOptions = locals.stationOptions;
